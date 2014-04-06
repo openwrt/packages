73764a009ffa7daab58d46a6b2df306d602139cb
Marek Lindner <mareklindner@neomailbox.ch>
2014-04-06 21:51:04 +0800

    batman-adv: add support for the isolation_mark option
    With the introduction of the Extended Isolation component[1]
    a new option is required by batman-adv: the isolation_mark.
    This patch adds support for it in the configuration file
    
    [1] http://www.open-mesh.org/projects/batman-adv/wiki/Extended-isolation
    
    Signed-off-by: Antonio Quartulli <antonio@open-mesh.com>
    signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

--- batman-adv/files/lib/batman-adv/config.sh
+++ batman-adv/files/lib/batman-adv/config.sh
@@ -12,7 +12,8 @@ bat_config()
 {
 	local mesh="$1"
 	local aggregated_ogms ap_isolation bonding bridge_loop_avoidance distributed_arp_table fragmentation
-	local gw_bandwidth gw_mode gw_sel_class hop_penalty network_coding log_level orig_interval vis_mode
+	local gw_bandwidth gw_mode gw_sel_class isolation_mark hop_penalty multicast_mode network_coding log_level
+	local orig_interval vis_mode
 
 	config_get aggregated_ogms "$mesh" aggregated_ogms
 	config_get ap_isolation "$mesh" ap_isolation
@@ -24,6 +25,7 @@ bat_config()
 	config_get gw_mode "$mesh" gw_mode
 	config_get gw_sel_class "$mesh" gw_sel_class
 	config_get hop_penalty "$mesh" hop_penalty
+	config_get isolation_mark "$mesh" isolation_mark
 	config_get network_coding "$mesh" network_coding
 	config_get log_level "$mesh" log_level
 	config_get orig_interval "$mesh" orig_interval
@@ -41,6 +43,7 @@ bat_config()
 	[ -n "$gw_mode" ] && echo $gw_mode > /sys/class/net/$mesh/mesh/gw_mode
 	[ -n "$gw_sel_class" ] && echo $gw_sel_class > /sys/class/net/$mesh/mesh/gw_sel_class
 	[ -n "$hop_penalty" ] && echo $hop_penalty > /sys/class/net/$mesh/mesh/hop_penalty
+	[ -n "$isolation_mark" ] && echo $isolation_mark > /sys/class/net/$mesh/mesh/isolation_mark
 	[ -n "$network_coding" ] && echo $network_coding > /sys/class/net/$mesh/mesh/network_coding 2>&-
 	[ -n "$log_level" ] && echo $log_level > /sys/class/net/$mesh/mesh/log_level 2>&-
 	[ -n "$orig_interval" ] && echo $orig_interval > /sys/class/net/$mesh/mesh/orig_interval
