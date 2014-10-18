f729ba98c5ba44523202e4bfe98c863830dd818c
Alexandru Ardelean <ardeleanalex@gmail.com>
2014-10-19 00:45:59 +0300

    python3: export CC + CCSHARED env vars to cross-compile Python C extensions properly
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

--- lang/python3/files/python3-package.mk
+++ lang/python3/files/python3-package.mk
@@ -72,6 +72,8 @@ endef
 define Build/Compile/Py3Mod
 	$(call HostPython3, \
 		cd $(PKG_BUILD_DIR)/$(strip $(1)); \
+		CC="$(TARGET_CC)" \
+		CCSHARED="$(TARGET_CC) $(FPIC)" \
 		CFLAGS="$(TARGET_CFLAGS)" \
 		CPPFLAGS="$(TARGET_CPPFLAGS)" \
 		LDFLAGS="$(TARGET_LDFLAGS)" \
