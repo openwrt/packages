78bf9cc55463187751dd3a3838a6e96e18ffec23
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2014-12-05 20:27:45 +0100

    openconnect: simplified cmdline appending
    Signed-off-by: Nikos Mavrogiannopoulos <nmav@gnutls.org>

--- net/openconnect/files/openconnect.sh
+++ net/openconnect/files/openconnect.sh
@@ -38,10 +38,16 @@ proto_openconnect_setup() {
 
 	cmdline="$server$port -i vpn-$config --non-inter --syslog --script /lib/netifd/vpnc-script"
 
-	[ -f /etc/openconnect/ca-vpn-$config.pem ] && append cmdline "--no-system-trust --cafile /etc/openconnect/ca-vpn-$config.pem"
 	[ -f /etc/openconnect/user-cert-vpn-$config.pem ] && append cmdline "-c /etc/openconnect/user-cert-vpn-$config.pem"
 	[ -f /etc/openconnect/user-key-vpn-$config.pem ] && append cmdline "--sslkey /etc/openconnect/user-key-vpn-$config.pem"
-	[ -n "$serverhash" ] && append cmdline "--no-system-trust --servercert=$serverhash"
+	if test -f /etc/openconnect/ca-vpn-$config.pem;then
+		append cmdline "--cafile /etc/openconnect/ca-vpn-$config.pem"
+		append cmdline "--no-system-trust"
+	fi
+	if test -n "$serverhash";then
+		append cmdline " --servercert=$serverhash"
+		append cmdline "--no-system-trust"
+	fi
 	[ -n "$authgroup" ] && append cmdline "--authgroup $authgroup"
 	[ -n "$username" ] && append cmdline "-u $username"
 	[ -n "$password" ] && {
