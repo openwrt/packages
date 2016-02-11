4c3eecfac3ac73e63fae28a0d51def8e1cd5e261
Sven Eckelmann <sven@narfation.org>
2016-02-11 23:08:20 +0100

    batman-adv: Add B.A.T.M.A.N. V support
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- batman-adv/files/compat-hacks.h
+++ batman-adv/files/compat-hacks.h
@@ -37,3 +37,40 @@
 #define IFF_NO_QUEUE	0; dev->tx_queue_len = 0
 
 #endif /* < KERNEL_VERSION(4, 3, 0) */
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 6, 0)
+
+#include <linux/ethtool.h>
+
+#define ethtool_link_ksettings batadv_ethtool_link_ksettings
+
+struct batadv_ethtool_link_ksettings {
+	struct {
+		__u32	speed;
+		__u8	duplex;
+	} base;
+};
+
+#define __ethtool_get_link_ksettings(__dev, __link_settings) \
+	batadv_ethtool_get_link_ksettings(__dev, __link_settings)
+
+static inline int
+batadv_ethtool_get_link_ksettings(struct net_device *dev,
+				  struct ethtool_link_ksettings *link_ksettings)
+{
+	struct ethtool_cmd cmd;
+	int ret;
+
+	memset(&cmd, 0, sizeof(cmd));
+	ret = __ethtool_get_settings(dev, &cmd);
+
+	if (ret != 0)
+		return ret;
+
+	link_ksettings->base.duplex = cmd.duplex;
+	link_ksettings->base.speed = ethtool_cmd_speed(&cmd);
+
+	return 0;
+}
+
+#endif /* < KERNEL_VERSION(4, 6, 0) */
