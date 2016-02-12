8b767e9382faf2f1a0dde5802e0611a4ed8feabc
Philipp Borgers <borgers@mi.fu-berlin.de>
2016-02-12 18:59:43 +0100

    fix quoting of daemon name
    The current quoting did not allow substitution and resulted in log messages with
    the prefix "${DAEMON}[hotplug]".

--- oonf-init-scripts/files/oonf_hotplug.sh
+++ oonf-init-scripts/files/oonf_hotplug.sh
@@ -3,7 +3,7 @@
 case "${ACTION}" in
 	ifup)
 		. /etc/rc.common /etc/init.d/${DAEMON} enabled && {
-			logger -t '${DAEMON}[hotplug]' -p daemon.info 'reloading configuration'
+			logger -t "${DAEMON}[hotplug]" -p daemon.info 'reloading configuration'
 			. /etc/rc.common /etc/init.d/${DAEMON} reload
 		}
 	;;
