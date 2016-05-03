93f968349876b2d8b21541e38151f24f92ab7442
titobrasolin <tito.brasolin@gmail.com>
2016-05-03 18:29:39 +0200

    lang/python-sip: Port from old packages and update to 4.12.2
    Updated to 4.12.2.
    Package source changed to a more reliable URL.
    
    Signed-off-by: Tito Brasolin <tito.brasolin@gmail.com>

---
+++ lang/python-sip/files/python-sip-package.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2010 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+PYTHON_SIP_CONFIG_TEMPLATE_DIR:=$(STAGING_DIR)/usr/share/python-sip
+PYTHON_SIP_CONFIG_TEMPLATE:=$(PYTHON_SIP_CONFIG_TEMPLATE_DIR)/sipconfig.template
+
