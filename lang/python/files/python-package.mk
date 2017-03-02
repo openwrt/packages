a6e6d8d4bc9bbf65bec6c01699610d364736904f
Alexandru Ardelean <ardeleanalex@gmail.com>
2017-03-02 14:14:02 +0200

    python: move filespec shell code into file
    Cleanup.
    And preparation for adding a bit more functionality.
    
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

--- lang/python/files/python-package.mk
+++ lang/python/files/python-package.mk
@@ -54,36 +54,18 @@ define PyPackage
 
   define Package/$(1)/install
 	find $(PKG_INSTALL_DIR) -name "*\.pyc" -o -name "*\.pyo" -o -name "*\.exe" | xargs rm -f
-	@echo "$$$$$$$$$$(call shvar,PyPackage/$(1)/filespec)" | ( \
-		IFS='|'; \
-		while read fop fspec fperm; do \
-		  fop=`echo "$$$$$$$$fop" | tr -d ' \t\n'`; \
-		  if [ "$$$$$$$$fop" = "+" ]; then \
-			if [ ! -e "$(PKG_INSTALL_DIR)$$$$$$$$fspec" ]; then \
-			  echo "File not found '$(PKG_INSTALL_DIR)$$$$$$$$fspec'"; \
-			  exit 1; \
-			fi; \
-			dpath=`dirname "$$$$$$$$fspec"`; \
-			if [ -n "$$$$$$$$fperm" ]; then \
-			  dperm="-m$$$$$$$$fperm"; \
-			else \
-			  dperm=`stat -c "%a" $(PKG_INSTALL_DIR)$$$$$$$$dpath`; \
-			fi; \
-			mkdir -p $$$$$$$$$dperm $$(1)$$$$$$$$dpath; \
-			echo "copying: '$$$$$$$$fspec'"; \
-			cp -fpR $(PKG_INSTALL_DIR)$$$$$$$$fspec $$(1)$$$$$$$$dpath/; \
-			if [ -n "$$$$$$$$fperm" ]; then \
-			  chmod -R $$$$$$$$fperm $$(1)$$$$$$$$fspec; \
-			fi; \
-		  elif [ "$$$$$$$$fop" = "-" ]; then \
-			echo "removing: '$$$$$$$$fspec'"; \
-			rm -fR $$(1)$$$$$$$$fspec; \
-		  elif [ "$$$$$$$$fop" = "=" ]; then \
-			echo "setting permissions: '$$$$$$$$fperm' on '$$$$$$$$fspec'"; \
-			chmod -R $$$$$$$$fperm $$(1)$$$$$$$$fspec; \
-		  fi; \
-		done; \
-	)
+	if [ -e files/python-package-install.sh ] ; then \
+		$(SHELL) files/python-package-install.sh \
+			"$(PKG_INSTALL_DIR)" "$$(1)" \
+			"$$$$$$$$$$(call shvar,PyPackage/$(1)/filespec)" ; \
+	elif [ -e $(STAGING_DIR)/mk/python-package-install.sh ] ; then \
+		$(SHELL) $(STAGING_DIR)/mk/python-package-install.sh \
+			"$(PKG_INSTALL_DIR)" "$$(1)" \
+			"$$$$$$$$$$(call shvar,PyPackage/$(1)/filespec)" ; \
+	else \
+		echo "No 'python-package-install.sh' script found" ; \
+		exit 1 ; \
+	fi
 	$(call PyPackage/$(1)/install,$$(1))
   endef
 endef
