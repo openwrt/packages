0bbb73d0abd205cda26b770ae11554471a845554
Daniel H <halmi@users.noreply.github.com>
2017-04-12 22:51:58 +0200

    acme: Fix for curl linked against mbed TLS. (#4254)
    Use newest acme.sh release (2.6.8).
    Remove dependency on ca-certificates and add dependency on ca-bundle.
    Update environment variable.
    
    Signed-off-by: Daniel Halmschlager <da@halms.at>

--- net/acme/files/run.sh
+++ net/acme/files/run.sh
@@ -10,7 +10,7 @@
 
 CHECK_CRON=$1
 ACME=/usr/lib/acme/acme.sh
-export SSL_CERT_DIR=/etc/ssl/certs
+export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 export NO_TIMESTAMP=1
 
 UHTTPD_LISTEN_HTTP=
