a4691a289c00f93f126df4c5d3fe68c3fb0ef72a
Alexandru Ardelean <ardeleanalex@gmail.com>
2017-05-19 17:18:33 +0300

    python3: remove __pycache__ folders pip & setuptools
    python3-pip & python3-setuptools have slightly
    different installation mechanisms.
    
    We need to remove the __pycache__ folders.
    Seems they're generated.
    
    This also reduces the size of the python3-pip &
    python3-setuptools packages.
    
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

--- lang/python/python3/files/python3-package-setuptools.mk
+++ lang/python/python3/files/python3-package-setuptools.mk
@@ -23,6 +23,7 @@ define Py3Package/python3-setuptools/install
 		$(PKG_BUILD_DIR)/install-setuptools/lib/python$(PYTHON3_VERSION)/site-packages/setuptools \
 		$(PKG_BUILD_DIR)/install-setuptools/lib/python$(PYTHON3_VERSION)/site-packages/easy_install.py \
 		$(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages
+	find $(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages/ -name __pycache__ | xargs rm -rf
 endef
 
 $(eval $(call Py3BasePackage,python3-setuptools, \
