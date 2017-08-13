e82f8d58ea81cd7c358b775af536ceb802bdc5e2
Alexandru Ardelean <ardeleanalex@gmail.com>
2017-08-13 22:06:08 +0300

    python,python3: add host-flags/settings to host pip install rules
    The host pip install should have the host's CFLAGS, LDFLAGS, etc
    available.
    And not the target's flags.
    Otherwise, weird things can happen when installing
    packages (host-side) that need to build C code.
    
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

--- lang/python/python/files/python-host.mk
+++ lang/python/python/files/python-host.mk
@@ -37,21 +37,26 @@ define HostPython
 	$(HOST_PYTHON_BIN) $(2);
 endef
 
+define host_python_settings
+	ARCH="$(HOST_ARCH)" \
+	CC="$(HOSTCC)" \
+	CCSHARED="$(HOSTCC) $(HOST_FPIC)" \
+	CXX="$(HOSTCXX)" \
+	LD="$(HOSTCC)" \
+	LDSHARED="$(HOSTCC) -shared" \
+	CFLAGS="$(HOST_CFLAGS)" \
+	CPPFLAGS="$(HOST_CPPFLAGS) -I$(HOST_PYTHON_INC_DIR)" \
+	LDFLAGS="$(HOST_LDFLAGS) -lpython$(PYTHON_VERSION) -Wl$(comma)-rpath=$(STAGING_DIR_HOSTPKG)/lib" \
+	_PYTHON_HOST_PLATFORM=linux2
+endef
+
 # $(1) => commands to execute before running pythons script
 # $(2) => python script and its arguments
 # $(3) => additional variables
 define Build/Compile/HostPyRunHost
 	$(call HostPython, \
 		$(if $(1),$(1);) \
-		CC="$(HOSTCC)" \
-		CCSHARED="$(HOSTCC) $(HOST_FPIC)" \
-		CXX="$(HOSTCXX)" \
-		LD="$(HOSTCC)" \
-		LDSHARED="$(HOSTCC) -shared" \
-		CFLAGS="$(HOST_CFLAGS)" \
-		CPPFLAGS="$(HOST_CPPFLAGS) -I$(HOST_PYTHON_INC_DIR)" \
-		LDFLAGS="$(HOST_LDFLAGS) -lpython$(PYTHON_VERSION) -Wl$(comma)-rpath=$(STAGING_DIR_HOSTPKG)/lib" \
-		_PYTHON_HOST_PLATFORM=linux2 \
+		$(call host_python_settings) \
 		$(3) \
 		, \
 		$(2) \
@@ -63,6 +68,7 @@ endef
 # Note: I shamelessly copied this from Yousong's logic (from python-packages);
 HOST_PYTHON_PIP:=$(STAGING_DIR_HOSTPKG)/bin/pip$(PYTHON_VERSION)
 define host_python_pip_install
+	$(call host_python_settings) \
 	$(HOST_PYTHON_PIP) install \
 		--root=$(1) \
 		--prefix=$(2) \
