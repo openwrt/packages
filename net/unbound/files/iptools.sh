09cc470924f6b5df74ebbfbbc92798e7759d07d8
Eric Luehrsen <ericluehrsen@gmail.com>
2018-06-28 20:41:37 -0400

    unbound: add UCI for forward stub and auth zone clauses
    With growing interest, DNS over TLS can be setup in Unbounds foward-zone:
    clause. A broader UCI solution is added to support forward-, stub-, and
    auth- zone clauses in a new 'zone' section. This implentation required
    reworking scripts, because they did not scale. 'forward_domain' and
    'prefetch_root' options are removed, and superceded by 'zone' section.
    
    Signed-off-by: Eric Luehrsen <ericluehrsen@gmail.com>

--- net/unbound/files/iptools.sh
+++ net/unbound/files/iptools.sh
@@ -160,3 +160,25 @@ domain_ptr_any() {
 
 ##############################################################################
 
+host_ptr_any() {
+  local subnet=$1
+  local arpa validip4 validip6
+
+  validip4=$( valid_subnet4 $subnet )
+  validip6=$( valid_subnet6 $subnet )
+
+
+  if [ "$validip4" = "ok" ] ; then
+    arpa=$( host_ptr_ip4 "$subnet" )
+  elif [ "$validip6" = "ok" ] ; then
+    arpa=$( host_ptr_ip6 "$subnet" )
+  fi
+
+
+  if [ -n "$arpa" ] ; then
+    echo $arpa
+  fi
+}
+
+##############################################################################
+
