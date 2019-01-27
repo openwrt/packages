e6fc697d418be2053fe4b99518f2c704fde6165e
Sven Eckelmann <sven@narfation.org>
2019-01-27 16:28:23 +0100

    batman-adv: upgrade package to latest release 2019.0
    * support latest kernels (3.16 - 5.0)
    * coding style cleanups and refactoring
    * allow to enable debug tracing without full batman-adv debugfs support
    * enable inconsistency reporting for most netlink dump commands
    * bugs squashed:
    
      - avoid unnecessary kernel warning (panic) during detection of interface loops
      - work around incorrect ethernet header offset in transmit code path
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- batman-adv/src/compat-hacks.h
+++ batman-adv/src/compat-hacks.h
@@ -348,6 +348,27 @@ typedef unsigned __poll_t;
 
 #endif /* < KERNEL_VERSION(4, 16, 0) */
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 0, 0)
+
+static inline int batadv_access_ok(int type, const void __user *p,
+				   unsigned long size)
+{
+	return access_ok(type, p, size);
+}
+
+#ifdef access_ok
+#undef access_ok
+#endif
+
+#define access_ok_get(_1, _2, _3 , access_ok_name, ...) access_ok_name
+#define access_ok(...) \
+	access_ok_get(__VA_ARGS__, access_ok3, access_ok2)(__VA_ARGS__)
+
+#define access_ok2(addr, size)	batadv_access_ok(VERIFY_WRITE, (addr), (size))
+#define access_ok3(type, addr, size)	batadv_access_ok((type), (addr), (size))
+
+#endif /* < KERNEL_VERSION(5, 0, 0) */
+
 /* <DECLARE_EWMA> */
 
 #include <linux/version.h>
