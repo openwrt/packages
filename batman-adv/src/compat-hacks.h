df9d2719fa6ab0fd37c4396d6cc593d488eea546
Sven Eckelmann <sven@narfation.org>
2019-02-22 20:19:42 +0100

    batman-adv: Drop compat-hack for from_timer
    from_timer is already defined by the mac80211's backports and
    doesn't have to be redefined in the compat-hacks.h.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- batman-adv/src/compat-hacks.h
+++ batman-adv/src/compat-hacks.h
@@ -146,24 +146,6 @@ batadv_ethtool_get_link_ksettings(struct net_device *dev,
 
 #endif /* < KERNEL_VERSION(4, 15, 0) */
 
-#ifndef from_timer
-
-#define TIMER_DATA_TYPE                unsigned long
-#define TIMER_FUNC_TYPE                void (*)(TIMER_DATA_TYPE)
-
-static inline void timer_setup(struct timer_list *timer,
-			       void (*callback)(struct timer_list *),
-			       unsigned int flags)
-{
-	__setup_timer(timer, (TIMER_FUNC_TYPE)callback,
-		      (TIMER_DATA_TYPE)timer, flags);
-}
-
-#define from_timer(var, callback_timer, timer_fieldname) \
-	container_of(callback_timer, typeof(*var), timer_fieldname)
-
-#endif /* !from_timer */
-	
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 16, 0)
 
 
