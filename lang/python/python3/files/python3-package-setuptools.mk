0383c374b91ab75016c0ba399ce1e087ba89c41b
Jeffery To <jeffery.to@gmail.com>
2019-05-29 22:42:02 +0800

    python3-setuptools,python3-pip: Use more predictable pycache cleanup
    This uses two find commands to delete __pycache__ contents then the
    __pycache__ directories, rather than a for loop.
    
    The second command omits a -empty test, so that if the first command
    doesn't remove all directory contents for some reason, the second
    command will return an error (find will not delete a non-empty
    directory).
    
    Signed-off-by: Jeffery To <jeffery.to@gmail.com>

--- lang/python/python3/files/python3-package-setuptools.mk
+++ lang/python/python3/files/python3-package-setuptools.mk
@@ -25,10 +25,8 @@ define Py3Package/python3-setuptools/install
 		$(PKG_BUILD_DIR)/install-setuptools/usr/lib/python$(PYTHON3_VERSION)/site-packages/setuptools-$(PYTHON3_SETUPTOOLS_VERSION).dist-info \
 		$(PKG_BUILD_DIR)/install-setuptools/usr/lib/python$(PYTHON3_VERSION)/site-packages/easy_install.py \
 		$(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages
-	for _ in \$(seq 1 10) ; do \
-		find $(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages/ -name __pycache__ -exec rm -rf {} \; || continue ; \
-		break ; \
-	done
+	find $(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages/ -path '*/__pycache__/*' -delete
+	find $(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages/ -type d -name __pycache__ -delete
 endef
 
 $(eval $(call Py3BasePackage,python3-setuptools, \
