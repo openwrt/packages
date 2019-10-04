721d3778f13376c6adce00155f3f6f21847e0d05
Zoltan Haindrich <kirk@rxd.hu>
2019-10-04 15:43:50 +0000

    prometheus-node-exporter-lua: an unavaliable wifi interface may have stopped the scraper from functioning
    Signed-off-by: Zoltan Haindrich <kirk@rxd.hu>
    (cherry picked from commit 7bec619f5367110b9e336ef1a65737d37b1f0056)

--- utils/prometheus-node-exporter-lua/files/usr/lib/lua/prometheus-collectors/wifi_stations.lua
+++ utils/prometheus-node-exporter-lua/files/usr/lib/lua/prometheus-collectors/wifi_stations.lua
@@ -26,42 +26,44 @@ local function scrape()
   for dev, dev_table in pairs(status) do
     for _, intf in ipairs(dev_table['interfaces']) do
       local ifname = intf['ifname']
-      local iw = iwinfo[iwinfo.type(ifname)]
-      local count = 0
+      if ifname ~= nil then
+        local iw = iwinfo[iwinfo.type(ifname)]
+        local count = 0
 
-      local assoclist = iw.assoclist(ifname)
-      for mac, station in pairs(assoclist) do
-        local labels = {
-          ifname = ifname,
-          mac = mac,
-        }
-        if station.signal and station.signal ~= 0 then
-          metric_wifi_station_signal(labels, station.signal)
-        end
-        if station.inactive then
-          metric_wifi_station_inactive(labels, station.inactive)
-        end
-        if station.expected_throughput and station.expected_throughput ~= 0 then
-          metric_wifi_station_exp_thr(labels, station.expected_throughput)
-        end
-        if station.tx_rate and station.tx_rate ~= 0 then
-          metric_wifi_station_tx_bitrate(labels, station.tx_rate)
-        end
-        if station.rx_rate and station.rx_rate ~= 0 then
-          metric_wifi_station_rx_bitrate(labels, station.rx_rate)
-        end
-        metric_wifi_station_tx_packets(labels, station.tx_packets)
-        metric_wifi_station_rx_packets(labels, station.rx_packets)
-        if station.tx_bytes then
-          metric_wifi_station_tx_bytes(labels, station.tx_bytes)
-        end
-        if station.rx_bytes then
-          metric_wifi_station_rx_bytes(labels, station.rx_bytes)
-        end
+        local assoclist = iw.assoclist(ifname)
+        for mac, station in pairs(assoclist) do
+          local labels = {
+            ifname = ifname,
+            mac = mac,
+          }
+          if station.signal and station.signal ~= 0 then
+            metric_wifi_station_signal(labels, station.signal)
+          end
+          if station.inactive then
+            metric_wifi_station_inactive(labels, station.inactive)
+          end
+          if station.expected_throughput and station.expected_throughput ~= 0 then
+            metric_wifi_station_exp_thr(labels, station.expected_throughput)
+          end
+          if station.tx_rate and station.tx_rate ~= 0 then
+            metric_wifi_station_tx_bitrate(labels, station.tx_rate)
+          end
+          if station.rx_rate and station.rx_rate ~= 0 then
+            metric_wifi_station_rx_bitrate(labels, station.rx_rate)
+          end
+          metric_wifi_station_tx_packets(labels, station.tx_packets)
+          metric_wifi_station_rx_packets(labels, station.rx_packets)
+          if station.tx_bytes then
+            metric_wifi_station_tx_bytes(labels, station.tx_bytes)
+          end
+          if station.rx_bytes then
+            metric_wifi_station_rx_bytes(labels, station.rx_bytes)
+          end
 
-        count = count + 1
+          count = count + 1
+        end
+        metric_wifi_stations({ifname = ifname}, count)
       end
-      metric_wifi_stations({ifname = ifname}, count)
     end
   end
 end
