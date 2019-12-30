2feef07c0c133af2e5faee18a06a5f79362a8308
Álvaro Fernández Rojas <noltari@gmail.com>
2019-12-30 11:49:24 +0100

    omxplayer: add new package
    OMXPlayer is a command-line video player for the Raspberry Pi.
    It can play video directly from the command-line and doesn't require a
    desktop.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

---
+++ multimedia/omxplayer/files/version.h
@@ -0,0 +1,7 @@
+#ifndef __VERSION_H__
+#define __VERSION_H__
+#define VERSION_DATE "Sun, 30 Sep 2019 17:15:00 +0100"
+#define VERSION_HASH "ecd446d"
+#define VERSION_BRANCH "master"
+#define VERSION_REPO "https://github.com/popcornmix/omxplayer.git"
+#endif
