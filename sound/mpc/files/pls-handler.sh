4fba03d6d16af923199ea876da4fe992f8cbdb91
Rosen Penev <rosenp@gmail.com>
2020-02-19 19:31:09 -0800

    mpc: update to 0.33
    Small Makefile cleanups.
    
    Ran script through shellcheck.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- sound/mpc/files/pls-handler.sh
+++ sound/mpc/files/pls-handler.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env sh
+#!/bin/sh
 echo parameter: file.pls
 mpc clear
-grep '^File[0-9]*' $1 | sed -e 's/^File[0-9]*=//' | mpc add
+grep '^File[0-9]*' "$1" | sed -e 's/^File[0-9]*=//' | mpc add
 mpc play
