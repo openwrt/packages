be4ee59a450f6ef16757648ebfc2f3e1711b1eeb
Lucian Cristian <lucian.cristian@gmail.com>
2020-06-10 21:21:14 +0300

    acme-common: user same script for acme and uacme
    Signed-off-by: Lucian Cristian <lucian.cristian@gmail.com>

--- net/acme-common/files/run.sh
+++ net/acme-common/files/run.sh
@@ -1,26 +1,43 @@
 #!/bin/sh
-# Wrapper for acme.sh to work on openwrt.
+# Wrapper for acme/uacme to work on openwrt.
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation; either version 3 of the License, or (at your option) any later
 # version.
 #
-# Author: Toke Høiland-Jørgensen <toke@toke.dk>
+# Initial Author: Toke Høiland-Jørgensen <toke@toke.dk>
+# Adapted for uacme: Lucian Cristian <lucian.cristian@gmail.com>
 
 CHECK_CRON=$1
-ACME=/usr/lib/acme/acme.sh
+
+if [ -e "/usr/lib/acme/acme.sh" ]; then
+    ACME=/usr/lib/acme/acme.sh
+    APP=acme
+elif [ -e "/usr/sbin/uacme" ]; then
+    ACME=/usr/sbin/uacme
+    HPROGRAM=/usr/share/uacme/uacme.sh
+    APP=uacme
+else
+    echo "Please install ACME or uACME package"
+    return 1
+fi
+
 export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 export NO_TIMESTAMP=1
 
 UHTTPD_LISTEN_HTTP=
 STATE_DIR='/etc/acme'
+STAGING_STATE_DIR='/etc/acme/staging'
+
 ACCOUNT_EMAIL=
 DEBUG=0
 NGINX_WEBSERVER=0
 UPDATE_NGINX=0
 UPDATE_UHTTPD=0
+UPDATE_HAPROXY=0
 USER_CLEANUP=
+
 
 . /lib/functions.sh
 
@@ -33,17 +50,17 @@ check_cron()
 
 log()
 {
-    logger -t acme -s -p daemon.info -- "$@"
+    logger -t $APP -s -p daemon.info -- "$@"
 }
 
 err()
 {
-    logger -t acme -s -p daemon.err -- "$@"
+    logger -t $APP -s -p daemon.err -- "$@"
 }
 
 debug()
 {
-    [ "$DEBUG" -eq "1" ] && logger -t acme -s -p daemon.debug -- "$@"
+    [ "$DEBUG" -eq "1" ] && logger -t $APP -s -p daemon.debug -- "$@"
 }
 
 get_listeners() {
@@ -57,7 +74,7 @@ get_listeners() {
 
 run_acme()
 {
-    debug "Running acme.sh as '$ACME $@'"
+    debug "Running $APP as '$ACME $@'"
     $ACME "$@"
 }
 
@@ -77,10 +94,10 @@ pre_checks()
 
         case "$cmd" in
             uhttpd)
-                debug "Found uhttpd listening on port 80; trying to disable."
-
+                debug "Found uhttpd listening on port 80."
+                if [ "$APP" = "acme" ]; then
                 UHTTPD_LISTEN_HTTP=$(uci get uhttpd.main.listen_http)
-
+                debug "Trying to disable."
                 if [ -z "$UHTTPD_LISTEN_HTTP" ]; then
                     err "$main_domain: Unable to find uhttpd listen config."
                     err "Manually disable uhttpd or set webroot to continue."
@@ -94,9 +111,12 @@ pre_checks()
                     uci commit uhttpd
                     return 1
                 fi
+                fi
             ;;
             nginx*)
-                debug "Found nginx listening on port 80; trying to disable."
+                debug "Found nginx listening on port 80."
+                if [ "$APP" = "acme" ]; then
+                debug "Trying to disable."
                 NGINX_WEBSERVER=1
                 local tries=0
                 while grep -sq "$cmd" "/proc/$pid/cmdline" && kill -0 "$pid"; do
@@ -109,22 +129,27 @@ pre_checks()
                     tries=$((tries + 1))
                     sleep 1
                 done
+                fi
             ;;
             "")
                 debug "Nothing listening on port 80."
+                debug "Standalone mode not supported, setup uhttpd or nginx"
+                return 1
             ;;
             *)
-                err "$main_domain: Cannot run in standalone mode; another daemon is listening on port 80."
-                err "Disable other daemon or set webroot to continue."
+                err "$main_domain: unsupported (apache/haproxy?) daemon is listening on port 80."
+                err "if webroot is set on your current webserver comment line 142 (return 1) from this script."
                 return 1
             ;;
         esac
     done
 
     iptables -I input_rule -p tcp --dport 80 -j ACCEPT -m comment --comment "ACME" || return 1
-    ip6tables -I input_rule -p tcp --dport 80 -j ACCEPT -m comment --comment "ACME" || return 1
     debug "v4 input_rule: $(iptables -nvL input_rule)"
-    debug "v6 input_rule: $(ip6tables -nvL input_rule)"
+    if [ -e "/usr/sbin/ip6tables" ]; then
+       ip6tables -I input_rule -p tcp --dport 80 -j ACCEPT -m comment --comment "ACME" || return 1
+       debug "v6 input_rule: $(ip6tables -nvL input_rule)"
+    fi
     return 0
 }
 
@@ -134,7 +159,9 @@ post_checks()
     # The comment ensures we only touch our own rules. If no rules exist, that
     # is fine, so hide any errors
     iptables -D input_rule -p tcp --dport 80 -j ACCEPT -m comment --comment "ACME" 2>/dev/null
-    ip6tables -D input_rule -p tcp --dport 80 -j ACCEPT -m comment --comment "ACME" 2>/dev/null
+    if [ -e "/usr/sbin/ip6tables" ]; then
+       ip6tables -D input_rule -p tcp --dport 80 -j ACCEPT -m comment --comment "ACME" 2>/dev/null
+    fi
 
     if [ -e /etc/init.d/uhttpd ] && ( [ -n "$UHTTPD_LISTEN_HTTP" ] || [ "$UPDATE_UHTTPD" -eq 1 ] ); then
         if [ -n "$UHTTPD_LISTEN_HTTP" ]; then
@@ -143,11 +170,18 @@ post_checks()
         fi
         uci commit uhttpd
         /etc/init.d/uhttpd reload
+        log "Restarting uhttpd..."
     fi
 
     if [ -e /etc/init.d/nginx ] && ( [ "$NGINX_WEBSERVER" -eq 1 ] || [ "$UPDATE_NGINX" -eq 1 ] ); then
         NGINX_WEBSERVER=0
         /etc/init.d/nginx restart
+        log "Restarting nginx..."
+    fi
+
+    if [ -e /etc/init.d/haproxy ] && [ "$UPDATE_HAPROXY" -eq 1 ]; then
+       /etc/init.d/haproxy restart
+       log "Restarting haproxy..."
     fi
 
     if [ -n "$USER_CLEANUP" ] && [ -f "$USER_CLEANUP" ]; then
@@ -188,6 +222,7 @@ issue_cert()
     local use_staging
     local update_uhttpd
     local update_nginx
+    local update_haproxy
     local keylength
     local keylength_ecc=0
     local domains
@@ -200,11 +235,14 @@ issue_cert()
     local user_cleanup
     local ret
     local domain_dir
+    local staging=
+    local HOOK=
 
     config_get_bool enabled "$section" enabled 0
     config_get_bool use_staging "$section" use_staging
     config_get_bool update_uhttpd "$section" update_uhttpd
     config_get_bool update_nginx "$section" update_nginx
+    config_get_bool update_haproxy "$section" update_haproxy
     config_get calias "$section" calias
     config_get dalias "$section" dalias
     config_get domains "$section" domains
@@ -216,11 +254,17 @@ issue_cert()
 
     UPDATE_NGINX=$update_nginx
     UPDATE_UHTTPD=$update_uhttpd
+    UPDATE_HAPROXY=$update_haproxy
     USER_CLEANUP=$user_cleanup
 
     [ "$enabled" -eq "1" ] || return
 
-    [ "$DEBUG" -eq "1" ] && acme_args="$acme_args --debug"
+    if [ "$APP" = "uacme" ]; then
+       [ "$DEBUG" -eq "1" ] && acme_args="--verbose --verbose"
+    elif [ "$APP" = "acme" ]; then
+       [ "$DEBUG" -eq "1" ] && acme_args="$acme_args --debug"
+    fi
+    [ "$use_staging" -eq "1" ] && STATE_DIR="$STAGING_STATE_DIR" && staging="--staging"
 
     set -- $domains
     main_domain=$1
@@ -239,8 +283,24 @@ issue_cert()
         domain_dir="$STATE_DIR/${main_domain}"
     fi
 
-    log "Running ACME for $main_domain"
+    log "Running $APP for $main_domain"
 
+    if [ "$APP" = "uacme" ]; then
+       if [ ! -f  "${domain_dir}/private/key.pem" ]; then
+           log "Create a new ACME account with email $ACCOUNT_EMAIL use staging=$use_staging"
+           run_acme $acme_args --confdir "{domain_dir}" $staging --yes new $ACCOUNT_EMAIL
+       fi
+
+       if [ -f "${domain_dir}/$main_domain/cert.pem" ]; then
+           log "Found previous cert config, use staging=$use_staging. Issuing renew."
+           [ "$keylength_ecc" -eq "1" ] && acme_args="$acme_args --type=EC"
+           export CHALLENGE_PATH="$webroot"
+           run_acme $acme_args --confdir "{domain_dir}" $staging --never-create issue $domains --hook=$HPROGRAM && ret=0 || ret=1
+           post_checks
+           return $ret
+       fi
+    fi
+    if [ "$APP" = "acme" ]; then
     handle_credentials() {
         local credential="$1"
         eval export $credential
@@ -260,14 +320,17 @@ issue_cert()
             return $ret
         fi
     fi
-
+    fi
 
     acme_args="$acme_args $(for d in $domains; do echo -n "-d $d "; done)"
     acme_args="$acme_args --keylength $keylength"
+    if [ "$APP" = "acme" ]; then
     [ -n "$ACCOUNT_EMAIL" ] && acme_args="$acme_args --accountemail $ACCOUNT_EMAIL"
     [ "$use_staging" -eq "1" ] && acme_args="$acme_args --staging"
+    fi
 
     if [ -n "$dns" ]; then
+       if [ "$APP" = "acme" ]; then
         log "Using dns mode"
         acme_args="$acme_args --dns $dns"
         if [ -n "$dalias" ]; then
@@ -280,9 +343,19 @@ issue_cert()
             log "Using challenge alias for dns mode"
             acme_args="$acme_args --challenge-alias $calias"
         fi
+       else
+           log "Using dns mode, dns-01 is not wrapped yet"
+           return 1
+#          uacme_args="$uacme_args --dns $dns"
+       fi
     elif [ -z "$webroot" ]; then
+       if [ "$APP" = "acme" ]; then
         log "Using standalone mode"
         acme_args="$acme_args --standalone --listen-v6"
+       else
+           log "Standalone not supported by $APP"
+           return 1
+       fi
     else
         if [ ! -d "$webroot" ]; then
             err "$main_domain: Webroot dir '$webroot' does not exist!"
@@ -290,10 +363,20 @@ issue_cert()
             return 1
         fi
         log "Using webroot dir: $webroot"
-        acme_args="$acme_args --webroot $webroot"
+        if [ "$APP" = "uacme" ]; then
+            export CHALLENGE_PATH="$webroot"
+        else
+            acme_args="$acme_args --webroot $webroot"
+        fi
     fi
 
-    if ! run_acme --home "$STATE_DIR" --issue $acme_args; then
+    if [ "$APP" = "uacme" ]; then
+        workdir="--confdir"
+        HOOK="--hook=$HPROGRAM"
+    else
+        workdir="--home"
+    fi
+    if ! run_acme $workdir "$STATE_DIR" $staging --issue $acme_args $HOOK; then
         failed_dir="${domain_dir}.failed-$(date +%s)"
         err "Issuing cert for $main_domain failed. Moving state to $failed_dir"
         [ -d "$domain_dir" ] && mv "$domain_dir" "$failed_dir"
@@ -306,15 +389,33 @@ issue_cert()
     fi
 
     if [ -e /etc/init.d/uhttpd ] && [ "$update_uhttpd" -eq "1" ]; then
-        uci set uhttpd.main.key="${domain_dir}/${main_domain}.key"
-        uci set uhttpd.main.cert="${domain_dir}/fullchain.cer"
+        if [ "$APP" = "uacme" ]; then
+            uci set uhttpd.main.key="${domain_dir}/private/${main_domain}.key"
+            uci set uhttpd.main.cert="${domain_dir}/fullchain.cer"
+        else
+            uci set uhttpd.main.key="${domain_dir}/${main_domain}.key"
+            uci set uhttpd.main.cert="${domain_dir}/fullchain.cer"
+        fi
         # commit and reload is in post_checks
     fi
 
     if [ -e /etc/init.d/nginx ] && [ "$update_nginx" -eq "1" ]; then
+        if [ "$APP" = "uacme" ]; then
+        sed -i "s#ssl_certificate\ .*#ssl_certificate ${domain_dir}/fullchain.cer;#g" /etc/nginx/nginx.conf
+        sed -i "s#ssl_certificate_key\ .*#ssl_certificate_key ${domain_dir}/private/${main_domain}.key;#g" /etc/nginx/nginx.conf
+        else
         sed -i "s#ssl_certificate\ .*#ssl_certificate ${domain_dir}/fullchain.cer;#g" /etc/nginx/nginx.conf
         sed -i "s#ssl_certificate_key\ .*#ssl_certificate_key ${domain_dir}/${main_domain}.key;#g" /etc/nginx/nginx.conf
+        fi
         # commit and reload is in post_checks
+    fi
+
+    if [ -e /etc/init.d/haproxy ] && [ "$update_haproxy" -eq 1 ]; then
+        if [ "$APP" = "uacme" ]; then
+            cat ${domain_dir}/${main_domain}/cert.pem ${domain_dir}/private/${main_domain}/key.pem > ${domain_dir}/${main_domain}/full_haproxy.$
+        else
+            cat ${domain_dir}/${main_domain}/fullchain.cer ${domain_dir}/${main_domain}/${main_domain}.key > ${domain_dir}/${main_domain}/full_$
+        fi
     fi
 
     post_checks
@@ -325,6 +426,7 @@ load_vars()
     local section="$1"
 
     STATE_DIR=$(config_get "$section" state_dir)
+    STAGING_STATE_DIR=$STATE_DIR/staging
     ACCOUNT_EMAIL=$(config_get "$section" account_email)
     DEBUG=$(config_get "$section" debug)
 }
@@ -342,6 +444,7 @@ if [ -z "$STATE_DIR" ] || [ -z "$ACCOUNT_EMAIL" ]; then
 fi
 
 [ -d "$STATE_DIR" ] || mkdir -p "$STATE_DIR"
+[ -d "$STAGING_STATE_DIR" ] || mkdir -p "$STAGING_STATE_DIR"
 
 trap err_out HUP TERM
 trap int_out INT
