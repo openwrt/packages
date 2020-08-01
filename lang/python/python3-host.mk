81c16658413ea6c4b01355a351823e667214e974
Rosen Penev <rosenp@gmail.com>
2020-08-01 13:59:57 -0700

    python3: fix host compilation with clang
    Matched rpath parameter with Makefile.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    (cherry-picked from commit b40c40151c2ac3dd11d908a0548ac97e6b5c8455)
    Signed-off-by: Gilles Depeyrot <g.depeyrot@transacom.fr>

--- lang/python/python3-host.mk
+++ lang/python/python3-host.mk
@@ -50,7 +50,7 @@ define host_python3_settings
 	LDSHARED="$(HOSTCC) -shared" \
 	CFLAGS="$(HOST_CFLAGS)" \
 	CPPFLAGS="$(HOST_CPPFLAGS) -I$(HOST_PYTHON3_INC_DIR)" \
-	LDFLAGS="$(HOST_LDFLAGS) -lpython$(PYTHON3_VERSION) -Wl$(comma)-rpath=$(STAGING_DIR_HOSTPKG)/lib" \
+	LDFLAGS="$(HOST_LDFLAGS) -lpython$(PYTHON3_VERSION) -Wl$(comma)-rpath$(comma)$(STAGING_DIR_HOSTPKG)/lib" \
 	_PYTHON_HOST_PLATFORM=linux2
 endef
 
