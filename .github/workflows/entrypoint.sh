da111fd095a3bdb1d0cfb521c0c835cd8a144e88
Paul Spooren <mail@aparcar.org>
2020-09-05 23:14:49 -1000

    CI: add runtime tests for packages
    Additional to manual runtime tests this CI addition runs a custom test
    script per package. Ideally this lowers the errors of package bumps,
    something which is time consuming when done manually for multiple
    architectures.
    
    This CI uses the official OpenWrt containers and tries to install and
    run compiled packages. The run depends on the content of `test.sh`,
    which is an `ash` script. It's called with the *packge name* and
    *package version* as arguments. This allows different behaviour if
    a single package generates multiple IPK files. The version is usable for
    the most trivial runtime check, e.g. `tmux -V | grep "$2"`.
    
    The current approach uses the qus project[1] which contains multiple
    QEMU binaries to run various architectures.
    
    [1]: https://github.com/dbhi/qus
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

---
+++ .github/workflows/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+mkdir -p /var/lock/
+
+opkg update
+
+for PKG in /ci/*.ipk; do
+	tar -xzOf "$PKG" ./control.tar.gz | tar xzf - ./control 
+	PKG_NAME=$(sed -ne 's#^Package: \(.*\)$#\1#p' ./control)
+	PKG_VERSION=$(sed -ne 's#^Version: \(.*\)$#\1#p' ./control)
+
+	echo "Testing package $PKG_NAME ($PKG_VERSION)"
+
+	opkg install "$PKG"
+
+	TEST_SCRIPT=$(find /ci/ -name "$PKG_NAME" -type d)/test.sh
+	if [ -f "$TEST_SCRIPT" ]; then
+		echo "Use package specific test.sh"
+		if sh "$TEST_SCRIPT" "$PKG_NAME" "$PKG_VERSION"; then
+			echo "Test successfull"
+		else
+			echo "Test failed"
+			exit 1
+		fi
+	else
+		echo "No test.sh script available"
+	fi
+
+	opkg remove "$PKG_NAME"
+done
