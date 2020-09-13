d0a74afad1d0980fdfe54cfb3903fc252cc3c476
Karel Kočí <cynerd@email.cz>
2020-09-13 11:28:02 +0200

    syslog-ng: tweak shell code of network_localhost little bit
    We can get rid of pipe with -n flag to sysctl.
    
    Signed-off-by: Karel Kočí <cynerd@email.cz>
    (cherry picked from commit 7b7d074c8d18ce7122d50ec207fd71a3bd88c47b)

--- admin/syslog-ng/files/scl/network_localhost/detect.sh
+++ admin/syslog-ng/files/scl/network_localhost/detect.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ "$(sysctl net.ipv6.conf.lo.disable_ipv6 | cut -d' ' -f 3)" = "0" ]; then
+if [ "$(sysctl -n net.ipv6.conf.lo.disable_ipv6)" = "0" ]; then
 	echo 'network(ip("::1") port(514) transport(udp) ip-protocol(6) )'
 else
 	echo 'network(ip("127.0.0.1") port(514) transport(udp) ip-protocol(4) )'
