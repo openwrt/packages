f7cb8b20e118834f9a6c5be298737829f90ea68b
Nick Hainke <vincent@systemli.org>
2021-03-13 20:16:45 +0100

    wg-installer: fix get_usage function
    The get_usage function always returns 0. The shell syntax was wrong.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>

--- net/wg-installer/wg-server/lib/wg_functions.sh
+++ net/wg-installer/wg-server/lib/wg_functions.sh
@@ -2,7 +2,7 @@
 . /usr/share/wginstaller/wg.sh
 
 wg_get_usage () {
-	num_interfaces = $(wg show interfaces | wc -w)
+	num_interfaces=$(wg show interfaces | wc -w)
 	json_init
 	json_add_int "num_interfaces" $num_interfaces
 	echo $(json_dump)
