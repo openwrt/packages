57a77386de7dda12f60bf3021efcde7f059833c8
Jo-Philipp Wich <jo@mein.io>
2021-04-09 18:52:15 +0200

    bonding: accept list of slaves in uci list notation
    Rework the bonding.sh protocol handler to accept slave interface names
    encoded in uci list notation. Also replace ifconfig up/down with ip
    link calls while we're at it.
    
    Fixes: #11455
    Fixes: https://github.com/openwrt/luci/issues/4473
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- net/bonding/files/lib/netifd/proto/bonding.sh
+++ net/bonding/files/lib/netifd/proto/bonding.sh
@@ -37,7 +37,7 @@ proto_bonding_init_config() {
 
 	proto_config_add_string "bonding_policy"
 	proto_config_add_string "link_monitoring"
-	proto_config_add_string "slaves"
+	proto_config_add_array "slaves"
 	proto_config_add_string "all_slaves_active"
 
 	proto_config_add_string "min_links"
@@ -64,6 +64,28 @@ proto_bonding_init_config() {
 	proto_config_add_string "downdelay"
 	proto_config_add_string "updelay"
 	proto_config_add_string "use_carrier"
+}
+
+proto_bonding_add_slave() {
+	local slave=$1
+	local idx=$2
+	local cfg=$3
+	local link=$4
+
+	if [ ! -e "/sys/class/net/$slave" ]; then
+		echo "$cfg" "No slave device $slave found"
+		proto_notify_error "$cfg" NO_DEVICE
+		proto_block_restart "$cfg"
+		return
+	fi
+
+	ip link set dev "$slave" down
+
+	sleep 1
+
+	echo "+$slave" > /sys/class/net/"$link"/bonding/slaves
+
+	ip link set dev "$slave" up
 }
 
 proto_bonding_setup() {
@@ -157,24 +179,7 @@ proto_bonding_setup() {
 	# Add slaves to bonding interface
 	local slaves
 	json_get_vars slaves
-
-	for slave in $slaves; do
-
-		if [ "$(cat /proc/net/dev |grep "$slave")" == "" ]; then
-			echo "$cfg" "No slave device $slave found"
-			proto_notify_error "$cfg" NO_DEVICE
-			proto_block_restart "$cfg"
-			return
-		fi
-
-		ifconfig "$slave" down
-
-		sleep 1
-
-		echo "+$slave" > /sys/class/net/"$link"/bonding/slaves
-
-		ifconfig "$slave" up
-	done
+	json_for_each_item proto_bonding_add_slave slaves "$cfg" "$link"
 
 	[ -n "$all_slaves_active" ] && echo "$all_slaves_active" > /sys/class/net/"$link"/bonding/all_slaves_active
 
