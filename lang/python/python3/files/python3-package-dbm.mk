78f6c2c5ad2fd3de8a33a1cddb02204177cf60ad
Alexandru Ardelean <ardeleanalex@gmail.com>
2021-05-19 14:46:06 +0300

    python3: use gdbm for Berkley dbm
    Python works with GNU DBM and with Berkley DBM.
    Berkley DBM has been under Oracle for some time.
    
    And it's not clear how many Python users actually use DBM.
    
    In the packages feed, we have both libdb47 (which is now under Oracle) and
    GNU DBM. The GNU DBM has a compatibility layer for Berkley DBM.
    There are newer versions than libdb47, but it's probably not worth having
    them yet. The libbd47 tarball is ~40+ MB. Odds are newer versions will be
    bigger and more bloated.
    
    This change merges the old `python3-gdbm` package into the `python3-dbm`
    package, since they are effectively using the same underlying library now,
    i.e. gdbm.
    
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

--- lang/python/python3/files/python3-package-dbm.mk
+++ lang/python/python3/files/python3-package-dbm.mk
@@ -8,10 +8,11 @@
 define Package/python3-dbm
 $(call Package/python3/Default)
   TITLE:=Python $(PYTHON3_VERSION) dbm module
-  DEPENDS:=+python3-light +libdb47
+  DEPENDS:=+python3-light +libgdbm
 endef
 
 $(eval $(call Py3BasePackage,python3-dbm, \
 	/usr/lib/python$(PYTHON3_VERSION)/dbm \
 	/usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_dbm.$(PYTHON3_SO_SUFFIX) \
+	/usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_gdbm.$(PYTHON3_SO_SUFFIX) \
 ))
