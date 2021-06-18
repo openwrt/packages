fcc41104e4f17b6545c86f2dddd06b7b9256504c
Ivan Pavlov <AuthorReflex@gmail.com>
2021-06-18 10:10:57 +0300

    openvpn: update to 2.5.3
    Fix a possible security issue with OpenSSL config autoloading on Windows (CVE-2021-3606).
    Include a number of small improvements and bug fixes.
    
    remove upstreamed: 115-fix-mbedtls-without-renegotiation.patch
    
    Signed-off-by: Ivan Pavlov <AuthorReflex@gmail.com>

--- net/openvpn/test.sh
+++ net/openvpn/test.sh
@@ -4,10 +4,7 @@ case "$1" in
 	"openvpn-mbedtls")
 		openvpn --version | grep "$2.*SSL (mbed TLS)"
 		;;
-	"openvpn-openssl")
-		openvpn --version | grep "$2.*SSL (OpenSSL)"
-		;;
-	"openvpn-wolfssl")
+	"openvpn-openssl"|"openvpn-wolfssl")
 		openvpn --version | grep "$2.*SSL (OpenSSL)"
 		;;
 esac
