989aecf2bb27b285ca2e3f828220ebf81e7a05aa
Jonathan Pagel <jonny_tischbein@systemli.org>
2021-07-28 09:33:15 +0200

    telegraf: Add package for telegraf
    Telegraf is a plugin-driven agent for collecting and sending metrics
    and events. It supports various inputs (including prometheus
    endpoints) and is able to send data into InfluxDB.
    https://www.influxdata.com/time-series-platform/telegraf/
    
    Signed-off-by: Jonathan Pagel <jonny_tischbein@systemli.org>
    (cherry picked from commit 0781a15c932187788a69d9a3eb005062fb8eaf7b)
    Signed-off-by: Jonathan Pagel <jonny_tischbein@systemli.org>

---
+++ utils/telegraf/variant-small/plugins/outputs/all/all.go
@@ -0,0 +1,12 @@
+package all
+
+import (
+	//Blank imports for plugins to register themselves
+	_ "github.com/influxdata/telegraf/plugins/outputs/exec"
+	_ "github.com/influxdata/telegraf/plugins/outputs/file"
+	_ "github.com/influxdata/telegraf/plugins/outputs/graphite"
+	_ "github.com/influxdata/telegraf/plugins/outputs/http"
+	_ "github.com/influxdata/telegraf/plugins/outputs/influxdb"
+	_ "github.com/influxdata/telegraf/plugins/outputs/prometheus_client"
+	_ "github.com/influxdata/telegraf/plugins/outputs/syslog"
+)
