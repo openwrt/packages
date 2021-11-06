bae4bd6935b5fd472df226dcd76ef09e1c45c4af
Oskari Rauta <oskari.rauta@gmail.com>
2021-11-06 22:05:46 +0200

    parted: update Makefile
     - updated Makefile fields license (GPL3) and COPYING as license file
     - fix config directive
     - split package to shared library and application (another package, fatresize has dependency to libparted)
    
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- utils/parted/test.sh
+++ utils/parted/test.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 case "$1" in
-	"partprobe")
-		test $(/sbin/partprobe --version | grep '^Copyright' | wc -l) -gt 0
-		;;
 	"parted")
 		test $(/sbin/parted --version | grep '^Copyright' | wc -l) -gt 0
 		;;
