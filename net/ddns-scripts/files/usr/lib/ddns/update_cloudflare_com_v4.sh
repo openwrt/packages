939fd38bc2be8111d3641fd181938e62ee2406d0
wangkezun <admin@wkz.io>
2022-01-15 01:59:50 +0800

    fix $zone_id empty error

--- net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
+++ net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
@@ -134,7 +134,7 @@ else
 fi
 __PRGBASE="$__PRGBASE --header 'Content-Type: application/json' "
 
-if [ -z "$zone_id" ]; then
+if [ ! -z "$zone_id" ]; then
 	__ZONEID="$zone_id"
 else
 	# read zone id for registered domain.TLD
