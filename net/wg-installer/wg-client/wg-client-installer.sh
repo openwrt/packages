a37f2741ed6324f1e9580584582d2777f69a4a1c
Nick Hainke <vincent@systemli.org>
2022-01-19 18:40:16 +0100

    wg-installer: rework code
    Use shellcheck to rework the code. Use "export" to return variables from
    a function call. Further, fix typos.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>

--- net/wg-installer/wg-client/wg-client-installer.sh
+++ net/wg-installer/wg-client/wg-client-installer.sh
@@ -12,8 +12,8 @@ while true; do
 		echo "help"
 		shift 1
 		;;
-	-i | --ip)
-		IP=$2
+	--endpoint)
+		ENDPOINT=$2
 		shift 2
 		;;
 	--user)
@@ -22,10 +22,6 @@ while true; do
 		;;
 	--password)
 		PASSWORD=$2
-		shift 2
-		;;
-	--bandwidth)
-		BANDWIDTH=$2
 		shift 2
 		;;
 	--mtu)
@@ -45,86 +41,56 @@ while true; do
 	esac
 done
 
-escape_ip () {
-	local gw_ip=$1
-
-	# ipv4 processing
-	ret_ip=$(echo $gw_ip | tr '.' '_')
-
-	# ipv6 processing
-	ret_ip=$(echo $ret_ip | tr ':' '_')
-	ret_ip=$(echo $ret_ip | cut -d '[' -f 2)
-	ret_ip=$(echo $ret_ip | cut -d ']' -f 1)
-
-	echo $ret_ip
-}
-
 register_client_interface () {
-	local privkey=$1
-	local pubkey=$2
-	local gw_ip=$3
-	local gw_port=$4
-	local endpoint=$5
-	local mtu_client=$6
+	local endpoint=$2
+	local mtu_client=$3
+	local privkey=$4
+	local pubkey=$5
+	local gw_port=$6
 
 	port_start=$(uci get wgclient.@client[0].port_start)
 	port_end=$(uci get wgclient.@client[0].port_end)
-	base_prefix=$(uci get wgclient.@client[0].base_prefix)
 
-	port=$(next_port $port_start $port_end)
+	port=$(next_port "$port_start" "$port_end")
 	ifname="wg_$port"
 
-	offset=$(($port - $port_start))
-	client_ip=$(owipcalc $base_prefix add $offset next 128)
-	client_ip_assign="${client_ip}/128"
+	ip link add dev "$ifname" type wireguard
+	ip -6 addr add dev "$ifname" fe80::2/64
+	wg set "$ifname" listen-port "$port" private-key "$privkey" peer "$pubkey" allowed-ips 0.0.0.0/0,::0/0 endpoint "${endpoint}:${gw_port}"
+	ip link set up dev "$ifname"
+	ip link set mtu "$mtu_client" dev "$ifname"
 
-	echo "Installing Interface With:"
-	echo "Endpoint ${endpoint}"
-	echo "Client IP ${client_ip}"
-	echo "Port ${port}"
-	echo "Pubkey ${pubkey}"
-
-	ip link add dev $ifname type wireguard
-
-	ip -6 addr add dev $ifname $client_ip
-	ip -6 addr add dev $ifname fe80::2/64
-	wg set $ifname listen-port $port private-key $privkey peer $pubkey allowed-ips 0.0.0.0/0,::0/0 endpoint "${endpoint}:${gw_port}"
-	ip link set up dev $ifname
-	ip link set mtu $mtu_client dev $ifname # configure mtu here!
+	export "$1=$ifname"
 }
 
 # rpc login
-token="$(request_token $IP $USER $PASSWORD)"
-if [ $? != 0 ]; then
-	echo "failed to register token"
+token="$(request_token "$ENDPOINT" "$USER" "$PASSWORD")"
+if [ $? -ne 0 ]; then
+	logger -t "wg-client-installer" "Failed to register token!"
 	exit 1
 fi
 
 # now call procedure
 case $CMD in
 "get_usage")
-	wg_rpcd_get_usage $token $IP
+	wg_rpcd_get_usage "$token" "$ENDPOINT"
 	;;
 "register")
 
-	if [ ! -z "$WG_KEY_FILE" ]; then
-		wg_priv_key_file=$WG_KEY_FILE
-		wg_pub_key=$(wg pubkey < $WG_KEY_FILE)
-	else
-		wg_priv_key_file=$(uci get wgclient.@client[0].wg_key)
-		wg_pub_key=$(cat $(uci get wgclient.@client[0].wg_pub))
+	if [ -n "$WG_KEY_FILE" ]; then
+		wg_priv_key_file="$WG_KEY_FILE"
+		wg_pub_key=$(wg pubkey < "$WG_KEY_FILE")
 	fi
 
-	register_output=$(wg_rpcd_register $token $IP $BANDWIDTH $WG_MTU $wg_pub_key)
-	if [ $? != 0 ]; then
-		echo "Failed to Register!"
+	wg_rpcd_register __gw_pubkey __gw_ipv4 __gw_ipv6 __gw_port "$token" "$ENDPOINT" "$WG_MTU" "$wg_pub_key"
+	if [ $? -ne 0 ]; then
+		logger -t "wg-client-installer" "Failed to Register!"
 		exit 1
 	fi
-	pubkey=$(echo $register_output | awk '{print $2}')
-	ip_addr=$(echo $register_output | awk '{print $4}')
-	port=$(echo $register_output | awk '{print $6}')
-	client_ip=$(echo $register_output | awk '{print $8}')
-	register_client_interface $wg_priv_key_file $pubkey $ip_addr $port $IP $WG_MTU
+
+	register_client_interface __interface "$ENDPOINT" "$WG_MTU" "$wg_priv_key_file" "$__gw_pubkey" "$__gw_port"
+	logger -t "wg-client-installer" "Registered: $__interface"
+	echo $__interface
 	;;
-*) echo "Usage: wg-client-installer [cmd] --ip [2001::1] --user wginstaller --password wginstaller" ;;
+*) echo "Usage: wg-client-installer [cmd] --endpoint [2001::1] --mtu 1500 --user wginstaller --password wginstaller" ;;
 esac
--- net/wg-installer/wg-server/lib/wg_functions.sh
+++ net/wg-installer/wg-server/lib/wg_functions.sh
@@ -1,13 +1,15 @@
+#!/bin/sh
+
 . /usr/share/libubox/jshn.sh
 . /usr/share/wginstaller/wg.sh
 
 wg_timeout () {
 	local int=$1
 
-	handshake=$(wg show $int latest-handshakes | awk '{print $2}')
+	handshake=$(wg show "$int" latest-handshakes | awk '{print $2}')
 	timeout=$(uci get wgserver.@server[0].timeout_handshake)
 
-	if [ $handshake -ge $timeout ]; then
+	if [ "$handshake" -ge "$timeout" ]; then
 		echo "1"
 	else
 		echo "0"
@@ -16,23 +18,23 @@ wg_timeout () {
 
 wg_check_interface () {
 	local int=$1
-	if [ $(wg_timeout $int) -eq "1" ]; then
-		ip link del dev $int
+	if [ "$(wg_timeout "$int")" -eq "1" ]; then
+		ip link del dev "$int"
 	fi
 }
 
 wg_check_interfaces () {
 	wg_interfaces=$(wg show interfaces)
 	for interface in $wg_interfaces; do
-		wg_check_interface $interface
+		wg_check_interface "$interface"
 	done
 }
 
 wg_get_usage () {
 	num_interfaces=$(wg show interfaces | wc -w)
 	json_init
-	json_add_int "num_interfaces" $num_interfaces
-	echo $(json_dump)
+	json_add_int "num_interfaces" "$num_interfaces"
+	json_dump
 }
 
 wg_register () {
@@ -40,52 +42,52 @@ wg_register () {
 	local mtu=$2
 	local public_key=$3
 
-	base_prefix=$(uci get wgserver.@server[0].base_prefix)
+	base_prefix_ipv6=$(uci get wgserver.@server[0].base_prefix_ipv6)
 	port_start=$(uci get wgserver.@server[0].port_start)
 	port_end=$(uci get wgserver.@server[0].port_end)
 
-	port=$(next_port $port_start $port_end)
+	port=$(next_port "$port_start" "$port_end")
 	ifname="wg_$port"
 
-	offset=$(($port - $port_start))
-	gw_ip=$(owipcalc $base_prefix add $offset next 128) # gateway ip
-	gw_ip_assign="${gw_ip}/128"
+	offset=$((port - port_start))
+	gw_ipv6=$(owipcalc "$base_prefix_ipv6" add "$offset" next 128) # gateway ip
+	gw_ipv6_assign="${gw_ipv6}/128"
 
 	gw_key=$(uci get wgserver.@server[0].wg_key)
 	gw_pub=$(uci get wgserver.@server[0].wg_pub)
 
-	if [ $(uci get wgserver.@server[0].wg_tmp_key) -eq 1 ]; then
+	if [ "$(uci get wgserver.@server[0].wg_tmp_key)" -eq 1 ]; then
 		[ -d "/tmp/run/wgserver" ] || mkdir -p /tmp/run/wgserver
 		gw_key="/tmp/run/wgserver/${ifname}.key"
 		gw_pub="/tmp/run/wgserver/${ifname}.pub"
-		wg genkey | tee $gw_key | wg pubkey > $gw_pub
+		wg genkey | tee "$gw_key" | wg pubkey > "$gw_pub"
 	fi
-	wg_server_pubkey=$(cat $gw_pub)
+	wg_server_pubkey=$(cat "$gw_pub")
 
 	# create wg tunnel
-	ip link add dev $ifname type wireguard
-	wg set $ifname listen-port $port private-key $gw_key peer $public_key allowed-ips 0.0.0.0/0,::0/0
-	ip -6 addr add $gw_ip_assign dev $ifname
-	ip -6 addr add fe80::1/64 dev $ifname
+	ip link add dev "$ifname" type wireguard
+	wg set "$ifname" listen-port "$port" private-key "$gw_key" peer "$public_key" allowed-ips 0.0.0.0/0,::0/0
+	ip -6 addr add "$gw_ipv6_assign" dev "$ifname"
+	ip -6 addr add fe80::1/64 dev "$ifname"
 
-	v4prefix=$(uci get wgserver.@server[0].base_v4prefix)
+	base_prefix_ipv4=$(uci get wgserver.@server[0].base_prefix_ipv4)
 	if [ $? -eq 0 ]; then
-		gw_ipv4=$(owipcalc $v4prefix add $offset next 32) # gateway ip
+		gw_ipv4=$(owipcalc "$base_prefix_ipv4" add "$offset" next 32) # gateway ip
 		gw_ipv4_assign="${gw_ipv4}/32"
-		ip addr add $gw_ipv4_assign broadcast 255.255.255.255 dev $ifname
+		ip addr add "$gw_ipv4_assign" broadcast 255.255.255.255 dev "$ifname"
 	fi
 
-	ip link set up dev $ifname
-	ip link set mtu $mtu dev $ifname
+	ip link set up dev "$ifname"
+	ip link set mtu "$mtu" dev "$ifname"
 
 	# craft return address
 	json_init
-	json_add_string "pubkey" $wg_server_pubkey
-	json_add_string "gw_ip" $gw_ip_assign
+	json_add_string "gw_pubkey" "$wg_server_pubkey"
 	if test -n "${gw_ipv4_assign-}"; then
-		json_add_string "gw_ipv4" $gw_ipv4_assign
+		json_add_string "gw_ipv4" "$gw_ipv4_assign"
 	fi
-	json_add_int "port" $port
+	json_add_string "gw_ipv6" "$gw_ipv6_assign"
+	json_add_int "gw_port" "$port"
 
-	echo $(json_dump)
+	json_dump
 }
