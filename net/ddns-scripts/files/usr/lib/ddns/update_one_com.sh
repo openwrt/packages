5dabdaeb53a7138de3a943d047ff70b107218248
Lugico <main@lugico.de>
2022-01-19 14:55:07 +0100

    ddns-scripts: one.com review
    Updated update_one_com.sh for more reliability and replaced space 
    indentation with tabs
    Updated Makefile
    
    Signed-off-by: Lugico <main@lugico.de>

--- net/ddns-scripts/files/usr/lib/ddns/update_one_com.sh
+++ net/ddns-scripts/files/usr/lib/ddns/update_one_com.sh
@@ -17,96 +17,109 @@ write_log 0 "one.com ddns script started"
 local __SUBDOMAIN __MAINDOMAIN __LOGINURL __RECORDID
 local __TTL=3600
 
+COOKIEJAR=$(mktemp /tmp/one_com_cookiejar.XXXXXX) || exit 1
+
 __SUBDOMAIN=$(echo $domain | sed -e 's/[^\.]*\.[^\.]*$//' -e 's/\.$//' )
-__MAINDOMAIN=$(echo $domain | sed -e "s/$__SUBDOMAIN\.//" )
+__MAINDOMAIN=$(echo $domain | sed -e "s/${__SUBDOMAIN}\.//" )
 
 
 # LOGGING IN
+# GET LOGIN POST URL FROM FORM
 __LOGINURL=$( $CURL \
-  -RsSL \
-  --stderr $ERRFILE \
-  -c /tmp/one_com_cookiejar \
-  "https://www.one.com/admin/" \
-  | grep 'Login-form login autofill' \
-  | sed -e 's/.*action="//' -e 's/".*//' -e 's/\&amp;/\&/g' \
+	-RsSL \
+	--stderr $ERRFILE \
+	-c $COOKIEJAR \
+	"https://www.one.com/admin/" \
+	| grep 'Login-form login autofill' \
+	| sed -e 's/.*action="//' -e 's/".*//' -e 's/\&amp;/\&/g' \
 )
 
-if ! [ "$( $CURL \
-  -RsSL \
-  --stderr $ERRFILE \
-  -c /tmp/one_com_cookiejar \
-  -b /tmp/one_com_cookiejar \
-  "$__LOGINURL" \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -X POST \
-  -d "username=$username&password=$password&credentialId=" \
-  | grep "Invalid username or password.")" == "" ] ; then
-  write_log 14 "Invalid credentials"
-  return 1
+# POST LOGIN DATA
+$CURL \
+	-RsSL \
+	--stderr $ERRFILE \
+	-c $COOKIEJAR \
+	-b $COOKIEJAR \
+	"${__LOGINURL}" \
+	-H "Content-Type: application/x-www-form-urlencoded" \
+	-X POST \
+	-d "username=${username}&password=${password}&credentialId=" \
+	| grep "Invalid username or password." > $DATFILE
+
+if [ "$?" == "0" ] ; then
+	write_log 14 "Invalid credentials"
+	return 1
 fi
 
 
 # SETTING DOMAIN
 $CURL -RsSL \
-  --stderr $ERRFILE \
-  -o $DATFILE \
-  -c /tmp/one_com_cookiejar \
-  -b /tmp/one_com_cookiejar \
-  "https://www.one.com/admin/select-admin-domain.do?domain=$__MAINDOMAIN"
+	--stderr $ERRFILE \
+	-c $COOKIEJAR \
+	-b $COOKIEJAR \
+	"https://www.one.com/admin/select-admin-domain.do?domain=${__MAINDOMAIN}" \
+	| grep "<meta name=\"one.com:active-domain\" content=\"${__MAINDOMAIN}\"/>" > $DATFILE
+
+if [ "$?" != "0" ] ; then
+	write_log 14 "Failed to select domain '${__MAINDOMAIN}'"
+	return 1
+fi
 
 
 # GETTING RECORD ID
 __RECORDID=$( $CURL \
-  -RsSL \
-  --stderr $ERRFILE \
-  -c /tmp/one_com_cookiejar \
-  -b /tmp/one_com_cookiejar \
-  "https://www.one.com/admin/api/domains/$__MAINDOMAIN/dns/custom_records" \
-  | sed 's/,/\n/g' \
-  | while read line ; do
+	-RsSL \
+	--stderr $ERRFILE \
+	-c $COOKIEJAR \
+	-b $COOKIEJAR \
+	"https://www.one.com/admin/api/domains/${__MAINDOMAIN}/dns/custom_records" \
+	| sed 's/,/\n/g' \
+	| while read line ; do
 
-    if ! [ "$(echo $line | grep '\"id\":\"' )" == "" ] ; then
-      id=$(echo $line | sed -e 's/\"id\":\"//' -e 's/"//' )
-    fi
-    if ! [ "$(echo $line | grep "\"prefix\":\"$__SUBDOMAIN\"" )" == "" ] ; then
-      if [ "$id" == "" ] ; then
-        echo "0"
-      fi
-      write_log 0 "record id: $id"
-      echo $id
-      break
-    fi
-  done \
+		if ! [ "$(echo $line | grep '\"id\":\"' )" == "" ] ; then
+			id=$(echo $line | sed -e 's/\"id\":\"//' -e 's/"//' )
+		fi
+		if ! [ "$(echo $line | grep "\"prefix\":\"${__SUBDOMAIN}\"" )" == "" ] ; then
+			if [ "$id" == "" ] ; then
+				echo "0"
+			fi
+			write_log 0 "record id: $id"
+			echo $id
+			break
+		fi
+	done \
 )
 
-if [ "$__RECORDID" == "" ] ; then
-  write_log 0 "domain record not found"
-  return 1
+if [ "${__RECORDID}" == "" ] ; then
+	write_log 0 "domain record not found"
+	return 1
 fi
 
-if [ "$__RECORDID" == "0" ] ; then
-  write_log 14 "no id for domain record found"
-  return 1
+if [ "${__RECORDID}" == "0" ] ; then
+	write_log 14 "no id for domain record found"
+	return 1
 fi
 
 
 
 # SENDING PATCH
-if [ "$( $CURL \
-  -RsSL \
-  --stderr $ERRFILE \
-  -c /tmp/one_com_cookiejar \
-  -b /tmp/one_com_cookiejar \
-  -X PATCH \
-  -d "{\"type\":\"dns_service_records\",\"id\":\"$__RECORDID\",\"attributes\":{\"type\":\"A\",\"prefix\":\"$__SUBDOMAIN\",\"content\":\"$__IP\",\"ttl\":$__TTL}}" \
-  -H "Content-Type: application/json" \
-  "https://www.one.com/admin/api/domains/$__MAINDOMAIN/dns/custom_records/$__RECORDID" \
-  | grep "priority")" == "" ] ; then
-  echo $result
-  write_log 14 "one.com gave an unexpected response"
-  return 1
+$CURL \
+	-RsSL \
+	--stderr $ERRFILE \
+	-c $COOKIEJAR \
+	-b $COOKIEJAR \
+	-X PATCH \
+	-d "{\"type\":\"dns_service_records\",\"id\":\"${__RECORDID}\",\"attributes\":{\"type\":\"A\",\"prefix\":\"${__SUBDOMAIN}\",\"content\":\"${__IP}\",\"ttl\":${__TTL}}}" \
+	-H "Content-Type: application/json" \
+	"https://www.one.com/admin/api/domains/${__MAINDOMAIN}/dns/custom_records/${__RECORDID}" \
+	| grep "priority" > $DATFILE
+
+if [ "$?" != "0" ] ; then
+	write_log 14 "one.com gave an unexpected response"
+	return 1
 fi
 
+rm $COOKIEJAR
 write_log 0 "one.com ddns script finished without errors"
 
 return 0
