131ec7b3bd6895aa3f86f57169dd23c15f174fe2
Rui Salvaterra <rsalvaterra@gmail.com>
2022-05-17 16:29:14 +0100

    openconnect: drop the dependency on resolveip
    We have nslookup and awk, let's use them.
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

--- net/openconnect/files/openconnect.sh
+++ net/openconnect/files/openconnect.sh
@@ -68,7 +68,7 @@ proto_openconnect_setup() {
 	logger -t openconnect "initializing..."
 
 	logger -t "openconnect" "adding host dependency for $server at $config"
-	for ip in $(resolveip -t 10 "$server"); do
+	for ip in $(nslookup "$server" | awk '/^Name:/ {getline; print $2;}'); do
 		logger -t "openconnect" "adding host dependency for $ip at $config"
 		proto_add_host_dependency "$config" "$ip" "$interface"
 	done
