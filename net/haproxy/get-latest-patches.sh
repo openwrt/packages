518fe97460748d268a89eeb6d339e577b71c141d
Christian Lachner <gladiac@gmail.com>
2022-06-22 08:53:01 +0200

    haproxy: Update HAProxy to v2.6.1
    - New major LTS release (https://www.mail-archive.com/haproxy@formilux.org/msg42371.html)
    - Sadly, no QUIC/H3 support for now because the QuicTLS library - which is a fork of OpenSSL - would be needed. However, we do not have a package for that and I currently do not want to build and statically link it into the haproxy package
    - Update haproxy download URL and hash
    
    Signed-off-by: Christian Lachner <gladiac@gmail.com>

--- net/haproxy/get-latest-patches.sh
+++ net/haproxy/get-latest-patches.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CLONEURL=https://git.haproxy.org/git/haproxy-2.4.git
-BASE_TAG=v2.4.17
+CLONEURL=https://git.haproxy.org/git/haproxy-2.6.git
+BASE_TAG=v2.6.1
 TMP_REPODIR=tmprepo
 PATCHESDIR=patches
 
