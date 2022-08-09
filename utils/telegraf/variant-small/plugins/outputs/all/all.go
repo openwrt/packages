7020b3a2991e5f056058cd052e3e14d2cd29ac8a
Jonathan Pagel <jonny_tischbein@systemli.org>
2022-08-09 07:27:45 +0200

    telegraf: Add influxdb2 output plugin to package version small
    Signed-off-by: Jonathan Pagel <jonny_tischbein@systemli.org>
    (cherry picked from commit a5e96189c3dadabb3e20d8139638bbb6da6773bf)
    Signed-off-by: Jonathan Pagel <jonny_tischbein@systemli.org>

--- utils/telegraf/variant-small/plugins/outputs/all/all.go
+++ utils/telegraf/variant-small/plugins/outputs/all/all.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/outputs/graphite"
 	_ "github.com/influxdata/telegraf/plugins/outputs/http"
 	_ "github.com/influxdata/telegraf/plugins/outputs/influxdb"
+	_ "github.com/influxdata/telegraf/plugins/outputs/influxdb_v2"
 	_ "github.com/influxdata/telegraf/plugins/outputs/prometheus_client"
 	_ "github.com/influxdata/telegraf/plugins/outputs/syslog"
 )
