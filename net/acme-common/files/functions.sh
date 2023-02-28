f6ce0528ba98f39ad3195a764dced7a3b656a845
Glen Huang <i@glenhuang.com>
2023-02-28 19:19:19 +0800

    acme: merge cli into init script
    Signed-off-by: Glen Huang <i@glenhuang.com>

--- net/acme-common/files/functions.sh
+++ net/acme-common/files/functions.sh
@@ -1,7 +1,7 @@
 log() {
 	prio="$1"
 	shift
-	if [ "$prio" != debug ] || [ "$debug" = 0 ]; then
+	if [ "$prio" != debug ] || [ "$debug" = 1 ]; then
 		logger -t "$LOG_TAG" -s -p "daemon.$prio" -- "$@"
 	fi
 }
