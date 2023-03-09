ba1482b70f752322544d961043c8b45ea8b9dfc7
Luca Barbato <luca.barbato@gmail.com>
2023-03-09 08:58:16 +0100

    rust-lang: Apply suggestions from code review
    Co-authored-by: Tianling Shen <cnsztl@gmail.com>
    Signed-off-by: Luca Barbato <luca.barbato@gmail.com>

--- lang/rust/rust-package.mk
+++ lang/rust/rust-package.mk
@@ -8,8 +8,10 @@ include $(rust_mk_path)rust-host.mk
 # $(1) path to the package
 # $(2) additional arguments to cargo
 define Build/Compile/Cargo
-	cd $(PKG_BUILD_DIR) && \
-	    export PATH="$(CARGO_HOME)/bin:$(PATH)" && \
-	    CARGO_HOME=$(CARGO_HOME) TARGET_CFLAGS="$(TARGET_CFLAGS) $(RUST_CFLAGS)" TARGET_CC=$(TARGET_CC_NOCACHE) CC=cc \
-	    cargo install -v --profile stripped --target $(RUSTC_TARGET_ARCH) --root $(PKG_INSTALL_DIR) --path "$(if $(strip $(1)),$(strip $(1)),.)" $(2)
+	( \
+		cd $(PKG_BUILD_DIR) ; \
+		export PATH="$(CARGO_HOME)/bin:$(PATH)" ; \
+		CARGO_HOME=$(CARGO_HOME) TARGET_CFLAGS="$(TARGET_CFLAGS) $(RUST_CFLAGS)" TARGET_CC=$(TARGET_CC_NOCACHE) CC=cc \
+			cargo install -v --profile stripped --target $(RUSTC_TARGET_ARCH) --root $(PKG_INSTALL_DIR) --path "$(if $(strip $(1)),$(strip $(1)),.)" $(2) ; \
+	)
 endef
