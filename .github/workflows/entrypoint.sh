e9bb94840288d35b90ca43574b698c43403a7ec4
Jeffery To <jeffery.to@gmail.com>
2023-03-22 15:46:13 +0800

    CI: Add local feed for CI-built packages
    To test each package, the CI-built target package (ipk) file is
    installed, but currently the target package's dependencies are installed
    from the standard opkg feeds.
    
    There are cases when the CI-built target packages should be
    installed/tested together:
    
    * If a pull request contains several new packages that depend on each
      other, the test step will fail as the new dependencies cannot be found
      in the current packages feed.
    
    * If a pull request upgrades a source package that builds several target
      packages that depend on each other, the test step may fail due to the
      version/ABI mismatch between a newer target package and the older
      dependencies installed from the packages feed.
    
    This sets up a local feed for the CI-built packages so that dependencies
    are also installed from the same set of packages.
    
    Signed-off-by: Jeffery To <jeffery.to@gmail.com>

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -2,7 +2,12 @@
 
 # not enabling `errtrace` and `pipefail` since those are bash specific
 set -o errexit # failing commands causes script to fail
-set -o nounset # undefined variables causes script to fail 
+set -o nounset # undefined variables causes script to fail
+
+echo "src/gz packages_ci file:///ci" >> /etc/opkg/distfeeds.conf
+
+FINGERPRINT="$(usign -F -p /ci/packages_ci.pub)"
+cp /ci/packages_ci.pub "/etc/opkg/keys/$FINGERPRINT"
 
 mkdir -p /var/lock/
 
@@ -11,7 +16,7 @@ opkg update
 [ -n "${CI_HELPER:=''}" ] || CI_HELPER="/ci/.github/workflows/ci_helpers.sh"
 
 for PKG in /ci/*.ipk; do
-	tar -xzOf "$PKG" ./control.tar.gz | tar xzf - ./control 
+	tar -xzOf "$PKG" ./control.tar.gz | tar xzf - ./control
 	# package name including variant
 	PKG_NAME=$(sed -ne 's#^Package: \(.*\)$#\1#p' ./control)
 	# package version without release
