2445fe9fb25e6d9eed28aa9ad237bd250d1600d1
Jeffery To <jeffery.to@gmail.com>
2023-05-05 17:23:49 +0800

    python3: Update to 3.11.3, refresh/redo patches
    * Removed patches:
    
      * 001-enable-zlib.patch: zlib module should be enabled automatically
    
      * 007-distutils-do-not-adjust-path.patch: Not necessary since we
        process shebang lines for all scripts (in python3-package.mk)
    
      * 030-bpo-43112-detect-musl-as-a-separate-SOABI-GH-24502.patch:
        Already merged
    
    * Move configure vars from config.site back into Makefile
    
      Centralizing all build information into one file makes it easier to
      maintain
    
    * No longer set ac_cv_header_uuid_h=yes as configure should detect
      libuuid
    
    * Order configure args by enable-/disable-/with-/without-, then
      alphabetically
    
    * Set ac_cv_working_openssl_hashlib=yes for host configure to bypass the
      OpenSSL API tests with LibreSSL
    
    * Use the default Host/Compile recipe instead of picking out specific
      targets to make
    
    Signed-off-by: Jeffery To <jeffery.to@gmail.com>

--- lang/python/python3/files/python3-package-readline.mk
+++ lang/python/python3/files/python3-package-readline.mk
@@ -8,7 +8,7 @@
 define Package/python3-readline
 $(call Package/python3/Default)
   TITLE:=Python $(PYTHON3_VERSION) readline module
-  DEPENDS:=+python3-light +libreadline +libncursesw
+  DEPENDS:=+python3-light +libreadline
 endef
 
 $(eval $(call Py3BasePackage,python3-readline, \
