1187a2d4d4a5c0f3afb393a19495d20cd7589679
SpooHEX/ <108245326+Spoety40@users.noreply.github.com>
2023-05-17 00:14:54 -0400

    Delete devel directory
    Signed-off-by: SpooHEX/ <108245326+Spoety40@users.noreply.github.com>

--- devel/autoconf/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-autoconf --version | grep $2 && \
-autoheader --version | grep $2 && \
-autom4te --version | grep $2 && \
-autoreconf --version | grep $2 && \
-autoscan --version | grep $2 && \
-autoupdate --version | grep $2 && \
-ifnames --version | grep $2
