openwrt/packages

commit 7a756db002490cbfce275fc16a2134cbb13feaa0

python3: bump to version 3.10.9
Refresh patches.
Bump setuptools to 65.5.0
Bump pip to 22.3.1
  Removed patch: patches-pip/001-pep517-pyc-fix.patch
  No longer needed as per:
    https://github.com/pypa/pip/commit/fa4b2efbabffb96536c10bfb94477912a0f20e7f

Signed-off-by: Alexandru Ardelean <alex@shruggie.ro>

lang/python/python3-version.mk
@@ -8,12 +8,12 @@
 # Note: keep in sync with setuptools & pip
 PYTHON3_VERSION_MAJOR:=3
 PYTHON3_VERSION_MINOR:=10
-PYTHON3_VERSION_MICRO:=7
+PYTHON3_VERSION_MICRO:=9
 
 PYTHON3_VERSION:=$(PYTHON3_VERSION_MAJOR).$(PYTHON3_VERSION_MINOR)
 
 PYTHON3_SETUPTOOLS_PKG_RELEASE:=1
 PYTHON3_PIP_PKG_RELEASE:=1
 
-PYTHON3_SETUPTOOLS_VERSION:=63.2.0
-PYTHON3_PIP_VERSION:=22.2.2
+PYTHON3_SETUPTOOLS_VERSION:=65.5.0
+PYTHON3_PIP_VERSION:=22.3.1

lang/python/python3/Makefile
@@ -16,7 +16,7 @@ PKG_VERSION:=$(PYTHON3_VERSION).$(PYTHON3_VERSION_MICRO)
 
 PKG_SOURCE:=Python-$(PKG_VERSION).tar.xz
 PKG_SOURCE_URL:=https://www.python.org/ftp/python/$(PKG_VERSION)
-PKG_HASH:=6eed8415b7516fb2f260906db5d48dd4c06acc0cb24a7d6cc15296a604dcdc48
+PKG_HASH:=5ae03e308260164baba39921fdb4dbf8e6d03d8235a939d4582b33f0b5e46a83
 
 PKG_MAINTAINER:=Jeffery To <jeffery.to@gmail.com>
 PKG_LICENSE:=PSF-2.0

lang/python/python3/patches/028-host-python-support-ssl-with-libressl.patch
@@ -87,7 +87,7 @@
      return 0;
  }
  
-@@ -4615,8 +4644,10 @@ static PyGetSetDef context_getsetlist[]
+@@ -4619,8 +4648,10 @@ static PyGetSetDef context_getsetlist[]
                          (setter) set_minimum_version, NULL},
      {"maximum_version", (getter) get_maximum_version,
                          (setter) set_maximum_version, NULL},

lang/python/python3/patches-pip/001-pep517-pyc-fix.patch
@@ -1,20 +1,0 @@
-diff --git a/pip/_vendor/pep517/in_process/__init__.py b/pip/_vendor/pep517/in_process/__init__.py
-index c932313..a01143b 100644
---- a/pip/_vendor/pep517/in_process/__init__.py
-+++ b/pip/_vendor/pep517/in_process/__init__.py
-@@ -10,8 +10,13 @@ try:
-     import importlib.resources as resources
- 
-     def _in_proc_script_path():
--        return resources.path(__package__, '_in_process.py')
-+        if resources.is_resource(__package__, '_in_process.py'):
-+            return resources.path(__package__, '_in_process.py')
-+        return resources.path(__package__, '_in_process.pyc')
- except ImportError:
-     @contextmanager
-     def _in_proc_script_path():
--        yield pjoin(dirname(abspath(__file__)), '_in_process.py')
-+        _in_proc_script = pjoin(dirname(abspath(__file__)), '_in_process.py')
-+        if not os.path.isfile(_in_proc_script):
-+            _in_proc_script = pjoin(dirname(abspath(__file__)), '_in_process.pyc')
-+        yield _in_proc_script

lang/python/python3/patches/014-remove-platform-so-suffix.patch
@@ -1,6 +1,6 @@
 --- a/configure
 +++ b/configure
-@@ -15616,7 +15616,7 @@ $as_echo_n "checking ABIFLAGS... " >&6;
+@@ -15617,7 +15617,7 @@ $as_echo_n "checking ABIFLAGS... " >&6;
  $as_echo "$ABIFLAGS" >&6; }
  { $as_echo "$as_me:${as_lineno-$LINENO}: checking SOABI" >&5
  $as_echo_n "checking SOABI... " >&6; }
@@ -11,7 +11,7 @@
  
 --- a/configure.ac
 +++ b/configure.ac
-@@ -4824,7 +4824,7 @@ AC_SUBST(SOABI)
+@@ -4825,7 +4825,7 @@ AC_SUBST(SOABI)
  AC_MSG_CHECKING(ABIFLAGS)
  AC_MSG_RESULT($ABIFLAGS)
  AC_MSG_CHECKING(SOABI)

lang/python/python3/patches/016-adjust-config-paths.patch
@@ -10,7 +10,7 @@
          '_PYTHON_SYSCONFIGDATA_NAME',
 --- a/Makefile.pre.in
 +++ b/Makefile.pre.in
-@@ -1593,7 +1593,7 @@ libinstall:	build_all $(srcdir)/Modules/
+@@ -1594,7 +1594,7 @@ libinstall:	build_all $(srcdir)/Modules/
  			esac; \
  		done; \
  	done
@@ -19,7 +19,7 @@
  		$(DESTDIR)$(LIBDEST); \
  	$(INSTALL_DATA) $(srcdir)/LICENSE $(DESTDIR)$(LIBDEST)/LICENSE.txt
  	if test -d $(DESTDIR)$(LIBDEST)/distutils/tests; then \
-@@ -1751,7 +1751,7 @@ sharedinstall: sharedmods
+@@ -1752,7 +1752,7 @@ sharedinstall: sharedmods
  		--install-scripts=$(BINDIR) \
  		--install-platlib=$(DESTSHARED) \
  		--root=$(DESTDIR)/
@@ -39,7 +39,7 @@
      fi
  elif test "$cross_compiling" = maybe; then
      as_fn_error $? "Cross compiling required --host=HOST-TUPLE and --build=ARCH" "$LINENO" 5
-@@ -15686,7 +15686,7 @@ fi
+@@ -15687,7 +15687,7 @@ fi
  
  
  
@@ -59,7 +59,7 @@
      fi
  elif test "$cross_compiling" = maybe; then
      AC_MSG_ERROR([Cross compiling required --host=HOST-TUPLE and --build=ARCH])
-@@ -4882,7 +4882,7 @@ fi],
+@@ -4883,7 +4883,7 @@ fi],
  
  dnl define LIBPL after ABIFLAGS and LDVERSION is defined.
  AC_SUBST(PY_ENABLE_SHARED)

lang/python/python3/patches/025-choose-python-config-version.patch
@@ -1,6 +1,6 @@
 --- a/Makefile.pre.in
 +++ b/Makefile.pre.in
-@@ -1643,7 +1643,7 @@ python-config: $(srcdir)/Misc/python-con
+@@ -1644,7 +1644,7 @@ python-config: $(srcdir)/Misc/python-con
  	@ # On Darwin, always use the python version of the script, the shell
  	@ # version doesn't use the compiler customizations that are provided
  	@ # in python (_osx_support.py).

commit 6601f014e4de3ecd9b5597a72dabb62dee3a6505

python3: Update to 3.10.11, refresh/restore patches
This also restores (and updates) a patch for pip that was removed
earlier but is still necessary.

Fixes: 7a756db00249 ("python3: bump to version 3.10.9")
Signed-off-by: Jeffery To <jeffery.to@gmail.com>

lang/python/python3-version.mk
@@ -8,7 +8,7 @@
 # Note: keep in sync with setuptools & pip
 PYTHON3_VERSION_MAJOR:=3
 PYTHON3_VERSION_MINOR:=10
-PYTHON3_VERSION_MICRO:=9
+PYTHON3_VERSION_MICRO:=11
 
 PYTHON3_VERSION:=$(PYTHON3_VERSION_MAJOR).$(PYTHON3_VERSION_MINOR)
 
@@ -16,4 +16,4 @@ PYTHON3_SETUPTOOLS_PKG_RELEASE:=1
 PYTHON3_PIP_PKG_RELEASE:=1
 
 PYTHON3_SETUPTOOLS_VERSION:=65.5.0
-PYTHON3_PIP_VERSION:=22.3.1
+PYTHON3_PIP_VERSION:=23.0.1

lang/python/python3/Makefile
@@ -11,12 +11,12 @@ include $(TOPDIR)/rules.mk
 include ../python3-version.mk
 
 PKG_NAME:=python3
-PKG_RELEASE:=7
+PKG_RELEASE:=1
 PKG_VERSION:=$(PYTHON3_VERSION).$(PYTHON3_VERSION_MICRO)
 
 PKG_SOURCE:=Python-$(PKG_VERSION).tar.xz
 PKG_SOURCE_URL:=https://www.python.org/ftp/python/$(PKG_VERSION)
-PKG_HASH:=5ae03e308260164baba39921fdb4dbf8e6d03d8235a939d4582b33f0b5e46a83
+PKG_HASH:=3c3bc3048303721c904a03eb8326b631e921f11cc3be2988456a42f115daf04c
 
 PKG_MAINTAINER:=Jeffery To <jeffery.to@gmail.com>
 PKG_LICENSE:=PSF-2.0

lang/python/python3/patches-pip/001-pyproject-hooks-pyc-fix.patch
@@ -1,0 +1,20 @@
+diff --git a/pip/_vendor/pyproject_hooks/_in_process/__init__.py b/pip/_vendor/pyproject_hooks/_in_process/__init__.py
+index 917fa065b3c7..c9ec8efd53b3 100644
+--- a/pip/_vendor/pyproject_hooks/_in_process/__init__.py
++++ b/pip/_vendor/pyproject_hooks/_in_process/__init__.py
+@@ -11,8 +11,13 @@ try:
+ except AttributeError:
+     # Python 3.8 compatibility
+     def _in_proc_script_path():
+-        return resources.path(__package__, '_in_process.py')
++        if resources.is_resource(__package__, '_in_process.py'):
++            return resources.path(__package__, '_in_process.py')
++        return resources.path(__package__, '_in_process.pyc')
+ else:
+     def _in_proc_script_path():
++        if resources.files(__package__).joinpath('_in_process.py').is_file():
++            return resources.as_file(
++                resources.files(__package__).joinpath('_in_process.py'))
+         return resources.as_file(
+-            resources.files(__package__).joinpath('_in_process.py'))
++            resources.files(__package__).joinpath('_in_process.pyc'))

lang/python/python3/patches/028-host-python-support-ssl-with-libressl.patch
@@ -30,7 +30,7 @@
      /* Cached module state, also used in SSLSocket and SSLSession code. */
      _sslmodulestate *state;
  } PySSLContext;
-@@ -2358,8 +2371,14 @@ _ssl__SSLSocket_write_impl(PySSLSocket *
+@@ -2388,8 +2401,14 @@ _ssl__SSLSocket_write_impl(PySSLSocket *
  
      do {
          PySSL_BEGIN_ALLOW_THREADS
@@ -45,7 +45,7 @@
          PySSL_END_ALLOW_THREADS
          self->err = err;
  
-@@ -2510,8 +2529,14 @@ _ssl__SSLSocket_read_impl(PySSLSocket *s
+@@ -2540,8 +2559,14 @@ _ssl__SSLSocket_read_impl(PySSLSocket *s
  
      do {
          PySSL_BEGIN_ALLOW_THREADS
@@ -60,7 +60,7 @@
          PySSL_END_ALLOW_THREADS
          self->err = err;
  
-@@ -3074,8 +3099,10 @@ _ssl__SSLContext_impl(PyTypeObject *type
+@@ -3104,8 +3129,10 @@ _ssl__SSLContext_impl(PyTypeObject *type
      self->hostflags = X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS;
      self->protocol = proto_version;
      self->msg_cb = NULL;
@@ -71,7 +71,7 @@
      self->alpn_protocols = NULL;
      self->set_sni_cb = NULL;
      self->state = get_ssl_state(module);
-@@ -3199,6 +3226,7 @@ context_clear(PySSLContext *self)
+@@ -3225,6 +3252,7 @@ context_clear(PySSLContext *self)
  {
      Py_CLEAR(self->set_sni_cb);
      Py_CLEAR(self->msg_cb);
@@ -79,7 +79,7 @@
      Py_CLEAR(self->keylog_filename);
      if (self->keylog_bio != NULL) {
          PySSL_BEGIN_ALLOW_THREADS
-@@ -3206,6 +3234,7 @@ context_clear(PySSLContext *self)
+@@ -3232,6 +3260,7 @@ context_clear(PySSLContext *self)
          PySSL_END_ALLOW_THREADS
          self->keylog_bio = NULL;
      }
@@ -87,7 +87,7 @@
      return 0;
  }
  
-@@ -4619,8 +4648,10 @@ static PyGetSetDef context_getsetlist[]
+@@ -4645,8 +4674,10 @@ static PyGetSetDef context_getsetlist[]
                          (setter) set_minimum_version, NULL},
      {"maximum_version", (getter) get_maximum_version,
                          (setter) set_maximum_version, NULL},