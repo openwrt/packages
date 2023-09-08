1e235dbdc05505afc781c4a1ab776d73f609ca3d
Paco Hope <github@filter.paco.to>
2023-09-08 16:28:33 -0400

    ddns-scripts: suppress curl output for route53
    When debugging and running update_route53_v1.sh interactively, curl
    progress bars are displayed, interrupting the output of the script.
    This change causes curl to suppress that progress bar on successful
    connections, but any errors will still come out on stderr.
    
    Signed-off-by: Paco Hope <github@filter.paco.to>

--- net/ddns-scripts/files/usr/lib/ddns/update_route53_v1.sh
+++ net/ddns-scripts/files/usr/lib/ddns/update_route53_v1.sh
@@ -80,7 +80,7 @@ signature="$(sign "${signing_key}" "${sigmsg}")"
 
 authorization="AWS4-HMAC-SHA256 Credential=${AWS_ACCESS_KEY_ID}/${credential}, SignedHeaders=${signed_headers}, Signature=${signature}"
 
-ANSWER="$(flock /tmp/$(basename -s .sh "$0").lock curl \
+ANSWER="$(flock /tmp/$(basename -s .sh "$0").lock curl --silent --show-error \
     -X "POST" \
     -H "Host: route53.amazonaws.com" \
     -H "X-Amz-Date: ${fulldate}" \
