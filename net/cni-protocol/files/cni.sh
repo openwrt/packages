a4ee7915e153ea5ee23f40b7d19f9d24dae92eab
Oskari Rauta <oskari.rauta@gmail.com>
2023-10-08 17:51:50 +0300

    cni-protocol: update protocol
    Changes to protocol file and it's description.
    Works better now and restarts firewall automaticly
    when tunnel comes available. More informative/guiding
    description.
    
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- net/cni-protocol/files/cni.sh
+++ net/cni-protocol/files/cni.sh
@@ -9,33 +9,50 @@
 proto_cni_init_config() {
 	no_device=0
 	available=0
-	no_proto_task=1
-	teardown_on_l3_link_down=1
 
 	proto_config_add_string "device:device"
+	proto_config_add_int "delay"
 }
 
 proto_cni_setup() {
-
 	local cfg="$1"
-	local device ipaddr netmask broadcast route routemask routesrc
+	local iface="$2"
+	local device delay
 
-	json_get_var device device
+	json_get_vars device delay
 
-	ipaddr=$(ip -4 -o a show "$device" | awk '{ print $4 }' | cut -d '/' -f1)
-	netmask=$(ip -4 -o a show "$device" | awk '{ print $4 }' | cut -d '/' -f2)
-	broadcast=$(ip -4 -o a show "$device" | awk '{ print $6 }')
-	route=$(ip -4 -o r show dev "$device" | awk '{ print $1 }' | cut -d '/' -f1)
-	routemask=$(ip -4 -o r show dev "$device" | awk '{ print $1 }' | cut -d '/' -f2)
-	routesrc=$(ip -4 -o r show dev "$device" | awk '{ print $7 }')
+	[ -n "$device" ] || {
+		echo "No cni interface specified"
+		proto_notify_error "$cfg" NO_DEVICE
+		proto_set_available "$cfg" 0
+		return 1
+	}
+
+	[ -n "$delay" ] && sleep "$delay"
+
+	[ -L "/sys/class/net/${iface}" ] || {
+		echo "The specified interface $iface is not present"
+		proto_notify_error "$cfg" NO_DEVICE
+		proto_set_available "$cfg" 0
+		return 1
+	}
+
+	local ipaddr netmask broadcast route routemask routesrc
+
+	ipaddr=$(ip -4 -o a show "$iface" | awk '{ print $4 }' | cut -d '/' -f1)
+	netmask=$(ip -4 -o a show "$iface" | awk '{ print $4 }' | cut -d '/' -f2)
+	broadcast=$(ip -4 -o a show "$iface" | awk '{ print $6 }')
+	route=$(ip -4 -o r show dev "$iface" | awk '{ print $1 }' | cut -d '/' -f1)
+	routemask=$(ip -4 -o r show dev "$iface" | awk '{ print $1 }' | cut -d '/' -f2)
+	routesrc=$(ip -4 -o r show dev "$iface" | awk '{ print $7 }')
 
 	[ -z "$ipaddr" ] && {
-		echo "cni network $cfg does not have ip address"
+		echo "interface $iface does not have ip address"
 		proto_notify_error "$cfg" NO_IPADDRESS
 		return 1
 	}
 
-	proto_init_update "$device" 1
+	proto_init_update "$iface" 1
 	[ -n "$ipaddr" ] && proto_add_ipv4_address "$ipaddr" "$netmask" "$broadcast" ""
 	[ -n "$route" ] && proto_add_ipv4_route "$route" "$routemask" "" "$routesrc" ""
 	proto_send_update "$cfg"
@@ -43,7 +60,6 @@ proto_cni_setup() {
 
 proto_cni_teardown() {
 	local cfg="$1"
-	#proto_set_available "$cfg" 0
 	return 0
 }
 
