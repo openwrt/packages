ddd0f93637ab5e79e3fbc012d2537bfee5006f82
Jeffery To <jeffery.to@gmail.com>
2023-10-30 19:28:50 +0800

    python-charset-normalizer: Update to 3.3.2
    This also updates the test.sh script to use an assert.
    
    Signed-off-by: Jeffery To <jeffery.to@gmail.com>

--- lang/python/python-charset-normalizer/test.sh
+++ lang/python/python-charset-normalizer/test.sh
@@ -2,11 +2,12 @@
 
 [ "$1" = python3-charset-normalizer ] || exit 0
 
-python3 - << EOF
-import sys
+python3 - << 'EOF'
+
 from charset_normalizer import from_bytes
 s = 'Bсеки човек има право на образование.'
 byte_str = s.encode('cp1251')
 result = from_bytes(byte_str).best()
-sys.exit(0 if str(result) == s else 1)
+assert str(result) == s
+
 EOF
