05ceb1dc016b9090cc64325bebc8ef4012bb3f9f
Eric Fahlgren <ericfahlgren@gmail.com>
2023-11-27 08:21:43 -0800

    snort3: complete rework
      - Add many options to config file.
      - Move rules and generated snort.lua to /tmp.
      - Add script for downloading rules.
      - Add preliminary reporting capabilites.
    
    Signed-off-by: Eric Fahlgren <ericfahlgren@gmail.com>

--- net/snort3/files/homenet.lua
+++ net/snort3/files/homenet.lua
@@ -1,3 +1,4 @@
+-- Unused when using 'snort-mgr', do not modify without deep understanding.
 -- setup HOME_NET below with your IP range/ranges to protect
-HOME_NET = [[ 192.168.1.0/24 10.1.0.1/24 ]]
-EXTERNAL_NET = "!$HOME_NET"
+--HOME_NET = [[ 192.168.1.0/24 10.1.0.0/24 ]]
+--EXTERNAL_NET = "!$HOME_NET"
