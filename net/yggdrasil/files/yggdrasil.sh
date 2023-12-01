50b0bf51dea8f9d6f48029449b82ea7f3f210787
Stan Grishin <stangri@melmac.ca>
2023-12-01 02:17:37 +0000

    yggdrasil: clean up shell code
    Signed-off-by: Stan Grishin <stangri@melmac.ca>

--- net/yggdrasil/files/yggdrasil.sh
+++ net/yggdrasil/files/yggdrasil.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+# shellcheck disable=SC1091,SC3037,SC3043
 
 [ -n "$INCLUDE_ONLY" ] || {
 	. /lib/functions.sh
@@ -16,85 +16,69 @@ proto_yggdrasil_setup_peer_if_non_interface() {
 	local peer_config="$1"
 	local peer_address
 	local peer_interface
-	config_get peer_address "${peer_config}" "address"
-	config_get peer_interface "${peer_config}" "interface"
-	if [ -z ${peer_interface} ]; then
-		json_add_string "" ${peer_address}
-	fi;
+	config_get peer_address "$peer_config" "address"
+	config_get peer_interface "$peer_config" "interface"
+	[ -z "$peer_interface" ] && json_add_string "" "$peer_address"
 }
 
 proto_yggdrasil_dump_peer_interface() {
 	local peer_config="$1"
 	local peer_interface
 
-	config_get peer_interface "${peer_config}" "interface"
+	config_get peer_interface "$peer_config" "interface"
 
-	if [ ! -z ${peer_interface} ]; then
-		peer_interfaces="${peer_interfaces}\n${peer_interface}"
-	fi;
+	peer_interfaces="${peer_interfaces}${peer_interface:+\n$peer_interface}"
 }
 
 proto_yggdrasil_setup_peer_if_interface() {
 	local peer_config="$1"
 	local peer_address
 	local peer_interface
-	config_get peer_interface "${peer_config}" "interface"
-	if [ "${peer_interface}" = "${peer_interface_filter}" ]; then
-		config_get peer_address "${peer_config}" "address"
-		json_add_string "" ${peer_address}
+	config_get peer_interface "$peer_config" "interface"
+	if [ "$peer_interface" = "$peer_interface_filter" ]; then
+		config_get peer_address "$peer_config" "address"
+		json_add_string "" "$peer_address"
 	fi;
 }
 
 proto_yggdrasil_append_to_interface_regex() {
-	if [ -z "${regex}" ]; then
-		regex="$1"
-	else
-		regex="${regex}|$1";
-	fi;
+	regex="${regex:+$regex|}$1";
 }
 
 proto_yggdrasil_setup_multicast_interface() {
 	local interface_config="$1"
 	local beacon
 	local listen
-	local port=0
+	local port
 	local password
-	local regex=""
+	local regex=
 
-	config_get beacon "${interface_config}" "beacon"
-	config_get listen "${interface_config}" "listen"
-	config_get port "${interface_config}" "port"
-	config_get password "${interface_config}" "password"
+	config_get beacon   "$interface_config" "beacon"
+	config_get listen   "$interface_config" "listen"
+	config_get password "$interface_config" "password"
+	config_get port     "$interface_config" "port" "0"
 
 	json_add_object ""
-	json_add_boolean "Beacon" $beacon
-	json_add_boolean "Listen" $listen
-	if [ ! -z ${port} ]; then
-		json_add_int "Port" $port
-	else
-		json_add_int "Port" 0
-	fi;
-	if [ ! -z ${password} ]; then
-		json_add_string "Password" $password
-	fi;
-
-	config_list_foreach "${interface_config}" interface proto_yggdrasil_append_to_interface_regex
-
+	json_add_boolean "Beacon" "$beacon"
+	json_add_boolean "Listen" "$listen"
+	json_add_int "Port" "$port"
+	[ -n "$password" ] && json_add_string "Password" "$password"
+	config_list_foreach "$interface_config" interface proto_yggdrasil_append_to_interface_regex
 	json_add_string "Regex" "^(${regex})\$"
-
 	json_close_object
 }
 
 proto_yggdrasil_add_string() {
-	json_add_string "" $1
+	json_add_string "" "$1"
 }
 
 proto_yggdrasil_generate_keypair() {
+	local PrivateKey
 	json_load "$(yggdrasil -genconf -json)"
 	json_get_vars PrivateKey
 	json_cleanup
-	private_key=$PrivateKey
-	public_key=${PrivateKey:64}
+	private_key="$PrivateKey"
+	public_key="${PrivateKey:64}"
 }
 
 proto_yggdrasil_setup() {
@@ -114,45 +98,35 @@ proto_yggdrasil_setup() {
 	local node_info_privacy
 
 	config_load network
-	config_get private_key "${config}" "private_key"
-	config_get public_key "${config}" "public_key"
-	config_get mtu "${config}" "mtu"
-	config_get node_info "${config}" "node_info"
-	config_get node_info_privacy "${config}" "node_info_privacy"
+	config_get private_key       "$config" "private_key"
+	config_get public_key        "$config" "public_key"
+	config_get mtu               "$config" "mtu"
+	config_get node_info         "$config" "node_info"
+	config_get node_info_privacy "$config" "node_info_privacy"
 
-	if [ -z $private_key ]; then
-		proto_yggdrasil_generate_keypair
-	fi;
+	[ -z "$private_key" ] && proto_yggdrasil_generate_keypair
 
 	umask 077
-	mkdir -p "${ygg_dir}"
+	mkdir -p "$ygg_dir"
 
-	if [ $private_key = "auto" ]; then
+	if [ "$private_key" = "auto" ]; then
 		proto_yggdrasil_generate_keypair
-		uci -t ${ygg_dir}/.uci.${config} batch <<EOF
+		uci -t "${ygg_dir}/.uci.${config}" batch <<EOF
 			set network.${config}.private_key='${private_key}'
 			set network.${config}.public_key='${public_key}'
 EOF
-		uci -t ${ygg_dir}/.uci.${config} commit;
+		uci -t "${ygg_dir}/.uci.${config}" commit;
 	fi;
 
 	# Generate config file
 	json_init
-	json_add_string "IfName" ${config}
-	json_add_string "AdminListen" ${ygg_sock}
-
-	json_add_string "PrivateKey" ${private_key}
-	json_add_string "PublicKey" ${public_key}
-
-	if [ ! -z $mtu ]; then
-		json_add_int "IfMTU" ${mtu}
-	fi;
-
-	if [ ! -z $node_info ]; then
-		json_add_string "NodeInfo" "%%_YGGDRASIL_NODEINFO_TEMPLATE_%%"
-	fi;
-
-	json_add_boolean "NodeInfoPrivacy" ${node_info_privacy}
+	json_add_string "IfName" "$config"
+	json_add_string "AdminListen" "$ygg_sock"
+	json_add_string "PrivateKey" "$private_key"
+	json_add_string "PublicKey" "$public_key"
+	[ -n "$mtu" ] && json_add_int "IfMTU" "$mtu"
+	[ -n "$node_info" ] && json_add_string "NodeInfo" "%%_YGGDRASIL_NODEINFO_TEMPLATE_%%"
+	json_add_boolean "NodeInfoPrivacy" "$node_info_privacy"
 
 	# Peers
 	json_add_array "Peers"
@@ -162,11 +136,11 @@ EOF
 	local peer_interfaces
 	peer_interfaces=""
 	config_foreach proto_yggdrasil_dump_peer_interface "yggdrasil_${config}_peer"
-	peer_interfaces=$(echo -e ${peer_interfaces} | sort | uniq)
+	peer_interfaces="$(echo -e "$peer_interfaces" | sort | uniq)"
 
 	json_add_object "InterfacePeers"
-	for peer_interface_filter in ${peer_interfaces}; do
-		json_add_array "${peer_interface_filter}"
+	for peer_interface_filter in $peer_interfaces; do
+		json_add_array "$peer_interface_filter"
 		config_foreach proto_yggdrasil_setup_peer_if_interface "yggdrasil_${config}_peer"
 		json_close_array
 	done
@@ -185,13 +159,13 @@ EOF
 	json_close_array
 
 	json_dump > "${ygg_cfg}.1"
-	awk -v s='"%%_YGGDRASIL_NODEINFO_TEMPLATE_%%"' -v r="${node_info}" '{gsub(s, r)} 1' "${ygg_cfg}.1" > ${ygg_cfg}
+	awk -v s='"%%_YGGDRASIL_NODEINFO_TEMPLATE_%%"' -v r="$node_info" '{gsub(s, r)} 1' "${ygg_cfg}.1" > "$ygg_cfg"
 	rm "${ygg_cfg}.1"
 
 	proto_run_command "$config" /usr/sbin/yggdrasil -useconffile "${ygg_cfg}"
 	proto_init_update "$config" 1
-	proto_add_ipv6_address "$(yggdrasil -useconffile "${ygg_cfg}" -address)" "7"
-	proto_add_ipv6_prefix "$(yggdrasil -useconffile "${ygg_cfg}" -subnet)"
+	proto_add_ipv6_address "$(yggdrasil -useconffile "$ygg_cfg" -address)" "7"
+	proto_add_ipv6_prefix "$(yggdrasil -useconffile "$ygg_cfg" -subnet)"
 	proto_send_update "$config"
 }
 
