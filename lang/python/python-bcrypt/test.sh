ae450d998d447213a553d7798470b4f5372af491
Jeffery To <jeffery.to@gmail.com>
2023-12-04 14:17:36 +0800

    python-bcrypt: Update to 4.1.1
    This also updates the test.sh script to use an assert.
    
    Signed-off-by: Jeffery To <jeffery.to@gmail.com>

--- lang/python/python-bcrypt/test.sh
+++ lang/python/python-bcrypt/test.sh
@@ -2,10 +2,11 @@
 
 [ "$1" = python3-bcrypt ] || exit 0
 
-python3 - << EOF
-import sys
+python3 - << 'EOF'
+
 import bcrypt
 password = b"super secret password"
 hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-sys.exit(0 if bcrypt.checkpw(password, hashed) else 1)
+assert bcrypt.checkpw(password, hashed)
+
 EOF
