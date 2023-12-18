78c0bc1943d6558b8b6a855a322a8ccd3a016871
Alexandru Ardelean <alex@shruggie.ro>
2023-12-18 14:08:51 +0200

    django: bump to version 5.0
    Signed-off-by: Alexandru Ardelean <alex@shruggie.ro>

---
+++ lang/python/django/test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+[ "$1" = "python3-django" ] || exit 0
+
+GOT_VER=$(/usr/bin/django-admin version)
+[ "$GOT_VER" = "$2" ] || {
+	echo "Incorrect version: expected '$2' ; obtained '$GOT_VER'"
+	exit 1
+}
+
+python3 - << EOF
+import sys
+import django
+
+if (django.__version__) != "$GOT_VER":
+    print("Wrong version: " + django.__version__)
+    sys.exit(1)
+
+sys.exit(0)
+EOF
+
