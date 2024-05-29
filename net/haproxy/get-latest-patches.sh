78621c5c57ea5e1c5f3d1af030e7cb6fdec6852a
Christian Lachner <gladiac@gmail.com>
2024-05-29 16:11:20 +0200

    haproxy: update to v3.0.0
    - New major LTS release
    - Update haproxy PKG_VERSION and PKG_HASH
    - Enabled QUIC support. It still has to be enabled in the haproxy config
    - See changes: http://git.haproxy.org/?p=haproxy-3.0.git;a=shortlog
    
    Signed-off-by: Christian Lachner <gladiac@gmail.com>

--- net/haproxy/get-latest-patches.sh
+++ net/haproxy/get-latest-patches.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CLONEURL=https://git.haproxy.org/git/haproxy-2.8.git
-BASE_TAG=v2.8.9
+CLONEURL=https://git.haproxy.org/git/haproxy-3.0.git
+BASE_TAG=v3.0.0
 TMP_REPODIR=tmprepo
 PATCHESDIR=patches
 
