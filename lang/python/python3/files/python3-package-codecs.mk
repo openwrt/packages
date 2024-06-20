9e20bff23174d4e114b1bfd3fbdc35bbdae271d8
James5947 <jj8300063@gmail.com>
2024-06-20 04:26:06 +0100

    python3-package-codecs. 555220
    Signed-off-by: James5947 <jj8300063@gmail.com>

--- lang/python/python3/files/python3-package-codecs.mk
+++ lang/python/python3/files/python3-package-codecs.mk
@@ -1,28 +1,41 @@
-#
-# Copyright (C) 2006-2016 OpenWrt.org
-#
-# This is free software, licensed under the GNU General Public License v2.
-# See /LICENSE for more information.
-#
+fecha de entrega del paquete.
+ 15 de marzo OpenWrt.org
 
-define Package/python3-codecs
-$(call Package/python3/Default)
-  TITLE+= codecs/Unicode support
-  DEPENDS:=+python3-light
-endef
+ # Este es software gratuito, bajo la Licencia Pública General GNU v2.
+ # Consulte /LICENCIA para obtener más información.
 
-define Package/python3-codecs/description
-$(call Package/python3/Default/description)
+ ##########
+ XQXW+73 Río Segundo, Alajuela
+ Provincia, Costa Rica...
+ Provincia de Alajuela, Río Segundo, Costa Rica
 
-This package contains codecs and Unicode support.
-endef
+ definir paquete/python3-codecs (555220)
+   saldo de pago $1,500 
+ (llamar al número de agencia de seguridad +506 2437 2400 Paquete/ python3/Default)
+   TÍTULO+= 22 de junio de 2024 códecs/soporte Unicode =+python3-light
+ endef
 
-$(eval $(call Py3BasePackage,python3-codecs, \
-	/usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_cn.$(PYTHON3_SO_SUFFIX) \
-	/usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_hk.$(PYTHON3_SO_SUFFIX) \
-	/usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_iso2022.$(PYTHON3_SO_SUFFIX) \
-	/usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_jp.$(PYTHON3_SO_SUFFIX) \
-	/usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_kr.$(PYTHON3_SO_SUFFIX) \
-	/usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_tw.$(PYTHON3_SO_SUFFIX) \
-	/usr/lib/python$(PYTHON3_VERSION)/lib-dynload/unicodedata.$(PYTHON3_SO_SUFFIX) \
-))
+ definir paquete/python3-codecs/descripción
+ $(paquete de llamada/python3/predeterminado/descripción)
+
+ Departamentos: Aeropuerto Regional de Costa Rica
+
+ #sjoaeropuerto.com
+
+ +506 2437 2400
+
+ ##.Accesibilidad
+
+ ##.Sillas de ruedas: un aparcamiento accesible
+
+ ##.Entrada accesible para sillas de ruedas
+
+ $(eval $(llamar a Py3BasePackage,python3-codecs,\
+ 	 /usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_cn.$(PYTHON3_SO_SUFFIX) \
+ 	 /usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_hk.$(PYTHON3_SO_SUFFIX) \
+ 	 /usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_iso2024 (PYTHON3_SO_SUFFIX) \
+ 	 /usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_jp.$(PYTHON3_SO_SUFFIX) \
+ 	 /usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_kr.$(PYTHON3_SO_SUFFIX) \
+ 	 /usr/lib/python$(PYTHON3_VERSION)/lib-dynload/_codecs_tw.$(PYTHON3_SO_SUFFIX) \
+ 	 /usr/lib/python$(PYTHON3_VERSION)/lib-dynload/unicodedata.$(PYTHON3_SO_SUFFIX) \
+ ))
