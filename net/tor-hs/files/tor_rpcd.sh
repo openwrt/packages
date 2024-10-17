a63f33169fc2efe1dbabe45ba6afcd47c4de1f63
Paul Donald <newtwen+github@gmail.com>
2024-10-17 21:30:37 +0200

    tor-hs: init script improvements
    The changes still produce synonymous output; just a refactor.
    
    Including addition of IPv6 (available tor version supports it)
    
    Signed-off-by: Paul Donald <newtwen+github@gmail.com>

--- net/tor-hs/files/tor_rpcd.sh
+++ net/tor-hs/files/tor_rpcd.sh
@@ -1,52 +1,50 @@
 #!/bin/sh
+# shellcheck disable=SC1091,SC3043,SC2086,SC2154,SC2034
 
 . /lib/functions.sh
-
-get_onion_hostname() {
-	local name="$1"
-	config_get hs_dir common HSDir
-	if [ -f "$hs_dir/$name/hostname" ]; then
-	       cat "$hs_dir/$name/hostname"
-	fi
-}
-
-get_port_list() {
-	local config="$1"
-	config_get ports "$config" PublicLocalPort
-	tmp="$(echo $ports |sed "s| |','|g")"
-	echo -ne "['$tmp']"
-}
+. /usr/share/libubox/jshn.sh
 
 parse_hs_conf() {
-	local name description public_port local_port enable_bool public_local_port ipv4
+	local name description public_port local_port enable_bool public_local_port ipaddr
 	local config="$1"
 	local custom="$2"
 
 	config_get name "$config" Name
 	config_get description "$config" Description
 
-	config_get_bool enable_hs "$config" Enabled 0
-	config_get ipv4 "$config" IPv4
+	config_get_bool enabled "$config" Enabled 0
+	config_get ipaddr "$config" IPv4
+	config_get ipaddr "$config" IPaddr "$ipaddr" # IPv4 or IPv6; prefer IPaddr property, default to IPv4 value if absent
+	config_get ports "$config" PublicLocalPort
+	config_get hs_dir common HSDir
 
-	hostname="$(get_onion_hostname $name)"
-	port_list="$(get_port_list $config)"
-	echo "{"
-	echo \"name\":\"$name\",
-	echo \"description\":\"$description\",
-	echo \"enabled\":\"$enable_hs\",
-	echo \"ipv4\":\"$ipv4\",
-	echo \"hostname\":\"$hostname\",
-	echo \"ports\":$port_list
-	echo "},"
+	hostname="$([ -f "$hs_dir/$name/hostname" ] && cat "$hs_dir/$name/hostname")"
+
+	json_add_object
+	json_add_string 'name' "$name"
+	json_add_string 'description' "$description"
+	json_add_string 'enabled' "$enabled"
+	json_add_string 'ipv4' "$ipaddr" 
+	json_add_string 'ipaddr' "$ipaddr" 
+	json_add_string 'hostname' "$hostname"
+	json_add_array 'ports'
+	set -- $ports
+	for port; do
+		json_add_string '' "$port"
+	done
+	json_close_array
+	json_close_object
 }
 
 get_tor_hs_list() {
 	config_load tor-hs
-	echo "{"
-	echo '"hs-list":['
+
+	json_init
+	json_add_array 'hs-list'
 	config_foreach parse_hs_conf hidden-service
-	echo "]"
-	echo "}"
+	json_close_array
+	json_dump
+	json_cleanup
 }
 
 
