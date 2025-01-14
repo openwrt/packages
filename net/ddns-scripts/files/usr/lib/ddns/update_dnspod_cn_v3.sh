9d5da216c904ec26189326fecbbb93eadef86d09
FriesI23 Qin <FriesI23@outlook.com>
2025-01-14 14:20:10 +0800

    ddns-scripts: Fixup dnspod.cn-v3
    Fix signature expiration issue during retries.
    
    Signed-off-by: FriesI23 Qin <FriesI23@outlook.com>

--- net/ddns-scripts/files/usr/lib/ddns/update_dnspod_cn_v3.sh
+++ net/ddns-scripts/files/usr/lib/ddns/update_dnspod_cn_v3.sh
@@ -8,6 +8,8 @@
 #	- at: net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
 # - github.com/nixonli/ddns-scripts_dnspod for "update_dnspod_cn.sh"
 #
+# v1.1.0: Publish script
+#
 # 2024 FriesI23 <FriesI23@outlook.com>
 #
 # API documentation at https://cloud.tencent.com/document/api/1427/84627
@@ -43,6 +45,10 @@ local __URLBASE="https://$__URLHOST"
 local __METHOD="POST"
 local __CONTENT_TYPE="application/json"
 
+# Build base command to use
+local __PRGBASE="$CURL -RsS -o $DATFILE --stderr $ERRFILE"
+local __PRGEXTA=""
+
 # split __HOST __DOMAIN from $domain
 # given data:
 # @example.com for "domain record"
@@ -58,6 +64,7 @@ tencentcloud_transfer() {
 	local __ERR __CODE
 
 	while :; do
+		__RUNPROG="$__PRGBASE $($__PRGEXTA)"
 		write_log 7 "#> $__RUNPROG"
 		eval "$__RUNPROG"
 		__ERR=$? # save communication error
@@ -105,9 +112,6 @@ tencentcloud_transfer() {
 
 	return 0
 }
-
-# Build base command to use
-__PRGBASE="$CURL -RsS -o $DATFILE --stderr $ERRFILE"
 
 # force network/interface-device to use for communication
 if [ -n "$bind_network" ]; then
@@ -298,7 +302,7 @@ if [ -n "$record_id" ]; then
 	__RECID="$record_id"
 else
 	# read record id for A or AAAA record of host.domain.TLD
-	__RUNPROG="$__PRGBASE $(build_describe_record_list_request_param)"
+	__PRGEXTA="build_describe_record_list_request_param"
 	# extract zone id
 	tencentcloud_transfer || return 1
 	__RECID=$(grep -o '"RecordId":[[:space:]]*[0-9]*' $DATFILE | grep -o '[0-9]*' | head -1)
@@ -335,11 +339,11 @@ __DATA=$(grep -o '"Value":\s*"[^"]*' $DATFILE | grep -o '[^"]*$' | head -1)
 
 if [ -z "$__RECID" ]; then
 	# create new record if record id not found
-	__RUNPROG="$__PRGBASE $(build_create_record_request_param $__IP $__RECLINE)"
+	__PRGEXTA="build_create_record_request_param $__IP $__RECLINE"
 	tencentcloud_transfer || return 1
 	return 0
 fi
 
-__RUNPROG="$__PRGBASE $(build_modify_record_request_param $__IP $__RECLINE $__RECID)"
+__PRGEXTA="build_modify_record_request_param $__IP $__RECLINE $__RECID"
 tencentcloud_transfer || return 1
 return
