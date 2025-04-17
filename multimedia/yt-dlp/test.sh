openwrt/packages

commit afe9935ff41b3d7cf4d4c08894be272b8d4061b7

yt-dlp: add CI version check
Signed-off-by: George Sapkin <george@sapk.in>

multimedia/yt-dlp/test.sh
@@ -1,0 +1,3 @@
+#!/bin/sh
+
+yt-dlp --version 2>&1 | sed 's/\.0\+/./g' | grep -x "$PKG_VERSION"

commit 5a289451af05d9872154455749578e4587842e7e

yt-dlp: run CI test for the main package only
Fixes: afe9935ff ("yt-dlp: add CI version check")
Signed-off-by: George Sapkin <george@sapk.in>

multimedia/yt-dlp/Makefile
@@ -2,7 +2,7 @@ include $(TOPDIR)/rules.mk
 
 PKG_NAME:=yt-dlp
 PKG_VERSION:=2025.3.31
-PKG_RELEASE:=1
+PKG_RELEASE:=2
 
 PYPI_NAME:=yt-dlp
 PKG_HASH:=1bfe0e660d1a70a09e27b2d58f92e30b1e2e362cc487829f2f824346ae49fb91

multimedia/yt-dlp/test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-yt-dlp --version 2>&1 | sed 's/\.0\+/./g' | grep -x "$PKG_VERSION"
+if [ "$1" = 'yt-dlp' ]; then
+	yt-dlp --version 2>&1 | sed 's/\.0\+/./g' | grep -x "$PKG_VERSION"
+fi