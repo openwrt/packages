openwrt/packages

commit b2d2ba8ae9e852f02753dfcf43773883e9b0d123

sqlite3: add CI version check
Signed-off-by: George Sapkin <george@sapk.in>

libs/sqlite3/test.sh
@@ -1,0 +1,5 @@
+#!/bin/sh
+
+if [ "$1" = 'sqlite3-cli' ]; then
+	sqlite3 -version 2>&1 | cut -d' ' -f1 | grep -x "$PKG_VERSION"
+fi

commit d035756cd6b27fafb04d5bb1005558947c2c1623

sqlite3: fix CI test
Fixes: b2d2ba8ae ("sqlite3: add CI version check")
Signed-off-by: George Sapkin <george@sapk.in>

libs/sqlite3/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ "$1" = 'sqlite3-cli' ]; then
-	sqlite3 -version 2>&1 | cut -d' ' -f1 | grep -x "$PKG_VERSION"
+	sqlite3 -version | grep -F "$PKG_VERSION"
 fi