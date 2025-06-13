openwrt/packages

commit 8ccace5af4c582aaf9360f127536b390ca30aff9

adguardhome: add CI version check
Signed-off-by: George Sapkin <george@sapk.in>

net/adguardhome/test.sh
@@ -1,0 +1,3 @@
+#!/bin/sh
+
+AdGuardHome --version 2>&1 | cut -d' ' -f4 | grep "${PKG_VERSION}"

commit df2b00583b066e95e57b9d90139512ac2ad23496

adguardhome: don't hide error messages in CI test
Fixes: 8ccace5af ("adguardhome: add CI version check")
Signed-off-by: George Sapkin <george@sapk.in>

net/adguardhome/test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-AdGuardHome --version 2>&1 | cut -d' ' -f4 | grep "${PKG_VERSION}"
+AdGuardHome --version | grep -F "$PKG_VERSION"