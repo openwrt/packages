openwrt/packages

commit 6746398cb4d3a64d3d6f71cefce304bcbcc39daf

yt-dlp: run CI test for the main package only
Fixes: afe9935ff ("yt-dlp: add CI version check")
Signed-off-by: George Sapkin <george@sapk.in>

multimedia/yt-dlp/test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-yt-dlp --version 2>&1 | sed 's/\.0\+/./g' | grep -x "$PKG_VERSION"
+if [ "$1" = 'yt-dlp' ]; then
+	yt-dlp --version 2>&1 | sed 's/\.0\+/./g' | grep -x "$PKG_VERSION"
+fi

multimedia/yt-dlp/Makefile
@@ -2,7 +2,7 @@ include $(TOPDIR)/rules.mk
 
 PKG_NAME:=yt-dlp
 PKG_VERSION:=2025.3.31
-PKG_RELEASE:=1
+PKG_RELEASE:=2
 
 PYPI_NAME:=yt-dlp
 PKG_HASH:=1bfe0e660d1a70a09e27b2d58f92e30b1e2e362cc487829f2f824346ae49fb91

commit 21145b7a05a947b82948368dfc75305322feead3

yt-dlp: don't hide error messages in CI test
Fixes: 6746398cb ("yt-dlp: run CI test for the main package only")
Signed-off-by: George Sapkin <george@sapk.in>

multimedia/yt-dlp/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ "$1" = 'yt-dlp' ]; then
-	yt-dlp --version 2>&1 | sed 's/\.0\+/./g' | grep -x "$PKG_VERSION"
+	yt-dlp --version | sed 's/\.0\+/./g' | grep -F "$PKG_VERSION"
 fi