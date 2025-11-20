openwrt/packages

commit 9236e4f3e3fb31f01e9362073e3d4ecfe9f8a3fb

sqlite3: import 3.7.12.1 (2012-05-22) from packages
Signed-off-by: Etienne CHAMPETIER <etienne.champetier@free.fr>

libs/sqlite3/Makefile
@@ -1,0 +1,105 @@
+#
+# Copyright (C) 2006-2012 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+include $(TOPDIR)/rules.mk
+
+PKG_NAME:=sqlite
+PKG_VERSION:=3071201
+PKG_RELEASE:=1
+
+PKG_SOURCE:=$(PKG_NAME)-autoconf-$(PKG_VERSION).tar.gz
+PKG_SOURCE_URL:=http://www.sqlite.org
+PKG_MD5SUM:=eb7bbd258913518ad30971ea7ecb0ca9
+
+PKG_LICENSE:=PUBLICDOMAIN
+PKG_LICENSE_FILES:=
+
+PKG_BUILD_DIR:=$(BUILD_DIR)/$(PKG_NAME)-autoconf-$(PKG_VERSION)
+PKG_BUILD_DEPENDS:=libncurses libreadline
+
+PKG_FIXUP:=autoreconf
+
+PKG_INSTALL:=1
+
+include $(INCLUDE_DIR)/package.mk
+
+define Package/sqlite3/Default
+  SUBMENU:=database
+  TITLE:=SQLite (v3.x) database engine
+  URL:=http://www.sqlite.org/
+endef
+
+define Package/sqlite3/Default/description
+ SQLite is a small C library that implements a self-contained, embeddable,
+ zero-configuration SQL database engine.
+endef
+
+define Package/libsqlite3
+  $(call Package/sqlite3/Default)
+  SECTION:=libs
+  CATEGORY:=Libraries
+  DEPENDS:=+libpthread
+  TITLE+= (library)
+endef
+
+define Package/libsqlite3/description
+$(call Package/sqlite3/Default/description)
+ This package contains the SQLite (v3.x) shared library, used by other
+ programs.
+endef
+
+define Package/sqlite3-cli
+  $(call Package/sqlite3/Default)
+  SECTION:=utils
+  CATEGORY:=Utilities
+  DEPENDS:=+libsqlite3 +libncurses +libreadline
+  TITLE+= (cli)
+endef
+
+define Package/sqlite3-cli/description
+$(call Package/sqlite3/Default/description)
+ This package contains a terminal-based front-end to the SQLite (v3.x) library
+ that can evaluate queries interactively and display the results in multiple
+ formats.
+endef
+
+TARGET_CFLAGS += $(FPIC) \
+	-DSQLITE_ENABLE_UNLOCK_NOTIFY \
+
+CONFIGURE_ARGS += \
+	--enable-shared \
+	--enable-static \
+
+CONFIGURE_VARS += \
+	config_BUILD_CC="$(HOSTCC)" \
+	config_BUILD_CFLAGS="-O2" \
+	config_TARGET_CC="$(TARGET_CC)" \
+	config_TARGET_CFLAGS="$(TARGET_CFLAGS)" \
+	config_TARGET_READLINE_INC="$(TARGET_CPPFLAGS)" \
+	config_TARGET_READLINE_LIBS="$(TARGET_LDFLAGS) -lreadline -lncurses" \
+
+define Build/InstallDev
+	$(INSTALL_DIR) $(1)/usr/include
+	$(CP) $(PKG_INSTALL_DIR)/usr/include/sqlite3{,ext}.h $(1)/usr/include/
+	$(INSTALL_DIR) $(1)/usr/lib
+	$(CP) $(PKG_INSTALL_DIR)/usr/lib/libsqlite3.{a,so*} $(1)/usr/lib/
+	$(INSTALL_DIR) $(1)/usr/lib/pkgconfig
+	$(CP) $(PKG_INSTALL_DIR)/usr/lib/pkgconfig/sqlite3.pc $(1)/usr/lib/pkgconfig/
+endef
+
+define Package/libsqlite3/install
+	$(INSTALL_DIR) $(1)/usr/lib
+	$(CP) $(PKG_INSTALL_DIR)/usr/lib/libsqlite3.so.* $(1)/usr/lib/
+endef
+
+define Package/sqlite3-cli/install
+	$(INSTALL_DIR) $(1)/usr/bin
+	$(INSTALL_BIN) $(PKG_INSTALL_DIR)/usr/bin/sqlite3 $(1)/usr/bin/
+endef
+
+$(eval $(call BuildPackage,libsqlite3))
+$(eval $(call BuildPackage,sqlite3-cli))

commit a42d99ae86780365c8c6d1c0ddef7766c798d7f8

sqlite3: set correct SPDX license identifiers
Replace boilerplate license header with a SPDX license identifier.

Set correct package license to blessing.

Fixes: 9236e4f ("sqlite3: import 3.7.12.1 (2012-05-22) from packages")
Link: https://spdx.org/licenses/blessing.html
Signed-off-by: George Sapkin <george@sapk.in>

libs/sqlite3/Makefile
@@ -1,9 +1,6 @@
+# SPDX-License-Identifier: GPL-2.0-only
 #
-# Copyright (C) 2006-2014 OpenWrt.org
-#
-# This is free software, licensed under the GNU General Public License v2.
-# See /LICENSE for more information.
-#
+# Copyright (C) 2006-2025 OpenWrt.org
 
 include $(TOPDIR)/rules.mk
 
@@ -17,8 +14,8 @@ PKG_SOURCE_URL:=https://www.sqlite.org/2025/
 PKG_HASH:=42e26dfdd96aa2e6b1b1be5c88b0887f9959093f650d693cb02eb9c36d146ca5
 
 PKG_CPE_ID:=cpe:/a:sqlite:sqlite
-PKG_LICENSE:=PUBLICDOMAIN
-PKG_LICENSE_FILES:=
+PKG_LICENSE:=blessing
+PKG_LICENSE_FILES:=LICENSE.md
 PKG_MAINTAINER:=
 
 PKG_BUILD_DIR:=$(BUILD_DIR)/$(PKG_NAME)-autoconf-$(PKG_SRC_VERSION)

libs/sqlite3/test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#
+# SPDX-License-Identifier: GPL-2.0-only
 
 if [ "$1" = 'sqlite3-cli' ]; then
 	sqlite3 -version | grep -F "$PKG_VERSION"