openwrt/packages

commit aebfd497949f3fcf56a7f0b5db160cdf5be8200c

sqlite3: bump to 3.49.1
Changelog: https://sqlite.org/releaselog/3_49_1.html
Suggested-by: Tianling Shen <cnsztl@immortalwrt.org>
Signed-off-by: George Sapkin <george@sapk.in>

libs/sqlite3/Makefile
@@ -8,12 +8,12 @@
 include $(TOPDIR)/rules.mk
 
 PKG_NAME:=sqlite
-PKG_VERSION:=3470200
+PKG_VERSION:=3490100
 PKG_RELEASE:=1
 
 PKG_SOURCE:=$(PKG_NAME)-autoconf-$(PKG_VERSION).tar.gz
-PKG_SOURCE_URL:=https://www.sqlite.org/2024/
-PKG_HASH:=f1b2ee412c28d7472bc95ba996368d6f0cdcf00362affdadb27ed286c179540b
+PKG_SOURCE_URL:=https://www.sqlite.org/2025/
+PKG_HASH:=106642d8ccb36c5f7323b64e4152e9b719f7c0215acf5bfeac3d5e7f97b59254
 
 PKG_CPE_ID:=cpe:/a:sqlite:sqlite
 PKG_LICENSE:=PUBLICDOMAIN
@@ -99,13 +99,14 @@ TARGET_CFLAGS += \
 	$(if $(CONFIG_SQLITE3_BATCH_ATOMIC_WRITE),-DSQLITE_ENABLE_BATCH_ATOMIC_WRITE) \
 	$(if $(CONFIG_SQLITE3_COLUMN_METADATA),-DSQLITE_ENABLE_COLUMN_METADATA)
 
-CONFIGURE_ARGS += \
+CONFIGURE_ARGS := \
+	$(filter-out --target=% $(DISABLE_IPV6) $(DISABLE_NLS),$(CONFIGURE_ARGS)) \
 	--disable-debug \
 	--disable-static-shell \
 	--enable-shared \
 	--enable-static \
 	--enable-threadsafe \
-	$(if $(CONFIG_SQLITE3_DYNAMIC_EXTENSIONS),--enable-dynamic-extensions,--disable-dynamic-extensions) \
+	$(if $(CONFIG_SQLITE3_DYNAMIC_EXTENSIONS),--enable-load-extension,--disable-load-extension) \
 	$(if $(CONFIG_SQLITE3_FTS3),--enable-fts3,--disable-fts3) \
 	$(if $(CONFIG_SQLITE3_FTS4),--enable-fts4,--disable-fts4) \
 	$(if $(CONFIG_SQLITE3_FTS5),--enable-fts5,--disable-fts5) \
@@ -130,7 +131,7 @@ endef
 
 define Package/libsqlite3/install
 	$(INSTALL_DIR) $(1)/usr/lib
-	$(CP) $(PKG_INSTALL_DIR)/usr/lib/libsqlite3.so.$(ABI_VERSION)* $(1)/usr/lib
+	$(CP) $(PKG_INSTALL_DIR)/usr/lib/libsqlite3.so* $(1)/usr/lib
 endef
 
 define Package/sqlite3-cli/install

commit 89a841ec65e9dfb05ef744184e3764895bed13b9

sqlite3: don't install ABI-less library
Don't install the ABI-less library to support potential multiple ABIs
side by side.

Add a matching tests to check the soname and ensure the ABI-less library
is not installed.

Fixes: aebfd49 ("sqlite3: bump to 3.49.1")
Signed-off-by: George Sapkin <george@sapk.in>

libs/sqlite3/Makefile
@@ -7,7 +7,7 @@ include $(TOPDIR)/rules.mk
 PKG_NAME:=sqlite
 PKG_VERSION:=3.51.0
 PKG_SRC_VERSION:=3510000
-PKG_RELEASE:=1
+PKG_RELEASE:=2
 
 PKG_SOURCE:=$(PKG_NAME)-autoconf-$(PKG_SRC_VERSION).tar.gz
 PKG_SOURCE_URL:=https://www.sqlite.org/2025/
@@ -137,7 +137,7 @@ endef
 
 define Package/libsqlite3/install
 	$(INSTALL_DIR) $(1)/usr/lib
-	$(CP) $(PKG_INSTALL_DIR)/usr/lib/libsqlite3.so* $(1)/usr/lib
+	$(CP) $(PKG_INSTALL_DIR)/usr/lib/libsqlite3.so.{$(PKG_VERSION),$(ABI_VERSION)} $(1)/usr/lib
 endef
 
 define Package/sqlite3-cli/install

libs/sqlite3/test.sh
@@ -2,6 +2,44 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only
 
-if [ "$1" = 'sqlite3-cli' ]; then
-	sqlite3 -version | grep -F "$PKG_VERSION"
-fi
+case "$PKG_NAME" in
+	libsqlite3)
+		apk add binutils
+
+		readelf_out=$(readelf --dynamic "/usr/lib/libsqlite3.so.$PKG_VERSION")
+		if [ $? -ne 0 ]; then
+			echo "readelf failed for /usr/lib/libsqlite3.so.$PKG_VERSION" >&2
+			exit 1
+		fi
+
+		soname=$(echo "$readelf_out" \
+			| grep -F '(SONAME)' \
+			| sed -E 's/.*\[(.*)\]/\1/')
+
+		if [ -z "$soname" ]; then
+			echo "soname not found in /usr/lib/libsqlite3.so.$PKG_VERSION" >&2
+			exit 1
+		fi
+
+		link_target=$(readlink "/usr/lib/$soname")
+		if [ $? -ne 0 ]; then
+			echo "Failed to read soname link /usr/lib/$soname" >&2
+			exit 1
+		fi
+
+		expected_target="libsqlite3.so.$PKG_VERSION"
+		if [ "$link_target" != "$expected_target" ]; then
+			echo "soname link /usr/lib/$soname points to '$link_target', expected '$expected_target'" >&2
+			exit 1
+		fi
+
+		if [ -f '/usr/lib/libsqlite3.so' ]; then
+			echo "/usr/lib/libsqlite3.so shouldn't be installed" >&2
+			exit 1
+		fi
+		;;
+
+	sqlite3-cli)
+		sqlite3 -version | grep -F "$PKG_VERSION"
+		;;
+esac