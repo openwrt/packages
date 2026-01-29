8172a03cb7c6e8d30c1466ce9866e594a65d65dd
George Sapkin <george@sapk.in>
2026-01-29 02:17:26 +0200

    golang: split src into separate packages
    Split misc sources and test data into separate packages. Reduces target
    src package size by a third.
    
    Move architecture-dependent generated source from src to the main
    package.
    
    Mark doc, misc, src and tests packages with PKGARCH:=all as they don't
    have any architecture-specific files.
    
    Fix stripping and strip compiler only.
    
    Fixes: c137c382 ("golang: new packages")
    Link: https://github.com/openwrt/packages/pull/28445
    Suggested-by: Jeffery To <jeffery.to@gmail.com>
    Signed-off-by: George Sapkin <george@sapk.in>
    (cherry picked from commit 5657eb0e2efb64e3f1001dd900310fbf0b488c20)

--- lang/golang/golang-compiler.mk
+++ lang/golang/golang-compiler.mk
@@ -36,8 +36,22 @@ endef
 # 2: destination prefix
 # 3: go version id
 # 4: file/directory name
+# 5: filter (optional)
 define GoCompiler/Default/Install/install-lib-data
+
+  ifeq ($(5),)
 	$(CP) "$(1)/$(4)" "$(2)/lib/go-$(3)/"
+  else
+	$(INSTALL_DIR) "$(2)/lib/go-$(3)/$(4)"; \
+	cd "$(1)/$(4)" && \
+	$(FIND) . ! -type d -a \( $(5) \) -print0 | \
+	cpio \
+		--make-directories \
+		--null \
+		--pass-through \
+		--unconditional \
+		"$(2)/lib/go-$(3)/$(4)/"
+  endif
 endef
 
 # 1: source go root
@@ -45,6 +59,7 @@ endef
 # 3: go version id
 # 4: GOOS_GOARCH
 # 5: install suffix (optional)
+# 6: if target, package architecture-specific sources
 define GoCompiler/Default/Install/Bin
 	$(call GoCompiler/Default/Install/make-dirs,$(2),$(3))
 
@@ -53,7 +68,7 @@ define GoCompiler/Default/Install/Bin
 	$(INSTALL_DATA) -p "$(1)/go.env" "$(2)/lib/go-$(3)/"
 	$(INSTALL_DATA) -p "$(1)/VERSION" "$(2)/lib/go-$(3)/"
 
-	for file in CONTRIBUTING.md LICENSE PATENTS README.md SECURITY.md; do \
+	for file in $(strip $(if $(filter target,$(6)),$(GO_BIN_FILES),$(GO_LEGAL_FILES))); do \
 		if [ -f "$(1)/$$$$file" ]; then \
 			$(INSTALL_DATA) -p "$(1)/$$$$file" "$(2)/lib/go-$(3)/" ; \
 		fi ; \
@@ -96,23 +111,38 @@ endef
 # 1: source go root
 # 2: destination prefix
 # 3: go version id
+define GoCompiler/Default/Install/Misc
+	$(call GoCompiler/Default/Install/make-dirs,$(2),$(3))
+	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),misc)
+endef
+
+# 1: source go root
+# 2: destination prefix
+# 3: go version id
+# 4: if target, package architecture-specific sources
 define GoCompiler/Default/Install/Src
 	$(call GoCompiler/Default/Install/make-dirs,$(2),$(3))
-
-	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),lib)
-	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),misc)
-	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),src)
-	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),test)
-
-	$(FIND) \
-		"$(2)/lib/go-$(3)/src/" \
-		\! -type d -a \( -name "*.bat" -o -name "*.rc" \) \
-		-delete
+	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),src,$(strip \
+		$(if $(filter target,$(4)), \
+			$(GO_TARGET_SRC_FILTERS), \
+			$(GO_HOST_SRC_FILTERS) \
+		) \
+	))
 
 	if [ -d "$(1)/pkg/include" ]; then \
 		$(INSTALL_DIR) "$(2)/lib/go-$(3)/pkg" ; \
 		$(CP) "$(1)/pkg/include" "$(2)/lib/go-$(3)/pkg/" ; \
 	fi
+endef
+
+# 1: source go root
+# 2: destination prefix
+# 3: go version id
+define GoCompiler/Default/Install/Tests
+	$(call GoCompiler/Default/Install/make-dirs,$(2),$(3))
+	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),lib)
+	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),src,$(GO_TARGET_TEST_FILTERS))
+	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),test)
 endef
 
 # 1: destination prefix
@@ -148,8 +178,9 @@ define GoCompiler/AddProfile
   endef
 
   # 1: override install prefix (optional)
+  # 2: if target, package architecture-specific sources
   define GoCompiler/$(1)/Install/Bin
-	$$(call GoCompiler/Default/Install/Bin,$(2),$$(or $$(1),$(3)),$(4),$(5),$(6))
+	$$(call GoCompiler/Default/Install/Bin,$(2),$$(or $$(1),$(3)),$(4),$(5),$(6),$$(2))
   endef
 
   # 1: override install prefix (optional)
@@ -163,8 +194,19 @@ define GoCompiler/AddProfile
   endef
 
   # 1: override install prefix (optional)
+  define GoCompiler/$(1)/Install/Misc
+	$$(call GoCompiler/Default/Install/Misc,$(2),$$(or $$(1),$(3)),$(4))
+  endef
+
+  # 1: override install prefix (optional)
+  # 2: if target, package architecture-specific sources
   define GoCompiler/$(1)/Install/Src
-	$$(call GoCompiler/Default/Install/Src,$(2),$$(or $$(1),$(3)),$(4))
+	$$(call GoCompiler/Default/Install/Src,$(2),$$(or $$(1),$(3)),$(4),$$(2))
+  endef
+
+  # 1: override install prefix (optional)
+  define GoCompiler/$(1)/Install/Tests
+	$$(call GoCompiler/Default/Install/Tests,$(2),$$(or $$(1),$(3)),$(4),$$(2))
   endef
 
   # 1: override install prefix (optional)
