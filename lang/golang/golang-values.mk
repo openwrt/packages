b05e404f41519ae6733a1a863207c90f4f3d47b3
George Sapkin <george@sapk.in>
2026-01-30 04:52:59 +0200

    golang: use upstream OS/arch format
    Use OS/arch format used by Go and reported by go tool dist list.
    
    Update OS/arch combinations.
    
    Signed-off-by: George Sapkin <george@sapk.in>
    (cherry picked from commit 1eaa1f848bd5e9a83b1726153037ca5f46fbc416)

--- lang/golang/golang-values.mk
+++ lang/golang/golang-values.mk
@@ -143,14 +143,14 @@ go_arch=$(subst \
 
 GO_OS:=linux
 GO_ARCH:=$(call go_arch,$(ARCH))
-GO_OS_ARCH:=$(GO_OS)_$(GO_ARCH)
+GO_OS_ARCH:=$(GO_OS)/$(GO_ARCH)
 
 GO_HOST_OS:=$(call tolower,$(HOST_OS))
 GO_HOST_ARCH:=$(call go_arch,$(subst \
   armv6l,arm,$(subst \
   armv7l,arm,$(subst \
   i686,i386,$(HOST_ARCH)))))
-GO_HOST_OS_ARCH:=$(GO_HOST_OS)_$(GO_HOST_ARCH)
+GO_HOST_OS_ARCH:=$(GO_HOST_OS)/$(GO_HOST_ARCH)
 
 # Filter lists for ARM64 cores
 # See https://en.wikipedia.org/wiki/ARM_architecture_family#Cores
@@ -285,21 +285,32 @@ GO_ARCH_DEPENDS:=@(aarch64||arm||i386||i686||loongarch64||mips||mips64||mips64el
 
 # From https://go.dev/src/internal/platform/supported.go
 GO_PIE_SUPPORTED_OS_ARCH:= \
-  android_386  android_amd64  android_arm  android_arm64 \
-  linux_386    linux_amd64    linux_arm    linux_arm64 \
-  windows_386  windows_amd64  windows_arm  windows_arm64 \
-  \
-  darwin_amd64 darwin_arm64 \
-  ios_amd64    ios_arm64 \
-  \
-  freebsd_amd64 \
-  \
-  aix_ppc64 \
-  \
-  linux_loong64 linux_ppc64le linux_riscv64 linux_s390x
+  aix/ppc64 \
+  android/386 \
+  android/amd64 \
+  android/arm \
+  android/arm64 \
+  darwin/amd64 \
+  darwin/arm64 \
+  freebsd/amd64 \
+  ios/amd64 \
+  ios/arm64 \
+  linux/386 \
+  linux/amd64 \
+  linux/arm \
+  linux/arm64 \
+  linux/loong64 \
+  linux/ppc64le \
+  linux/riscv64 \
+  linux/s390x \
+  openbsd/arm64 \
+  windows/386 \
+  windows/amd64 \
+  windows/arm \
+  windows/arm64
 
 # From https://go.dev/src/cmd/go/internal/work/init.go
-go_pie_install_suffix=$(if $(filter $(1),aix_ppc64 windows_386 windows_amd64 windows_arm windows_arm64),,shared)
+go_pie_install_suffix=$(if $(filter $(1),aix/ppc64 windows/386 windows/amd64 windows/arm windows/arm64),,shared)
 
 ifneq ($(filter $(GO_HOST_OS_ARCH),$(GO_PIE_SUPPORTED_OS_ARCH)),)
   GO_HOST_PIE_SUPPORTED:=1
