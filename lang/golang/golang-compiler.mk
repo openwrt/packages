openwrt/packages

commit c137c382573f114902607546c5ed5b562df0b9b0

golang: new packages
From golang.org:
The Go programming language is an open source project to make
programmers more productive.

This commit consists of two "parts":

* golang/host: Main Go compiler for host (installed to
  STAGING_DIR_HOST/lib/go-cross), used to cross-compile Go programs to
  be packaged.

* golang (and golang-src/golang-doc): Main Go compiler for on-target
  development. These packages are quite large, but I would expect only
  developers to install these.

Signed-off-by: Jeffery To <jeffery.to@gmail.com>

lang/golang/golang-compiler.mk
@@ -1,0 +1,186 @@
+#
+# Copyright (C) 2018 Jeffery To
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+ifeq ($(origin GO_INCLUDE_DIR),undefined)
+  GO_INCLUDE_DIR:=$(dir $(lastword $(MAKEFILE_LIST)))
+endif
+
+include $(GO_INCLUDE_DIR)/golang-values.mk
+
+
+# $(1) valid GOOS_GOARCH combinations
+# $(2) go version id
+define GoCompiler/Default/CheckHost
+	$(if $(filter $(GO_HOST_OS_ARCH),$(1)),,$(error go-$(2) cannot be installed on $(GO_HOST_OS)/$(GO_HOST_ARCH)))
+endef
+
+# $(1) source go root
+# $(2) destination prefix
+# $(3) go version id
+# $(4) additional environment variables (optional)
+define GoCompiler/Default/Make
+	( \
+		cd $(1)/src ; \
+		$(if $(2),GOROOT_FINAL=$(2)/lib/go-$(3)) \
+		$(4) \
+		$(BASH) make.bash --no-banner ; \
+	)
+endef
+
+# $(1) destination prefix
+# $(2) go version id
+define GoCompiler/Default/Install/make-dirs
+	$(INSTALL_DIR) $(1)/lib/go-$(2)
+	$(INSTALL_DIR) $(1)/share/go-$(2)
+endef
+
+# $(1) source go root
+# $(2) destination prefix
+# $(3) go version id
+# $(4) file/directory name
+define GoCompiler/Default/Install/install-share-data
+	$(CP) $(1)/$(4) $(2)/share/go-$(3)/
+	$(LN) ../../share/go-$(3)/$(4) $(2)/lib/go-$(3)/
+endef
+
+# $(1) source go root
+# $(2) destination prefix
+# $(3) go version id
+# $(4) GOOS_GOARCH
+define GoCompiler/Default/Install/Bin
+	$(call GoCompiler/Default/Install/make-dirs,$(2),$(3))
+
+	$(call GoCompiler/Default/Install/install-share-data,$(1),$(2),$(3),api)
+
+	$(INSTALL_DATA) -p $(1)/VERSION $(2)/lib/go-$(3)/
+
+	for file in AUTHORS CONTRIBUTING.md CONTRIBUTORS LICENSE PATENTS README README.md; do \
+		if [ -f $(1)/$$$$file ]; then \
+			$(INSTALL_DATA) -p $(1)/$$$$file $(2)/share/go-$(3)/ ; \
+		fi ; \
+	done
+
+	$(INSTALL_DIR) $(2)/lib/go-$(3)/bin
+
+  ifeq ($(4),$(GO_HOST_OS_ARCH))
+	$(INSTALL_BIN) -p $(1)/bin/* $(2)/lib/go-$(3)/bin/
+  else
+	$(INSTALL_BIN) -p $(1)/bin/$(4)/* $(2)/lib/go-$(3)/bin/
+  endif
+
+	$(INSTALL_DIR) $(2)/lib/go-$(3)/pkg
+	$(CP) $(1)/pkg/$(4) $(2)/lib/go-$(3)/pkg/
+
+	$(INSTALL_DIR) $(2)/lib/go-$(3)/pkg/tool/$(4)
+	$(INSTALL_BIN) -p $(1)/pkg/tool/$(4)/* $(2)/lib/go-$(3)/pkg/tool/$(4)/
+endef
+
+# $(1) destination prefix
+# $(2) go version id
+define GoCompiler/Default/Install/BinLinks
+	$(INSTALL_DIR) $(1)/bin
+	$(LN) ../lib/go-$(2)/bin/go $(1)/bin/go
+	$(LN) ../lib/go-$(2)/bin/gofmt $(1)/bin/gofmt
+endef
+
+# $(1) source go root
+# $(2) destination prefix
+# $(3) go version id
+define GoCompiler/Default/Install/Doc
+	$(call GoCompiler/Default/Install/make-dirs,$(2),$(3))
+
+	$(call GoCompiler/Default/Install/install-share-data,$(1),$(2),$(3),doc)
+	$(call GoCompiler/Default/Install/install-share-data,$(1),$(2),$(3),favicon.ico)
+	$(call GoCompiler/Default/Install/install-share-data,$(1),$(2),$(3),robots.txt)
+endef
+
+# $(1) source go root
+# $(2) destination prefix
+# $(3) go version id
+define GoCompiler/Default/Install/Src
+	$(call GoCompiler/Default/Install/make-dirs,$(2),$(3))
+
+	$(call GoCompiler/Default/Install/install-share-data,$(1),$(2),$(3),lib)
+	$(call GoCompiler/Default/Install/install-share-data,$(1),$(2),$(3),misc)
+	$(call GoCompiler/Default/Install/install-share-data,$(1),$(2),$(3),src)
+	$(call GoCompiler/Default/Install/install-share-data,$(1),$(2),$(3),test)
+
+	$(FIND) \
+		$(2)/share/go-$(3)/src/ \
+		\! -type d -a \( -name '*.bat' -o -name '*.rc' \) \
+		-delete
+
+	if [ -d $(1)/pkg/include ]; then \
+		$(INSTALL_DIR) $(2)/lib/go-$(3)/pkg ; \
+		$(INSTALL_DIR) $(2)/share/go-$(3)/pkg ; \
+		$(CP) $(1)/pkg/include $(2)/share/go-$(3)/pkg/ ; \
+		$(LN) ../../../share/go-$(3)/pkg/include $(2)/lib/go-$(3)/pkg/ ; \
+	fi
+endef
+
+# $(1) destination prefix
+# $(2) go version id
+define GoCompiler/Default/Uninstall
+	rm -rf $(1)/lib/go-$(2)
+	rm -rf $(1)/share/go-$(2)
+endef
+
+# $(1) destination prefix
+define GoCompiler/Default/Uninstall/BinLinks
+	rm -f $(1)/bin/go
+	rm -f $(1)/bin/gofmt
+endef
+
+
+# $(1) profile name
+# $(2) source go root
+# $(3) destination prefix
+# $(4) go version id
+# $(5) GOOS_GOARCH
+define GoCompiler/AddProfile
+
+  # $$(1) valid GOOS_GOARCH combinations
+  define GoCompiler/$(1)/CheckHost
+	$$(call GoCompiler/Default/CheckHost,$$(1),$(4))
+  endef
+
+  # $$(1) additional environment variables (optional)
+  define GoCompiler/$(1)/Make
+	$$(call GoCompiler/Default/Make,$(2),$(3),$(4),$$(1))
+  endef
+
+  # $$(1) override install prefix (optional)
+  define GoCompiler/$(1)/Install/Bin
+	$$(call GoCompiler/Default/Install/Bin,$(2),$$(or $$(1),$(3)),$(4),$(5))
+  endef
+
+  # $$(1) override install prefix (optional)
+  define GoCompiler/$(1)/Install/BinLinks
+	$$(call GoCompiler/Default/Install/BinLinks,$$(or $$(1),$(3)),$(4))
+  endef
+
+  # $$(1) override install prefix (optional)
+  define GoCompiler/$(1)/Install/Doc
+	$$(call GoCompiler/Default/Install/Doc,$(2),$$(or $$(1),$(3)),$(4))
+  endef
+
+  # $$(1) override install prefix (optional)
+  define GoCompiler/$(1)/Install/Src
+	$$(call GoCompiler/Default/Install/Src,$(2),$$(or $$(1),$(3)),$(4))
+  endef
+
+  # $$(1) override install prefix (optional)
+  define GoCompiler/$(1)/Uninstall
+	$$(call GoCompiler/Default/Uninstall,$$(or $$(1),$(3)),$(4))
+  endef
+
+  # $$(1) override install prefix (optional)
+  define GoCompiler/$(1)/Uninstall/BinLinks
+	$$(call GoCompiler/Default/Uninstall/BinLinks,$$(or $$(1),$(3)))
+  endef
+
+endef

lang/golang/golang-values.mk
@@ -1,0 +1,72 @@
+#
+# Copyright (C) 2018 Jeffery To
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+ifeq ($(origin GO_INCLUDE_DIR),undefined)
+  GO_INCLUDE_DIR:=$(dir $(lastword $(MAKEFILE_LIST)))
+endif
+
+include $(GO_INCLUDE_DIR)/golang-version.mk
+
+
+unexport \
+  GOARCH GOBIN GOCACHE GODEBUG GOHOSTARCH GOOS GOPATH GORACE GOROOT GOTMPDIR GCCGO \
+  CGO_ENABLED \
+  CGO_CFLAGS CGO_CFLAGS_ALLOW CGO_CFLAGS_DISALLOW \
+  CGO_CPPFLAGS CGO_CPPFLAGS_ALLOW CGO_CPPFLAGS_DISALLOW \
+  CGO_CXXFLAGS CGO_CXXFLAGS_ALLOW CGO_CXXFLAGS_DISALLOW \
+  CGO_FFLAGS CGO_FFLAGS_ALLOW CGO_FFLAGS_DISALLOW \
+  CGO_LDFLAGS CGO_LDFLAGS_ALLOW CGO_LDFLAGS_DISALLOW \
+  GOARM GO386 GOMIPS \
+  GOROOT_FINAL GO_EXTLINK_ENABLED GIT_ALLOW_PROTOCOL \
+  CC_FOR_TARGET CXX_FOR_TARGET GO_DISTFLAGS GO_GCFLAGS GO_LDFLAGS GOBUILDTIMELOGFILE GOROOT_BOOTSTRAP \
+  BOOT_GO_GCFLAGS GOEXPERIMENT GOBOOTSTRAP_TOOLEXEC
+  # there are more magic environment variables to track down, but ain't nobody got time for that
+
+go_arch=$(subst \
+  aarch64,arm64,$(subst \
+  i386,386,$(subst \
+  mipsel,mipsle,$(subst \
+  mips64el,mips64le,$(subst \
+  powerpc64,ppc64,$(subst \
+  x86_64,amd64,$(1)))))))
+
+GO_OS:=linux
+GO_ARCH:=$(call go_arch,$(ARCH))
+GO_OS_ARCH:=$(GO_OS)_$(GO_ARCH)
+
+GO_HOST_OS:=$(call tolower,$(HOST_OS))
+GO_HOST_ARCH:=$(call go_arch,$(subst \
+  armv6l,arm,$(subst \
+  armv7l,arm,$(subst \
+  i486,i386,$(subst \
+  i586,i386,$(subst \
+  i686,i386,$(HOST_ARCH)))))))
+GO_HOST_OS_ARCH:=$(GO_HOST_OS)_$(GO_HOST_ARCH)
+
+GO_HOST_TARGET_SAME:=$(if $(and $(findstring $(GO_OS_ARCH),$(GO_HOST_OS_ARCH)),$(findstring $(GO_HOST_OS_ARCH),$(GO_OS_ARCH))),1)
+GO_HOST_TARGET_DIFFERENT:=$(if $(GO_HOST_TARGET_SAME),,1)
+
+# ensure binaries can run on older CPUs
+GO_386:=387
+
+GO_ARM:=$(if $(CONFIG_arm_v7),7,$(if $(CONFIG_arm_v6),6,$(if $(findstring $(GO_ARCH),arm),5,)))
+
+GO_MIPS:=$(if $(filter $(GO_ARCH),mips mipsle),$(if $(CONFIG_HAS_FPU),hardfloat,softfloat),)
+
+# -fno-plt: causes "unexpected GOT reloc for non-dynamic symbol" errors
+# -mips32r2: conflicts with -march=mips32 set by go
+GO_CFLAGS_TO_REMOVE:=$(if \
+$(filter $(GO_ARCH),386),-fno-plt,$(if \
+$(filter $(GO_ARCH),mips mipsle),-mips32r2,))
+
+# mips64 / mips64el doesn't have softfloat support yet
+# https://github.com/golang/go/issues/14635
+GO_ARCH_DEPENDS:=@(aarch64||arm||i386||i686||mips||mipsel||powerpc64||x86_64)
+
+GO_TARGET_PREFIX:=/usr
+GO_TARGET_VERSION_ID:=$(GO_VERSION_MAJOR_MINOR)
+GO_TARGET_ROOT:=$(GO_TARGET_PREFIX)/lib/go-$(GO_TARGET_VERSION_ID)

lang/golang/golang-version.mk
@@ -1,0 +1,14 @@
+#
+# Copyright (C) 2018 Jeffery To
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+ifeq ($(origin GO_INCLUDE_DIR),undefined)
+  GO_INCLUDE_DIR:=$(dir $(lastword $(MAKEFILE_LIST)))
+endif
+
+
+GO_VERSION_MAJOR_MINOR:=1.10
+GO_VERSION_PATCH:=

commit 3195a5803c058e60091e2bcf92a753cd5acfee11

golang: split src into separate packages
Split misc sources and test data into separate packages. Reduces target
src package size by a third.

Move architecture-dependent generated source from src to the main
package.

Mark doc, misc, src and tests packages with PKGARCH:=all as they don't
have any architecture-specific files.

Fix stripping and strip compiler only.

Fixes: c137c382 ("golang: new packages")
Link: https://github.com/openwrt/packages/pull/28445
Suggested-by: Jeffery To <jeffery.to@gmail.com>
Signed-off-by: George Sapkin <george@sapk.in>

lang/golang/go-strip-helper
@@ -1,0 +1,16 @@
+#!/bin/sh
+#
+# Copyright (C) 2026 George Sapkin
+#
+# SPDX-License-Identifier: GPL-2.0-only
+
+for last; do :; done
+
+case "$last" in
+	*-doc|*-misc|*-src|*-tests)
+		echo "Not stripping: $last"
+		exit 0
+		;;
+esac
+
+"$@"

lang/golang/golang-bootstrap/Makefile
@@ -1,6 +1,6 @@
 #
-# Copyright (C) 2018-2023, Jeffery To
-# Copyright (C) 2025-2026, George Sapkin
+# Copyright (C) 2018-2023 Jeffery To
+# Copyright (C) 2025-2026 George Sapkin
 #
 # SPDX-License-Identifier: GPL-2.0-only
 
@@ -111,9 +111,8 @@ BOOTSTRAP_UNPACK:=$(HOST_TAR) -C "$(BOOTSTRAP_BUILD_DIR)" --strip-components=1 -
 BOOTSTRAP_1_17_UNPACK:=$(HOST_TAR) -C "$(BOOTSTRAP_1_17_BUILD_DIR)" --strip-components=1 -xzf "$(DL_DIR)/$(BOOTSTRAP_1_17_SOURCE)"
 BOOTSTRAP_1_20_UNPACK:=$(HOST_TAR) -C "$(BOOTSTRAP_1_20_BUILD_DIR)" --strip-components=1 -xzf "$(DL_DIR)/$(BOOTSTRAP_1_20_SOURCE)"
 
-# don't strip ELF executables in test data
+# Don't strip ELF executables in test data
 RSTRIP:=:
-STRIP:=:
 
 ifeq ($(GO_TARGET_SPECTRE_SUPPORTED),1)
   PKG_CONFIG_DEPENDS+=CONFIG_GOLANG_SPECTRE
@@ -253,13 +252,17 @@ define Host/Install
 
 	rm -rf "$(HOST_GO_ROOT)/pkg/$(GO_HOST_OS_ARCH)"
 
+	$(INSTALL_DIR) "$(STAGING_DIR_HOSTPKG)/bin"
+	$(INSTALL_BIN) ../go-strip-helper "$(STAGING_DIR_HOSTPKG)/bin"
+
 	$(INSTALL_DIR) "$(HOST_GO_ROOT)/openwrt"
-	$(INSTALL_BIN) ../go-gcc-helper "$(HOST_GO_ROOT)/openwrt/"
+	$(INSTALL_BIN) ../go-gcc-helper "$(HOST_GO_ROOT)/openwrt"
 	$(LN) go-gcc-helper "$(HOST_GO_ROOT)/openwrt/gcc"
 	$(LN) go-gcc-helper "$(HOST_GO_ROOT)/openwrt/g++"
 endef
 
 define Host/Uninstall
+	rm -f "$(STAGING_DIR_HOSTPKG)/bin/go-strip-helper"
 	rm -rf "$(HOST_GO_ROOT)/openwrt"
 
 	$(call GoCompiler/Host/Uninstall)

lang/golang/golang-compiler.mk
@@ -36,8 +36,22 @@ endef
 # 2: destination prefix
 # 3: go version id
 # 4: file/directory name
+# 5: filter (optional)
 define GoCompiler/Default/Install/install-lib-data
+
+  ifeq ($(5),)
 	$(CP) "$(1)/$(4)" "$(2)/lib/go-$(3)/"
+  else
+	$(INSTALL_DIR) "$(2)/lib/go-$(3)/$(4)"; \
+	cd "$(1)/$(4)" && \
+	$(FIND) . ! -type d -a \( $(5) \) -print0 | \
+	cpio \
+		--make-directories \
+		--null \
+		--pass-through \
+		--unconditional \
+		"$(2)/lib/go-$(3)/$(4)/"
+  endif
 endef
 
 # 1: source go root
@@ -45,6 +59,7 @@ endef
 # 3: go version id
 # 4: GOOS_GOARCH
 # 5: install suffix (optional)
+# 6: if target, package architecture-specific sources
 define GoCompiler/Default/Install/Bin
 	$(call GoCompiler/Default/Install/make-dirs,$(2),$(3))
 
@@ -53,7 +68,7 @@ define GoCompiler/Default/Install/Bin
 	$(INSTALL_DATA) -p "$(1)/go.env" "$(2)/lib/go-$(3)/"
 	$(INSTALL_DATA) -p "$(1)/VERSION" "$(2)/lib/go-$(3)/"
 
-	for file in CONTRIBUTING.md LICENSE PATENTS README.md SECURITY.md; do \
+	for file in $(strip $(if $(filter target,$(6)),$(GO_BIN_FILES),$(GO_LEGAL_FILES))); do \
 		if [ -f "$(1)/$$$$file" ]; then \
 			$(INSTALL_DATA) -p "$(1)/$$$$file" "$(2)/lib/go-$(3)/" ; \
 		fi ; \
@@ -96,23 +111,38 @@ endef
 # 1: source go root
 # 2: destination prefix
 # 3: go version id
+define GoCompiler/Default/Install/Misc
+	$(call GoCompiler/Default/Install/make-dirs,$(2),$(3))
+	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),misc)
+endef
+
+# 1: source go root
+# 2: destination prefix
+# 3: go version id
+# 4: if target, package architecture-specific sources
 define GoCompiler/Default/Install/Src
 	$(call GoCompiler/Default/Install/make-dirs,$(2),$(3))
-
-	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),lib)
-	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),misc)
-	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),src)
-	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),test)
-
-	$(FIND) \
-		"$(2)/lib/go-$(3)/src/" \
-		\! -type d -a \( -name "*.bat" -o -name "*.rc" \) \
-		-delete
+	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),src,$(strip \
+		$(if $(filter target,$(4)), \
+			$(GO_TARGET_SRC_FILTERS), \
+			$(GO_HOST_SRC_FILTERS) \
+		) \
+	))
 
 	if [ -d "$(1)/pkg/include" ]; then \
 		$(INSTALL_DIR) "$(2)/lib/go-$(3)/pkg" ; \
 		$(CP) "$(1)/pkg/include" "$(2)/lib/go-$(3)/pkg/" ; \
 	fi
+endef
+
+# 1: source go root
+# 2: destination prefix
+# 3: go version id
+define GoCompiler/Default/Install/Tests
+	$(call GoCompiler/Default/Install/make-dirs,$(2),$(3))
+	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),lib)
+	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),src,$(GO_TARGET_TEST_FILTERS))
+	$(call GoCompiler/Default/Install/install-lib-data,$(1),$(2),$(3),test)
 endef
 
 # 1: destination prefix
@@ -148,8 +178,9 @@ define GoCompiler/AddProfile
   endef
 
   # 1: override install prefix (optional)
+  # 2: if target, package architecture-specific sources
   define GoCompiler/$(1)/Install/Bin
-	$$(call GoCompiler/Default/Install/Bin,$(2),$$(or $$(1),$(3)),$(4),$(5),$(6))
+	$$(call GoCompiler/Default/Install/Bin,$(2),$$(or $$(1),$(3)),$(4),$(5),$(6),$$(2))
   endef
 
   # 1: override install prefix (optional)
@@ -163,8 +194,19 @@ define GoCompiler/AddProfile
   endef
 
   # 1: override install prefix (optional)
+  define GoCompiler/$(1)/Install/Misc
+	$$(call GoCompiler/Default/Install/Misc,$(2),$$(or $$(1),$(3)),$(4))
+  endef
+
+  # 1: override install prefix (optional)
+  # 2: if target, package architecture-specific sources
   define GoCompiler/$(1)/Install/Src
-	$$(call GoCompiler/Default/Install/Src,$(2),$$(or $$(1),$(3)),$(4))
+	$$(call GoCompiler/Default/Install/Src,$(2),$$(or $$(1),$(3)),$(4),$$(2))
+  endef
+
+  # 1: override install prefix (optional)
+  define GoCompiler/$(1)/Install/Tests
+	$$(call GoCompiler/Default/Install/Tests,$(2),$$(or $$(1),$(3)),$(4),$$(2))
   endef
 
   # 1: override install prefix (optional)

lang/golang/golang-values.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018-2023 Jeffery To
+# Copyright (C) 2025-2026 George Sapkin
 #
 # SPDX-License-Identifier: GPL-2.0-only
 
@@ -249,6 +250,30 @@ else ifeq ($(GO_ARCH),ppc64)
   GO_PPC64:=power8
 
 endif
+
+GO_GENERATED_FILES := \
+  src/cmd/cgo/zdefaultcc.go \
+  src/cmd/go/internal/cfg/zdefaultcc.go \
+  src/cmd/internal/objabi/zbootstrap.go \
+  src/go/build/zcgo.go \
+  src/internal/buildcfg/zbootstrap.go \
+  src/internal/runtime/sys/zversion.go \
+  src/time/tzdata/zzipdata.go
+
+GO_LEGAL_FILES := \
+  CONTRIBUTING.md \
+  LICENSE \
+  PATENTS \
+  README.md \
+  SECURITY.md
+
+GO_BIN_FILES := \
+  $(GO_GENERATED_FILES) \
+  $(GO_LEGAL_FILES)
+
+GO_HOST_SRC_FILTERS := ! -name '*.bat' -a ! -name '*.rc'
+GO_TARGET_SRC_FILTERS := ! -ipath '*/testdata/*' -a ! -name '*_test.go' -a ! -name '*.bat' -a ! -name '*.rc'
+GO_TARGET_TEST_FILTERS := -ipath '*/testdata/*' -o -name '*_test.go'
 
 
 # Target Go

lang/golang/golang-version.mk
@@ -1,6 +1,6 @@
 #
-# Copyright (C) 2018-2023, Jeffery To
-# Copyright (C) 2025-2026, George Sapkin
+# Copyright (C) 2018-2023 Jeffery To
+# Copyright (C) 2025-2026 George Sapkin
 #
 # SPDX-License-Identifier: GPL-2.0-only
 
@@ -17,9 +17,8 @@ include ../golang-values.mk
 PKG_UNPACK:=$(HOST_TAR) -C "$(PKG_BUILD_DIR)" --strip-components=1 -xzf "$(DL_DIR)/$(PKG_SOURCE)"
 HOST_UNPACK:=$(HOST_TAR) -C "$(HOST_BUILD_DIR)" --strip-components=1 -xzf "$(DL_DIR)/$(PKG_SOURCE)"
 
-# don't strip ELF executables in test data
-RSTRIP:=:
-STRIP:=:
+# Don't strip ELF executables in test data
+RSTRIP:=$(subst $(SCRIPT_DIR)/rstrip.sh,go-strip-helper $(SCRIPT_DIR)/rstrip.sh,$(RSTRIP))
 
 ifeq ($(GO_TARGET_SPECTRE_SUPPORTED),1)
   PKG_CONFIG_DEPENDS+=CONFIG_GOLANG_SPECTRE
@@ -79,6 +78,7 @@ endef
 define Package/$(PKG_NAME)-doc
   $(call Package/$(PKG_NAME)/Default)
   TITLE+= (documentation)
+  PKGARCH:=all
   PROVIDES:=@golang-doc
   $(if $(filter $(GO_DEFAULT_VERSION),$(GO_VERSION_MAJOR_MINOR)),DEFAULT_VARIANT:=1)
 endef
@@ -89,9 +89,24 @@ define Package/$(PKG_NAME)-doc/description
   This package provides the documentation for the Go programming language.
 endef
 
+define Package/$(PKG_NAME)-misc
+  $(call Package/$(PKG_NAME)/Default)
+  TITLE+= (misc source files)
+  PKGARCH:=all
+  PROVIDES:=@golang-misc
+  $(if $(filter $(GO_DEFAULT_VERSION),$(GO_VERSION_MAJOR_MINOR)),DEFAULT_VARIANT:=1)
+endef
+
+define Package/$(PKG_NAME)-misc/description
+  $(call Package/$(PKG_NAME)/Default/description)
+
+  This package provides the Go compiler miscellaneous sources.
+endef
+
 define Package/$(PKG_NAME)-src
   $(call Package/$(PKG_NAME)/Default)
   TITLE+= (source files)
+  PKGARCH:=all
   PROVIDES:=@golang-src
   $(if $(filter $(GO_DEFAULT_VERSION),$(GO_VERSION_MAJOR_MINOR)),DEFAULT_VARIANT:=1)
 endef
@@ -101,6 +116,22 @@ define Package/$(PKG_NAME)-src/description
 
   This package provides the Go programming language source files needed for
   cross-compilation.
+endef
+
+define Package/$(PKG_NAME)-tests
+  $(call Package/$(PKG_NAME)/Default)
+  TITLE+= (compiler tests)
+  DEPENDS+= +golang$(GO_VERSION_MAJOR_MINOR)-src
+  EXTRA_DEPENDS:=golang$(GO_VERSION_MAJOR_MINOR)-src (=$(PKG_VERSION)-r$(PKG_RELEASE))
+  PKGARCH:=all
+  PROVIDES:=@golang-tests
+  $(if $(filter $(GO_DEFAULT_VERSION),$(GO_VERSION_MAJOR_MINOR)),DEFAULT_VARIANT:=1)
+endef
+
+define Package/$(PKG_NAME)-tests/description
+  $(call Package/$(PKG_NAME)/Default/description)
+
+  This package provides the Go compiler tests for stdlib.
 endef
 
 
@@ -149,7 +180,7 @@ define Host/Install
 	rm -rf "$(HOST_GO_ROOT)/pkg/$(GO_HOST_OS_ARCH)"
 
 	$(INSTALL_DIR) "$(HOST_GO_ROOT)/openwrt"
-	$(INSTALL_BIN) ../go-gcc-helper "$(HOST_GO_ROOT)/openwrt/"
+	$(INSTALL_BIN) ../go-gcc-helper "$(HOST_GO_ROOT)/openwrt"
 	$(LN) go-gcc-helper "$(HOST_GO_ROOT)/openwrt/gcc"
 	$(LN) go-gcc-helper "$(HOST_GO_ROOT)/openwrt/g++"
 endef
@@ -242,19 +273,27 @@ define Build/Compile
 endef
 
 define Package/$(PKG_NAME)/install
-	$(call GoCompiler/Package/Install/Bin,$(1)$(PKG_GO_PREFIX))
+	$(call GoCompiler/Package/Install/Bin,$(1)$(PKG_GO_PREFIX),target)
 endef
 
 define Package/$(PKG_NAME)-doc/install
 	$(call GoCompiler/Package/Install/Doc,$(1)$(PKG_GO_PREFIX))
 endef
 
+define Package/$(PKG_NAME)-misc/install
+	$(call GoCompiler/Package/Install/Misc,$(1)$(PKG_GO_PREFIX))
+endef
+
 define Package/$(PKG_NAME)-src/install
-	$(call GoCompiler/Package/Install/Src,$(1)$(PKG_GO_PREFIX))
+	$(call GoCompiler/Package/Install/Src,$(1)$(PKG_GO_PREFIX),target)
+endef
+
+define Package/$(PKG_NAME)-tests/install
+	$(call GoCompiler/Package/Install/Tests,$(1)$(PKG_GO_PREFIX))
 endef
 
 # src/debug contains ELF executables as test data and they reference these
 # libraries we need to call this to pass CheckDependencies in package-pack.mk
-define Package/$(PKG_NAME)-src/extra_provides
+define Package/$(PKG_NAME)-tests/extra_provides
 	echo 'libc.so.6' libstdc++.so.6' libtiff.so.6' | tr ' ' '\n'
 endef

lang/golang/golang1.25/Makefile
@@ -1,6 +1,6 @@
 #
-# Copyright (C) 2018-2023, Jeffery To
-# Copyright (C) 2025-2026, George Sapkin
+# Copyright (C) 2018-2023 Jeffery To
+# Copyright (C) 2025-2026 George Sapkin
 #
 # SPDX-License-Identifier: GPL-2.0-only
 
@@ -13,7 +13,7 @@ GO_BOOTSTRAP_VERSION:=bootstrap
 PKG_HASH:=58cbf771e44d76de6f56d19e33b77d745a1e489340922875e46585b975c2b059
 
 PKG_VERSION:=$(GO_VERSION_MAJOR_MINOR)$(if $(GO_VERSION_PATCH),.$(GO_VERSION_PATCH))
-PKG_RELEASE:=1
+PKG_RELEASE:=2
 
 GO_SOURCE_URLS:=https://go.dev/dl/ \
                 https://dl.google.com/go/ \
@@ -78,4 +78,6 @@ include ../golang-version.mk
 $(eval $(call HostBuild))
 $(eval $(call BuildPackage,$(PKG_NAME)))
 $(eval $(call BuildPackage,$(PKG_NAME)-doc))
+$(eval $(call BuildPackage,$(PKG_NAME)-misc))
 $(eval $(call BuildPackage,$(PKG_NAME)-src))
+$(eval $(call BuildPackage,$(PKG_NAME)-tests))

lang/golang/golang1.25/test.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 case "$1" in
-	golang*doc|golang*src) exit ;;
+	golang*doc|golang*misc|golang*src|golang*test) exit ;;
 esac
 
 go version | grep -F " go$PKG_VERSION "