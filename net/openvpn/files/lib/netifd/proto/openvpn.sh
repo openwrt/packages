openwrt/packages

commit 2607b761549a4793eff91dcb60a287c05f631846

openvpn: introduce proto handler
openvpn needs a proto handler. Here it is.

Removed all of the up/down scripts from the init handler
and made those entirely optional (with some ucode examples).

The config options have been updated to reflect v 2.6/2.7,
with a 'd' flag to denote deprecated. Deprecated flags are
gated behind an 'allow_deprecated' config flag, which must
be on to use them. Some flags will cease to work in the next
version.

Users should not be using compression. Openvpn has enough
security holes and pitfalls already without using
compression.

Updated the example configs (left in place as legacy
documentation) and removed older cryptos which do not exist
in ovpn any longer.

A migration script is included -x. /etc/config/openvpn
entries become interface entries in /etc/config/network
with proto='openvpn'. The source config is retained.

Signed-off-by: Paul Donald <newtwen+github@gmail.com>
https://github.com/openwrt/packages/pull/28533

net/openvpn/Makefile
@@ -10,7 +10,7 @@ include $(TOPDIR)/rules.mk
 PKG_NAME:=openvpn
 
 PKG_VERSION:=2.6.14
-PKG_RELEASE:=4
+PKG_RELEASE:=5
 
 PKG_SOURCE_URL:=\
 	https://build.openvpn.net/downloads/releases/ \
@@ -90,11 +90,6 @@ define Build/Configure
 	)
 endef
 
-define Package/openvpn-$(BUILD_VARIANT)/conffiles
-/etc/config/openvpn
-/etc/openvpn.user
-endef
-
 define Package/openvpn-$(BUILD_VARIANT)/install
 	$(INSTALL_DIR) \
 		$(1)/usr/sbin \
@@ -102,7 +97,9 @@ define Package/openvpn-$(BUILD_VARIANT)/install
 		$(1)/etc/init.d \
 		$(1)/etc/config \
 		$(1)/etc/openvpn \
+		$(1)/etc/uci-defaults \
 		$(1)/lib/functions \
+		$(1)/lib/netifd/proto \
 		$(1)/lib/upgrade/keep.d \
 		$(1)/usr/libexec \
 		$(1)/etc/hotplug.d/openvpn
@@ -112,34 +109,26 @@ define Package/openvpn-$(BUILD_VARIANT)/install
 		$(1)/usr/sbin/
 
 	$(INSTALL_BIN) \
-		files/openvpn.init \
-		$(1)/etc/init.d/openvpn
+		files/lib/netifd/proto/openvpn.sh \
+		$(1)/lib/netifd/proto/
 
 	$(INSTALL_BIN) \
-		files/usr/libexec/openvpn-hotplug \
-		$(1)/usr/libexec/openvpn-hotplug
-
-	$(INSTALL_DATA) \
-		files/lib/functions/openvpn.sh \
-		$(1)/lib/functions/openvpn.sh
-
-	$(INSTALL_DATA) \
-		files/etc/hotplug.d/openvpn/01-user \
-		$(1)/etc/hotplug.d/openvpn/01-user
-
-	$(INSTALL_DATA) \
-		files/etc/openvpn.user \
-		$(1)/etc/openvpn.user
+		files/etc/uci-defaults/60_openvpn_migrate.sh \
+		$(1)/etc/uci-defaults/
 
 	$(INSTALL_DATA) \
 		files/openvpn.options \
 		$(1)/usr/share/openvpn/openvpn.options
 
-	$(INSTALL_CONF) files/openvpn.config \
-		$(1)/etc/config/openvpn
+	$(INSTALL_BIN) \
+		files/up.uc files/down.uc files/route-pre-down.uc files/route-up.uc \
+		files/ipchange.uc \
+		files/client-connect.uc files/client-disconnect.uc files/client-crresponse.uc \
+		files/auth-user-pass-verify.uc files/tls-verify.uc \
+		$(1)/usr/share/openvpn/
 
 	$(INSTALL_DATA) \
-		files/openvpn.upgrade \
+		files/openvpn.sysupgrade \
 		$(1)/lib/upgrade/keep.d/openvpn
 endef
 

net/openvpn/files/etc/hotplug.d/openvpn/01-user
@@ -1,27 +1,0 @@
-#!/bin/sh
-
-[ -e "/etc/openvpn.user" ] && {
-	env -i ACTION="$ACTION" INSTANCE="$INSTANCE" \
-		/bin/sh \
-		/etc/openvpn.user \
-		$*
-}
-
-# Wrap user defined scripts on up/down/route-up/route-pre-down/ipchange events
-# Scriptp set with up/down/route-up/route-pre-down/ipchange in the openvpn config are also executed with the command=user_xxxx
-case "$ACTION" in
-	up) command=$user_up ;;
-	down) command=$user_down ;;
-	route-up) command=$user_route_up ;;
-	route-pre-down) command=$user_route_pre_down ;;
-	ipchange) command=$user_ipchange ;;
-	*) command= ;;
-esac
-
-if [ -n "$command" ]; then
-	shift
-	exec /bin/sh -c "$command $*"
-fi
-
-exit 0
-

net/openvpn/files/etc/openvpn.user
@@ -1,11 +1,0 @@
-#!/bin/sh
-#
-# This file is interpreted as shell script.
-# Put your custom openvpn action here, they will
-# be executed with each opevnp event.
-#
-# $ACTION
-#      <down>    down action is generated after the TUN/TAP device is closed
-#      <up>      up action is generated after the TUN/TAP device is opened
-# $INSTANCE  Name of the openvpn instance which went up or down
-

net/openvpn/files/lib/netifd/proto/openvpn.sh
@@ -1,0 +1,189 @@
+#!/bin/sh
+# OpenVPN netifd proto handler for OpenWrt
+# Copyright (C) 2026
+# shellcheck disable=SC1091,2046,2091,3043,3060
+
+[ -x /usr/sbin/openvpn ] || exit 0
+
+[ -n "$INCLUDE_ONLY" ] || {
+	. /lib/functions.sh
+	. /usr/share/openvpn/openvpn.options # OPENVPN_* options
+	. ../netifd-proto.sh
+	. /usr/share/libubox/jshn.sh
+	init_proto "$@"
+}
+
+# Helper to DRY up repeated option handling in init/setup
+option_builder() {
+	# option_builder <action:add|build> <LIST_VAR_NAME> <type>
+	local action="$1"; shift
+	local list_var="$1"; shift
+	local opt_type="$1"; shift
+	local f v
+
+	for f in $(eval echo \$"$list_var")
+	do
+		f=${f%%:*}
+
+		if [ "$action" = "add" ]; then
+			case "$opt_type" in
+				bool) proto_config_add_boolean "$f:bool" ;;
+				protobool) proto_config_add_boolean "$f:protobool" ;;
+				uinteger) proto_config_add_int "$f:uinteger" ;;
+				integer) proto_config_add_int "$f:integer" ;;
+				string) proto_config_add_string "$f:string" ;;
+				protostring) proto_config_add_string "$f:protostring" ;;
+				file) proto_config_add_string "$f:file" ;;
+				list) proto_config_add_array "$f:list" ;;
+			esac
+		elif [ "$action" = "build" ]; then
+			[ "${f#*:}" = "d" ] && [ "$allow_deprecated" = 0 ] && continue
+			case "$opt_type" in
+				bool)
+					json_get_var v "$f"
+					[ "$v" = 1 ] && append exec_params " --${f//_/-}"
+					;;
+				uinteger|integer|string)
+					json_get_var v "$f"
+					[ -n "$v" ] && append exec_params " --${f//_/-} $v"
+					;;
+				file)
+					json_get_var v "$f"
+					[ -f "$v" ] || continue
+					[ -n "$v" ] && append exec_params " --${f//_/-} $v"
+					;;
+				list)
+					json_get_values v "$f"
+					[ -n "${v}" ] && append exec_params "$(for d in $v; do echo " --${f//_/-} $d"; done)"
+					;;
+			esac
+		fi
+	done
+}
+
+
+# Not real config params used by openvpn - only by our proto handler
+PROTO_BOOLS='
+allow_deprecated
+'
+
+PROTO_STRINGS='
+username
+password
+cert_password
+'
+
+proto_openvpn_init_config() {
+	available=1
+	no_device=1
+	lasterror=1
+	renew_handler=1
+
+	# There may be opvnvpn options which mean that a tap L2 device exists.
+	# TODO: Set no_device to depend on tap device
+
+	proto_add_dynamic_defaults
+
+	# Add proto config options - netifd compares these for changes between interface events
+	option_builder add PROTO_BOOLS protobool
+	option_builder add PROTO_STRINGS string
+	option_builder add OPENVPN_BOOLS bool
+	option_builder add OPENVPN_UINTS uinteger
+	option_builder add OPENVPN_INTS integer
+	option_builder add OPENVPN_PARAMS_STRING string
+	option_builder add OPENVPN_PARAMS_FILE file
+	option_builder add OPENVPN_LIST list
+
+}
+
+
+proto_openvpn_setup() {
+	local config="$1"
+	local allow_deprecated exec_params
+	allow_deprecated=0
+
+	exec_params=
+
+	json_get_var allow_deprecated allow_deprecated
+
+	# Build exec params from configured options we get from ubus values stored during init_config
+	option_builder build OPENVPN_BOOLS bool
+	option_builder build OPENVPN_UINTS uinteger
+	option_builder build OPENVPN_INTS integer
+	option_builder build OPENVPN_PARAMS_STRING string
+	option_builder build OPENVPN_PARAMS_FILE file
+	option_builder build OPENVPN_LIST list
+
+	proto_add_dynamic_defaults
+
+	json_get_var username username
+	json_get_var password password
+	json_get_var cert_password cert_password
+	json_get_var config_file config
+
+	mkdir -p /var/run
+	# combine into --askpass:
+	if [ -n "$cert_password" ]; then
+		cp_file="/var/run/openvpn.$config.pass"
+		umask 077
+		printf '%s\n' "${cert_password:-}" > "$cp_file"
+		umask 022
+		append exec_params " --askpass $cp_file"
+	elif [ -n "$askpass" ]; then
+		append exec_params " --askpass $askpass"
+	fi
+
+	# combine into --auth-user-pass:
+	if [ -n "$username" ] || [ -n "$password" ]; then
+		auth_file="/var/run/openvpn.$config.auth"
+		umask 077
+		printf '%s\n' "${username:-}" "${password:-}" > "$auth_file"
+		umask 022
+		append exec_params " --auth-user-pass $auth_file"
+	elif [ -n "$auth_user_pass" ]; then
+		auth_file="$auth_user_pass"
+	fi
+
+	# shellcheck disable=SC2154
+	cd_dir="${config_file%/*}"
+	[ "$cd_dir" = "$config_file" ] && cd_dir="/"
+
+	# Testing option
+	# ${tls_exit:+--tls-exit} \
+
+	json_get_var dev_type dev_type
+	# shellcheck disable=SC2086
+	proto_run_command "$config" openvpn \
+		$([ -z "$dev_type" ] && echo " --dev-type tun") \
+		--cd "$cd_dir" \
+		--status "/var/run/openvpn.$config.status" \
+		--syslog "openvpn_$config" \
+		--tmp-dir "/var/run" \
+		$exec_params
+
+	# last param wins; user provided status or syslog supersedes these.
+
+}
+
+proto_openvpn_renew() {
+	config="$1"
+	local sigusr1
+
+	sigusr1="$(kill -l SIGUSR1)"
+	[ -n "$sigusr1" ] && proto_kill_command "$config" "$sigusr1"
+
+}
+
+proto_openvpn_teardown() {
+	local iface="$1"
+	rm -f \
+		"/var/run/openvpn.$iface.pass" \
+		"/var/run/openvpn.$iface.auth" \
+		"/var/run/openvpn.$iface.status" 
+	proto_kill_command "$iface"
+}
+
+
+[ -n "$INCLUDE_ONLY" ] || {
+	add_protocol openvpn
+}

net/openvpn/files/usr/libexec/openvpn-hotplug
@@ -1,10 +1,0 @@
-#!/bin/sh
-
-ACTION=$1
-shift
-INSTANCE=$1
-shift
-
-export ACTION=$ACTION
-export INSTANCE=$INSTANCE
-exec /sbin/hotplug-call openvpn "$@"

net/openvpn/files/lib/functions/openvpn.sh
@@ -1,16 +1,0 @@
-#!/bin/sh
-
-get_openvpn_option() {
-	local config="$1"
-	local variable="$2"
-	local option="$3"
-
-	local value="$(sed -rne 's/^[ \t]*'"$option"'[ \t]+'"'([^']+)'"'[ \t]*$/\1/p' "$config" | tail -n1)"
-	[ -n "$value" ] || value="$(sed -rne 's/^[ \t]*'"$option"'[ \t]+"(([^"\\]|\\.)+)"[ \t]*$/\1/p' "$config" | tail -n1 | sed -re 's/\\(.)/\1/g')"
-	[ -n "$value" ] || value="$(sed -rne 's/^[ \t]*'"$option"'[ \t]+(([^ \t\\]|\\.)+)[ \t]*$/\1/p' "$config" | tail -n1 | sed -re 's/\\(.)/\1/g')"
-	[ -n "$value" ] || return 1
-
-	export -n "$variable=$value"
-	return 0
-}
-

net/openvpn/files/auth-user-pass-verify.uc
@@ -1,0 +1,21 @@
+#!/usr/bin/env ucode
+
+// cmd method { via-env|via-file } 
+
+print('cmd ', ARGV, '\n');
+
+// See the environment variables passed to this script: https://ucode.mein.io/module-core.html#getenv
+print(getenv(), '\n');
+
+// https://openvpn.net/community-docs/community-articles/openvpn-2-6-manual.html#environmental-variables-177179
+// https://openvpn.net/community-docs/community-articles/openvpn-2-6-manual.html#script-hooks-177179
+
+
+/* do something */
+
+
+/*
+exit(0); // client auth request accepted
+exit(1); // reject client
+exit(2); // deferred auth
+*/

net/openvpn/files/client-connect.uc
@@ -1,0 +1,18 @@
+#!/usr/bin/env ucode
+
+// cmd [args]
+
+print('cmd ', ARGV, '\n');
+
+// See the environment variables passed to this script: https://ucode.mein.io/module-core.html#getenv
+print(getenv(), '\n');
+
+// https://openvpn.net/community-docs/community-articles/openvpn-2-6-manual.html#environmental-variables-177179
+// https://openvpn.net/community-docs/community-articles/openvpn-2-6-manual.html#script-hooks-177179
+
+
+/* do something */
+
+
+// If script returns a non-zero error status, it will cause the client to be disconnected.
+exit(0);

commit a65a2f0fba0a3ac21a5575567046524c582cc2f8

openvpn: add hotplug handling back in
This commit adds hotplug handling back in.

Fixes: 2607b761 ("openvpn: introduce proto handler")

Signed-off-by: Florian Eckert <fe@dev.tdt.de>

net/openvpn/Makefile
@@ -90,6 +90,10 @@ define Build/Configure
 	)
 endef
 
+define Package/openvpn-$(BUILD_VARIANT)/conffiles
+/etc/openvpn.user
+endef
+
 define Package/openvpn-$(BUILD_VARIANT)/install
 	$(INSTALL_DIR) $(1)/usr/sbin
 	$(INSTALL_BIN) \
@@ -127,6 +131,21 @@ define Package/openvpn-$(BUILD_VARIANT)/install
 	$(INSTALL_DATA) \
 		files/lib/upgrade/keep.d/openvpn \
 		$(1)/lib/upgrade/keep.d/
+
+	$(INSTALL_DIR) $(1)/usr/libexec
+	$(INSTALL_BIN) \
+		files/usr/libexec/openvpn-hotplug \
+		$(1)/usr/libexec/
+
+	$(INSTALL_DIR) $(1)/etc
+	$(INSTALL_DATA) \
+		files/etc/openvpn.user \
+		$(1)/etc/
+
+	$(INSTALL_DIR) $(1)/etc/hotplug.d/openvpn
+	$(INSTALL_DATA) \
+		files/etc/hotplug.d/openvpn/01-user \
+		$(1)/etc/hotplug.d/openvpn/
 endef
 
 $(eval $(call BuildPackage,openvpn-openssl))

net/openvpn/files/etc/hotplug.d/openvpn/01-user
@@ -1,0 +1,39 @@
+#!/bin/sh
+
+main() {
+	local command
+	[ -e "/etc/openvpn.user" ] && {
+		env -i ACTION="$ACTION" INSTANCE="$INSTANCE" \
+			/bin/sh \
+			/etc/openvpn.user \
+			$*
+	}
+
+	# Wrap user defined scripts on up|down|route-up|route-pre-down|ipchange
+	# events. Scripts set with up|down|route-up|route-pre-down|ipchange
+	# in the openvpn config are also executed with the command=user_xxxx
+	case "$ACTION" in
+	up)
+		command=$user_up
+		;;
+	down)
+		command=$user_down
+		;;
+	route-up)
+		command=$user_route_up
+		;;
+	route-pre-down)
+		command=$user_route_pre_down
+		;;
+	ipchange)
+		command=$user_ipchange
+		;;
+	esac
+
+	if [ -n "$command" ]; then
+		shift
+		exec /bin/sh -c "$command $*"
+	fi
+}
+
+main

net/openvpn/files/etc/openvpn.user
@@ -1,0 +1,11 @@
+#!/bin/sh
+#
+# This file is interpreted as shell script.
+# Put your custom openvpn action here, they will
+# be executed with each opevnp event.
+#
+# $ACTION
+#      <down>    down action is generated after the TUN/TAP device is closed
+#      <up>      up action is generated after the TUN/TAP device is opened
+# $INSTANCE  Name of the openvpn instance which went up or down
+

net/openvpn/files/lib/netifd/proto/openvpn.sh
@@ -162,6 +162,28 @@ proto_openvpn_setup() {
 	# Testing option
 	# ${tls_exit:+--tls-exit} \
 
+	# Add hotplug handling
+	append exec_params " --script-security 3"
+
+	append exec_params " --up '/usr/libexec/openvpn-hotplug up $config'"
+	[ -n "$up" ] && append exec_params " --setenv user_up '$up'"
+
+	append exec_params " --down '/usr/libexec/openvpn-hotplug down $config'"
+	[ -n "$down" ] && append exec_params " --setenv user_down '$down'"
+
+	append exec_params " --route-up '/usr/libexec/openvpn-hotplug route-up $config'"
+	[ -n "$route_up" ] && append exec_params " --setenv user_route_up '$route_up'"
+
+	append exec_params " --route-pre-down '/usr/libexec/openvpn-hotplug route-pre-down $config'"
+	[ -n "$route_pre_down" ] && append exec_params " --setenv user_route_pre_down '$route_pre_down'"
+
+	json_get_var client client
+	json_get_var tls_client tls_client
+	if [ "$client" = 1 ] || [ "$tls_client" = 1 ]; then
+		append exec_params " --ipchange '/usr/libexec/openvpn-hotplug ipchange $config'"
+		[ -n "$ip_change" ] && append exec_params " --setenv user_ipchange '$ipchange'"
+	fi
+
 	# shellcheck disable=SC2086
 	eval "proto_run_command '$config' openvpn $exec_params"
 

net/openvpn/files/usr/libexec/openvpn-hotplug
@@ -1,0 +1,10 @@
+#!/bin/sh
+
+ACTION=$1
+shift
+INSTANCE=$1
+shift
+
+export ACTION=$ACTION
+export INSTANCE=$INSTANCE
+exec /sbin/hotplug-call openvpn "$@"