1033b7a19d903755627d617c97a7d990034612c1
Alexandru Ardelean <alex@shruggie.ro>
2026-04-22 12:16:55 +0300

    hwdata: update to 0.406
    Updates PCI, USB, OUI, and other hardware ID databases.
    
    Changelog: https://github.com/vcrhonek/hwdata/commits/v0.406
    
    Signed-off-by: Alexandru Ardelean <alex@shruggie.ro>

--- utils/hwdata/test.sh
+++ utils/hwdata/test.sh
@@ -3,11 +3,14 @@
 case "$1" in
 	pciids)
 		test -s /usr/share/hwdata/pci.ids
+		grep -q "^8086" /usr/share/hwdata/pci.ids
 		;;
 	usbids)
 		test -s /usr/share/hwdata/usb.ids
+		grep -q "^0781" /usr/share/hwdata/usb.ids
 		;;
 	pnpids)
 		test -s /usr/share/hwdata/pnp.ids
+		grep -q "PNP" /usr/share/hwdata/pnp.ids
 		;;
 esac
