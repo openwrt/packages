fed2091da47c03ee80e38eaaac318556814db3c7
George Sapkin <george@sapk.in>
2026-05-11 22:53:02 +0300

    checksec: mark version check override as executable
    Mark version check override as executable and add explicit package checks.
    
    Signed-off-by: George Sapkin <george@sapk.in>

--- utils/checksec/test-version.sh
+++ utils/checksec/test-version.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+# shellckeck shell=busybox
+
 # checksec reported version doesn't match package version as of 3.1.0
 
-case "$1" in
+case "$PKG_NAME" in
 checksec)
 	checksec --version 2>&1 | grep -qF "2.7.1"
+	;;
+
+*)
+	echo "Untested package: $PKG_NAME" >&2
+	exit 1
 	;;
 esac
