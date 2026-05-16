d4d4f77a7b3b51c838ea304e33fb69fc58eb4c51
George Sapkin <george@sapk.in>
2026-05-16 18:29:09 +0300

    python3: add more packages to version check overrides
    Add source packages and library to version check overrides.
    
    Fixes: b5d3a38e ("python3: move version checks to override")
    Signed-off-by: George Sapkin <george@sapk.in>

--- lang/python/python3/test-version.sh
+++ lang/python/python3/test-version.sh
@@ -9,27 +9,47 @@ python3-light)
 	python3 --version | grep -Fx "Python $PKG_VERSION"
 	;;
 
+libpython3-3.14|\
 python3-asyncio|\
+python3-asyncio-src|\
 python3-base-src|\
 python3-codecs|\
+python3-codecs-src|\
 python3-ctypes|\
+python3-ctypes-src|\
 python3-dbm|\
+python3-dbm-src|\
 python3-decimal|\
+python3-decimal-src|\
 python3-dev|\
+python3-dev-src|\
 python3-light-src|\
 python3-logging|\
+python3-logging-src|\
 python3-lzma|\
+python3-lzma-src|\
 python3-multiprocessing|\
+python3-multiprocessing-src|\
 python3-ncurses|\
+python3-ncurses-src|\
 python3-openssl|\
+python3-openssl-src|\
 python3-pydoc|\
+python3-pydoc-src|\
 python3-readline|\
+python3-readline-src|\
 python3-sqlite3|\
+python3-sqlite3-src|\
 python3-unittest|\
+python3-unittest-src|\
 python3-urllib|\
+python3-urllib-src|\
 python3-uuid|\
+python3-uuid-src|\
 python3-venv|\
-python3-xml)
+python3-venv-src|\
+python3-xml|\
+python3-xml-src)
 	exit 0
 	;;
 
