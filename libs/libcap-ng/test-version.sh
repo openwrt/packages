cb2711a35fb4fdee82c9850002aa74c89402c95a
George Sapkin <george@sapk.in>
2026-05-16 18:32:28 +0300

    libcap-ng: fix version check override
    Fixes: 12b01bad ("libcap-ng-bin: add test-version.sh to skip version check")
    Signed-off-by: George Sapkin <george@sapk.in>

--- libs/libcap-ng/test-version.sh
+++ libs/libcap-ng/test-version.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+
+# shellckeck shell=busybox
+
 case "$1" in
-captest|filecap|netcap|pscap) exit 0 ;;
-*) echo "Untested package: $1" >&2; exit 1 ;;
+libcap-ng|\
+libcap-ng-bin)
+	exit 0
+	;;
+*)
+	echo "Untested package: $1" >&2
+	exit 1
+	;;
 esac
