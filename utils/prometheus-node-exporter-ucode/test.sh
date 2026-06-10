a69621931532adf77266e7f57a77fa1ca2c9b3ea
Ricard Nacher <ricard.nacher@midokura.com>
2026-06-10 16:54:07 +0200

    prometheus-node-exporter-ucode: add nat_traffic collector
    Add a nat_traffic collector for prometheus-node-exporter-ucode,
    ported from the equivalent Lua collector in prometheus-node-exporter-lua.
    
    Reads /proc/net/nf_conntrack and exports per-(src, dst) pair byte
    totals (both directions summed) as node_nat_traffic gauge metrics.
    
    Also includes a ucode-based unit test with a fixture file that can be
    run locally with: ucode test/nat_traffic.uc
    
    Signed-off-by: Ricard Nacher <ricard.nacher@midokura.com>

--- utils/prometheus-node-exporter-ucode/test.sh
+++ utils/prometheus-node-exporter-ucode/test.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-prometheus-node-exporter-ucode time
+case "$1" in
+prometheus-node-exporter-ucode-nat_traffic)
+	[ -f /proc/net/nf_conntrack ] && prometheus-node-exporter-ucode nat_traffic
+	;;
+*)
+	prometheus-node-exporter-ucode time
+	;;
+esac
