9e3e7f45f4c1b22607d6c35bf5bef0cbc516c651
Etienne Champetier <champetier.etienne@gmail.com>
2026-07-05 11:56:13 -0400

    prometheus-node-exporter-ucode: fixup CI
    Skip ci version check, return 0 when /proc/net/nf_conntrack is missing.
    
    Signed-off-by: Etienne Champetier <champetier.etienne@gmail.com>

--- utils/prometheus-node-exporter-ucode/test.sh
+++ utils/prometheus-node-exporter-ucode/test.sh
@@ -2,7 +2,7 @@
 
 case "$1" in
 prometheus-node-exporter-ucode-nat_traffic)
-	[ -f /proc/net/nf_conntrack ] && prometheus-node-exporter-ucode nat_traffic
+	[ -f /proc/net/nf_conntrack ] && prometheus-node-exporter-ucode nat_traffic || :
 	;;
 *)
 	prometheus-node-exporter-ucode time
