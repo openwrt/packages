openwrt/packages

commit a6e94447f744dcc568f5684f0f29043856884cc7

golang: fix missing dependency check
Add libraries used in test data to extra_provides to pass dependency
checks in package-pack.

Remove unnecessary dependencies.

Fixes: https://github.com/openwrt/packages/issues/27633
Fixes: b2119465 ("golang: Update to 1.24.0")
Link: https://github.com/openwrt/packages/pull/28445
Signed-off-by: George Sapkin <george@sapk.in>

lang/golang/golang-version.mk
@@ -92,7 +92,6 @@ endef
 define Package/$(PKG_NAME)-src
   $(call Package/$(PKG_NAME)/Default)
   TITLE+= (source files)
-  DEPENDS+= +libstdcpp +libtiff
   PROVIDES:=@golang-src
   $(if $(filter $(GO_DEFAULT_VERSION),$(GO_VERSION_MAJOR_MINOR)),DEFAULT_VARIANT:=1)
 endef
@@ -255,8 +254,7 @@ define Package/$(PKG_NAME)-src/install
 endef
 
 # src/debug contains ELF executables as test data and they reference these
-# libraries we need to call this in Package/$(GO_VERSION_MAJOR_MINOR)/extra_provides to pass
-# CheckDependencies in package-pack.mk
+# libraries we need to call this to pass CheckDependencies in package-pack.mk
 define Package/$(PKG_NAME)-src/extra_provides
-	echo 'libc.so.6'
+	echo 'libc.so.6' libstdc++.so.6' libtiff.so.6' | tr ' ' '\n'
 endef

commit ec09862ada5b7a458c673a2386914af9137d093b

golang: remove redundant quotes in extra_provides
Fixes: a6e94447f ("golang: fix missing dependency check")
Signed-off-by: George Sapkin <george@sapk.in>

lang/golang/golang-version.mk
@@ -295,5 +295,5 @@ endef
 # src/debug contains ELF executables as test data and they reference these
 # libraries we need to call this to pass CheckDependencies in package-pack.mk
 define Package/$(PKG_NAME)-tests/extra_provides
-	echo 'libc.so.6' libstdc++.so.6' libtiff.so.6' | tr ' ' '\n'
+	echo 'libc.so.6 libstdc++.so.6 libtiff.so.6' | tr ' ' '\n'
 endef