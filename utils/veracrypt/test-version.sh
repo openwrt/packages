f5605b9f7a0cf6070d02e37695af9cef5d787915
Ville Takio <ville+git@takio.fi>
2026-09-14 16:09:02 +0300

    veracrypt: mark helper GPL-2.0-only
    License mount.veracrypt and OpenWrt packaging helpers under GNU GPL v2
    only. VeraCrypt remains Apache-2.0 AND TrueCrypt License 3.0; wxWidgets
    remains the wxWindows Library Licence 3.1.
    
    https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
    
    Signed-off-by: Ville Takio <ville+git@takio.fi>

--- utils/veracrypt/test-version.sh
+++ utils/veracrypt/test-version.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-only
 # Skip generic --version probing of mount.veracrypt (it is a mount helper).
 
 case "$PKG_NAME" in
--- utils/veracrypt/test.sh
+++ utils/veracrypt/test.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-only
 veracrypt --text --version 2>&1 | grep -F "$PKG_VERSION"
